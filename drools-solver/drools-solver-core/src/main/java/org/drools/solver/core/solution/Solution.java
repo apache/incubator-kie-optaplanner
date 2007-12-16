73dfb68aa2c924b3f488021bf24e980f3b98e2d0
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2007-12-16 15:16:03 +0000

    javadoc
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@17270 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-solver/drools-solver-core/src/main/java/org/drools/solver/core/solution/Solution.java
+++ drools-solver/drools-solver-core/src/main/java/org/drools/solver/core/solution/Solution.java
@@ -7,12 +7,18 @@ import java.util.Collection;
  */
 public interface Solution {
 
-    Solution cloneSolution();
-
     /**
      * Called when the solution needs to be asserted into an empty WorkingMemory.
+     * These facts can be used by the score rules.
      * @return all the facts of this solution
      */
     Collection<? extends Object> getFacts();
+
+    /**
+     * Called when the solution needs to be cloned during solving,
+     * for example to store a clone of the current solution as the best solution. 
+     * @return a clone of which the properties that change during solving are deep cloned
+     */
+    Solution cloneSolution();
 
 }
