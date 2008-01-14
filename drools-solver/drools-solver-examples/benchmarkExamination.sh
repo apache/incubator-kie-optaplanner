0e852a9e5cdf05adcf6cf11bc5185ccb355c708a
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2008-01-14 19:11:58 +0000

    benchmarkExamination
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@17832 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ drools-solver/drools-solver-examples/benchmarkExamination.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+mainClass=org.drools.solver.examples.itc2007.examination.app.ExaminationBenchmarkApp
+if [ $# -ge 1 ];
+  then
+    mvn exec:exec -Dexec.mainClass="${mainClass}" -Dexec.programArgs="$*"
+  else
+    mvn exec:exec -Dexec.mainClass="${mainClass}"
+fi
