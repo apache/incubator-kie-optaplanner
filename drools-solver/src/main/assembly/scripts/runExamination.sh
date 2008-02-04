8b97dab5232a42cd98d91c93bf70ddf83f3e7974
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2008-02-04 21:02:35 +0000

    assembly for itc2007Examination
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@18303 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ drools-solver/src/main/assembly/scripts/runExamination.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+mainJar=lib/drools-solver-examples-4.1.0.SNAPSHOT.jar
+mainClass=org.drools.solver.examples.itc2007.examination.app.ExaminationCmdApp
+
+echo "Usage: ./runExamination.sh [maximumSecondsSpendPerSolution]"
+echo "For example: ./runExamination.sh 429"
+echo "Some notes:"
+echo "- Working dir should be the directory of this script."
+echo "- Java should be at least JDK 6 (not just JRE 6)"
+echo "- Environment variable JAVA_HOME should be set to the JDK installation directory"
+echo "  For example: export JAVA_HOME=/usr/lib/sun-jdk-...-6"
+echo
+echo "Starting..."
+
+# -Xmx128M or less works too, but might be slower
+$JAVA_HOME/bin/java -server -Xmx256M -cp ${mainJar} ${mainClass} $*
---
+++ drools-solver/src/main/assembly/scripts/runGuiExamination.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+mainJar=lib/drools-solver-examples-4.1.0.SNAPSHOT.jar
+mainClass=org.drools.solver.examples.itc2007.examination.app.ExaminationApp
+
+echo "Usage: ./runGuiExamination.sh"
+echo "For example: ./runGuiExamination.sh"
+echo "Some notes:"
+echo "- Working dir should be the directory of this script."
+echo "- Java should be at least JDK 6 (not just JRE 6)"
+echo "- Environment variable JAVA_HOME should be set to the JDK installation directory"
+echo "  For example: export JAVA_HOME=/usr/lib/sun-jdk-...-6"
+echo
+echo "Starting..."
+
+# -Xmx128M or less works too, but might be slower
+$JAVA_HOME/bin/java -server -Xmx256M -cp ${mainJar} ${mainClass} $*
