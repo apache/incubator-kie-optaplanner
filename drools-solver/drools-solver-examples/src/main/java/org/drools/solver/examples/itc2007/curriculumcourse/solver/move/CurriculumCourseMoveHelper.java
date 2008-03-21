e433502e9a71ba7c0044c5a94817361da1a5a6f2
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2008-03-21 20:38:49 +0000

    curruculumcourse moves
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@19192 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ drools-solver/drools-solver-examples/src/main/java/org/drools/solver/examples/itc2007/curriculumcourse/solver/move/CurriculumCourseMoveHelper.java
@@ -0,0 +1,39 @@
+package org.drools.solver.examples.itc2007.curriculumcourse.solver.move;
+
+import org.drools.FactHandle;
+import org.drools.WorkingMemory;
+import org.drools.solver.examples.itc2007.curriculumcourse.domain.Lecture;
+import org.drools.solver.examples.itc2007.curriculumcourse.domain.Period;
+import org.drools.solver.examples.itc2007.curriculumcourse.domain.Room;
+
+/**
+ * @author Geoffrey De Smet
+ */
+public class CurriculumCourseMoveHelper {
+
+    public static void movePeriod(WorkingMemory workingMemory, Lecture lecture, Period period) {
+        FactHandle factHandle = workingMemory.getFactHandle(lecture);
+        workingMemory.modifyRetract(factHandle);
+        lecture.setPeriod(period);
+        workingMemory.modifyInsert(factHandle, lecture);
+    }
+
+    public static void moveRoom(WorkingMemory workingMemory, Lecture lecture, Room room) {
+        FactHandle factHandle = workingMemory.getFactHandle(lecture);
+        workingMemory.modifyRetract(factHandle);
+        lecture.setRoom(room);
+        workingMemory.modifyInsert(factHandle, lecture);
+    }
+
+    public static void moveLecture(WorkingMemory workingMemory, Lecture lecture, Period period, Room room) {
+        FactHandle factHandle = workingMemory.getFactHandle(lecture);
+        workingMemory.modifyRetract(factHandle);
+        lecture.setPeriod(period);
+        lecture.setRoom(room);
+        workingMemory.modifyInsert(factHandle, lecture);
+    }
+
+    private CurriculumCourseMoveHelper() {
+    }
+
+}
