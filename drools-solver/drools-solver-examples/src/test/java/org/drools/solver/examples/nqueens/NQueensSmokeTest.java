cbf5d61aa9b28c9b592789e2ad04d575c382e6ea
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2008-10-02 17:15:31 +0000

    smoke test voor NQueens
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@23269 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ drools-solver/drools-solver-examples/src/test/java/org/drools/solver/examples/nqueens/NQueensSmokeTest.java
@@ -0,0 +1,35 @@
+package org.drools.solver.examples.nqueens;
+
+import junit.framework.TestCase;
+import org.drools.solver.core.Solver;
+import org.drools.solver.core.solution.Solution;
+import org.drools.solver.config.XmlSolverConfigurer;
+import org.drools.solver.examples.common.persistence.XstreamSolutionDaoImpl;
+import org.drools.solver.examples.common.persistence.SolutionDao;
+
+/**
+ * @author Geoffrey De Smet
+ */
+public class NQueensSmokeTest extends TestCase {
+
+    public static final String SOLVER_CONFIG
+            = "/org/drools/solver/examples/nqueens/solver/nqueensSmokeSolverConfig.xml";
+    public static final String UNSOLVED_DATA
+            = "/org/drools/solver/examples/nqueens/data/unsolvedNQueensSmoke.xml";
+
+
+    public void testSmoke() {
+        XmlSolverConfigurer configurer = new XmlSolverConfigurer();
+        configurer.configure(SOLVER_CONFIG);
+        Solver solver = configurer.buildSolver();
+        SolutionDao solutionDao = new XstreamSolutionDaoImpl();
+        Solution startingSolution = solutionDao.readSolution(getClass().getResourceAsStream(UNSOLVED_DATA));
+        solver.setStartingSolution(startingSolution);
+        solver.solve();
+        Solution bestSolution = solver.getBestSolution();
+        assertNotNull(bestSolution);
+        double bestScore = solver.getBestScore();
+        assertEquals(0.0, bestScore);
+    }
+
+}
