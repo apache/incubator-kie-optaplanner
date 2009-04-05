a2eee95961d619617a031003594e3499f4f2452d
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2009-04-05 18:56:07 +0000

    score_refactor attempt 1 - subpart of Score and ScoreComparator without weaving them into the code
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@25945 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-solver/drools-solver-core/src/test/java/org/drools/solver/core/score/DefaultHardAndSoftScoreTest.java
+++ drools-solver/drools-solver-core/src/test/java/org/drools/solver/core/score/DefaultHardAndSoftScoreTest.java
@@ -5,12 +5,12 @@ import junit.framework.TestCase;
 /**
  * @author Geoffrey De Smet
  */
-public class HardAndSoftScoreTest extends TestCase {
+public class DefaultHardAndSoftScoreTest extends TestCase {
 
     public void testCompareTo() {
-        Score a = new HardAndSoftScore(-1, -300);
-        Score b = new HardAndSoftScore(-20, -20);
-        Score c = new HardAndSoftScore(-20);
+        Score a = new DefaultHardAndSoftScore(-1, -300);
+        Score b = new DefaultHardAndSoftScore(-20, -20);
+        Score c = new DefaultHardAndSoftScore(-20);
         assertTrue(a.compareTo(b) > 0);
         assertTrue(b.compareTo(a) < 0);
         assertTrue(b.compareTo(c) > 0);
