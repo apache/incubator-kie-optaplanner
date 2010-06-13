5fe65a3cd0938879d1e58946b8c1521e45a14499
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2010-06-13 14:43:40 +0000

    nurse rostering competition app and assembly
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@33471 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ drools-planner/drools-planner-examples/src/main/java/org/drools/planner/examples/nurserostering/competition/NurseRosteringCompetitionSprintApp.java
@@ -0,0 +1,90 @@
+package org.drools.planner.examples.nurserostering.competition;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.drools.planner.config.XmlSolverConfigurer;
+import org.drools.planner.core.Solver;
+import org.drools.planner.examples.common.app.LoggingMain;
+import org.drools.planner.examples.nurserostering.domain.NurseRoster;
+import org.drools.planner.examples.nurserostering.persistence.NurseRosteringSolutionExporter;
+import org.drools.planner.examples.nurserostering.persistence.NurseRosteringSolutionImporter;
+
+/**
+ * @author Geoffrey De Smet
+ */
+public class NurseRosteringCompetitionSprintApp extends LoggingMain {
+
+    public static final String COMPETITION_LOGGING_CONFIG
+            = "/org/drools/planner/examples/nurserostering/competition/competition-log4j.xml";
+    public static final String SOLVER_CONFIG
+            = "/org/drools/planner/examples/nurserostering/competition/nurseRosteringCompetitionSprintSolverConfig.xml";
+    private static final int COMPETITION_TIME_SECONDS_SPEND = 10;
+
+    public static void main(String[] args) {
+        long maximumSecondsSpend;
+        if (args.length > 1) {
+            throw new IllegalArgumentException("Only one argument (maximumSecondsSpend) is allowed.");
+        } else if (args.length == 1) {
+            try {
+                maximumSecondsSpend = Integer.parseInt(args[0]);
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("The argument maximumSecondsSpend (" + args[0]
+                        + ") is not a number.");
+            }
+        } else {
+            maximumSecondsSpend = COMPETITION_TIME_SECONDS_SPEND;
+        }
+        new NurseRosteringCompetitionSprintApp(maximumSecondsSpend).solve();
+    }
+
+    protected NurseRosteringSolutionImporter importer;
+    protected NurseRosteringSolutionExporter exporter;
+    protected Solver solver;
+    protected File inputDir;
+    protected File outputDir;
+
+    public NurseRosteringCompetitionSprintApp(long maximumSecondsSpend) {
+        super(COMPETITION_LOGGING_CONFIG);
+        inputDir = new File("input");
+        if (!inputDir.exists()) {
+            throw new IllegalArgumentException("The input directory  (" + inputDir + ") does not exist.");
+        }
+        outputDir = new File("output");
+        if (!outputDir.exists()) {
+            throw new IllegalArgumentException("The output directory (" + outputDir + ") does not exist.");
+        }
+        importer = new NurseRosteringSolutionImporter();
+        exporter = new NurseRosteringSolutionExporter();
+        
+        XmlSolverConfigurer configurer = new XmlSolverConfigurer();
+        configurer.configure(SOLVER_CONFIG);
+        configurer.getConfig().getTerminationConfig().setMaximumSecondsSpend(maximumSecondsSpend);
+        solver = configurer.buildSolver();
+    }
+
+    private void solve() {
+        File[] inputFiles = inputDir.listFiles();
+        if (inputFiles == null) {
+            throw new IllegalArgumentException("The input directory  (" + inputDir + ") does not exist.");
+        }
+        Arrays.sort(inputFiles);
+        for (File inputFile : inputFiles) {
+            String inputFileName = inputFile.getName();
+            if (inputFileName.endsWith(".xml")) {
+                logger.info("Solving {}", inputFileName);
+                NurseRoster startingSolution = (NurseRoster) importer.readSolution(inputFile);
+                solver.setStartingSolution(startingSolution);
+                solver.solve();
+                NurseRoster bestSolution = (NurseRoster) solver.getBestSolution();
+                File outputFile = new File(outputDir, inputFileName.substring(0, inputFileName.length() - 4)
+                        + "_geoffrey_de_smet.xml");
+                exporter.writeSolution(bestSolution, outputFile);
+            }
+        }
+        logger.info("Done");
+    }
+
+}
