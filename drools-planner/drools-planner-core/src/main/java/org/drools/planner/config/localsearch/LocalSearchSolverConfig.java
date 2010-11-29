d8f119e22881c08f8c46fa5e8478a86b74fb4b9a
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2010-11-29 14:49:13 +0000

    JBRULES-2809 environmentmode trace to detect the corruption early
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@36111 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-planner/drools-planner-core/src/main/java/org/drools/planner/config/localsearch/LocalSearchSolverConfig.java
+++ drools-planner/drools-planner-core/src/main/java/org/drools/planner/config/localsearch/LocalSearchSolverConfig.java
@@ -40,7 +40,6 @@ import org.drools.planner.core.localsearch.LocalSearchSolver;
 import org.drools.planner.core.localsearch.bestsolution.BestSolutionRecaller;
 import org.drools.planner.core.localsearch.decider.Decider;
 import org.drools.planner.core.localsearch.decider.DefaultDecider;
-import org.drools.planner.core.move.Move;
 import org.drools.planner.core.solution.initializer.StartingSolutionInitializer;
 import org.drools.planner.core.score.definition.ScoreDefinition;
 
@@ -192,7 +191,7 @@ public class LocalSearchSolverConfig {
         localSearchSolver.setBestSolutionRecaller(new BestSolutionRecaller());
         localSearchSolver.setTermination(terminationConfig.buildTermination(scoreDefinition));
         localSearchSolver.setDecider(buildDecider());
-        if (environmentMode == EnvironmentMode.DEBUG) {
+        if (environmentMode == EnvironmentMode.DEBUG || environmentMode == EnvironmentMode.TRACE) {
             localSearchSolver.setAssertStepScoreIsUncorrupted(true);
         }
         return localSearchSolver;
@@ -251,7 +250,10 @@ public class LocalSearchSolverConfig {
         decider.setSelector(selectorConfig.buildSelector());
         decider.setAcceptor(acceptorConfig.buildAcceptor());
         decider.setForager(foragerConfig.buildForager());
-        if (environmentMode == EnvironmentMode.DEBUG) {
+        if ( environmentMode == EnvironmentMode.TRACE) {
+            decider.setAssertMoveScoreIsUncorrupted(true);
+        }
+        if (environmentMode == EnvironmentMode.DEBUG || environmentMode == EnvironmentMode.TRACE) {
             decider.setAssertUndoMoveIsUncorrupted(true);
         }
         return decider;
@@ -310,49 +312,6 @@ public class LocalSearchSolverConfig {
         } else if (inheritedConfig.getForagerConfig() != null) {
             foragerConfig.inherit(inheritedConfig.getForagerConfig());
         }
-    }
-
-    /**
-     * A solver has a single Random instance. Some solver configurations use the Random instance a lot more than others.
-     * For example simulated annealing depends highly on random numbers,
-     * while tabu search only depends on it to deal with score ties.
-     * The environment mode influences the seed of that Random instance.
-     * <p/>
-     * The environment mode also allows you to detect common bugs in your implementation.
-     */
-    public enum EnvironmentMode {
-        /**
-         * The debug mode is reproducible (see the reproducible mode)
-         * and also turns on assertions (such as {@link DefaultDecider#assertUndoMoveIsUncorrupted})
-         * to fail-fast on a bug in a {@link Move} implementation, a score rule, ...
-         * <p>
-         * The debug mode is slow.
-         */
-        DEBUG,
-        /**
-         * The reproducible mode is the default mode because it is recommended during development.
-         * In this mode, 2 runs on the same computer will execute the same code in the same order.
-         * They will also yield the same result, except if they use a time based termination
-         * and they have a sufficiently large difference in allocated CPU time.
-         * This allows you to benchmark new optimizations (such as a new move implementation) fairly.
-         * <p>
-         * The reproducible mode is not much slower than the production mode.
-         * </p>
-         * In practice, this mode uses the default random seed,
-         * and it also disables certain concurrency optimizations (such as work stealing).
-         * TODO: JBRULES-681 Multi-threaded support which implement those concurrency optimizations
-         */
-        REPRODUCIBLE,
-        /**
-         * The production mode is the fastest and the most robust, but not reproducible.
-         * It is recommended for a production environment.
-         * <p>
-         * The random seed is different on every run, which makes it more robust against an unlucky random seed.
-         * An unlucky random seed gives a bad result on a certain data set with a certain solver configuration.
-         * Note that in most use cases the impact of the random seed is relatively low on the result.
-         * An occasional bad result is far more likely caused by another issue (such as a score trap).
-         */
-        PRODUCTION
     }
 
 }
