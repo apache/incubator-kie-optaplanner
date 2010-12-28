9d9cb1a33f7de246900c14565621cf79fd3fbbee
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2010-12-28 15:16:11 +0100

    remove planner benchmark scripts - no longer used

--- drools-planner/drools-planner-examples/benchmarkPatientAdmissionSchedule.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-mainClass=org.drools.planner.examples.pas.app.PatientAdmissionScheduleBenchmarkApp
-if [ $# -ge 1 ];
-  then
-    mvn exec:exec -Dexec.mainClass="${mainClass}" -Dexec.programArgs="$*"
-  else
-    mvn exec:exec -Dexec.mainClass="${mainClass}"
-fi
--- drools-planner/drools-planner-examples/benchmarkSmartTravelingTournament.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-mainClass=org.drools.planner.examples.travelingtournament.app.smart.SmartTravelingTournamentBenchmarkApp
-if [ $# -ge 1 ];
-  then
-    mvn exec:exec -Dexec.mainClass="${mainClass}" -Dexec.programArgs="$*"
-  else
-    mvn exec:exec -Dexec.mainClass="${mainClass}"
-fi
