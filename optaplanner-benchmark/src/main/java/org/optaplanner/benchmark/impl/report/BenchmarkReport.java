edcf279dab5ab44fccc6c6ab66ca9338515b7d3a
carrot-osc <359468@mail.muni.cz>
2013-05-06 01:10:58 +0200

    BestScorePerTimeChart (scatter plot) added to BenchmarkReport

--- optaplanner-benchmark/src/main/java/org/optaplanner/benchmark/impl/report/BenchmarkReport.java
+++ optaplanner-benchmark/src/main/java/org/optaplanner/benchmark/impl/report/BenchmarkReport.java
@@ -17,6 +17,7 @@
 package org.optaplanner.benchmark.impl.report;
 
 import java.awt.BasicStroke;
+import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileOutputStream;
@@ -30,12 +31,34 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+
 import javax.imageio.ImageIO;
 
-import freemarker.template.Configuration;
-import freemarker.template.Template;
-import freemarker.template.TemplateException;
 import org.apache.commons.io.IOUtils;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.LegendItem;
+import org.jfree.chart.LegendItemCollection;
+import org.jfree.chart.LegendItemSource;
+import org.jfree.chart.axis.CategoryAxis;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.labels.ItemLabelAnchor;
+import org.jfree.chart.labels.ItemLabelPosition;
+import org.jfree.chart.labels.StandardCategoryItemLabelGenerator;
+import org.jfree.chart.labels.XYItemLabelGenerator;
+import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.category.BarRenderer;
+import org.jfree.chart.renderer.xy.StandardXYItemRenderer;
+import org.jfree.chart.renderer.xy.XYItemRenderer;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.chart.title.LegendTitle;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.data.general.DatasetUtilities;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+import org.jfree.ui.TextAnchor;
 import org.optaplanner.benchmark.impl.DefaultPlannerBenchmark;
 import org.optaplanner.benchmark.impl.ProblemBenchmark;
 import org.optaplanner.benchmark.impl.SingleBenchmark;
@@ -43,22 +66,10 @@ import org.optaplanner.benchmark.impl.SolverBenchmark;
 import org.optaplanner.benchmark.impl.statistic.MillisecondsSpendNumberFormat;
 import org.optaplanner.benchmark.impl.statistic.ProblemStatistic;
 import org.optaplanner.core.api.solver.SolverFactory;
-import org.jfree.chart.JFreeChart;
-import org.jfree.chart.axis.CategoryAxis;
-import org.jfree.chart.axis.NumberAxis;
-import org.jfree.chart.labels.ItemLabelAnchor;
-import org.jfree.chart.labels.ItemLabelPosition;
-import org.jfree.chart.labels.StandardCategoryItemLabelGenerator;
-import org.jfree.chart.plot.CategoryPlot;
-import org.jfree.chart.plot.PlotOrientation;
-import org.jfree.chart.plot.XYPlot;
-import org.jfree.chart.renderer.category.BarRenderer;
-import org.jfree.chart.renderer.xy.StandardXYItemRenderer;
-import org.jfree.chart.renderer.xy.XYItemRenderer;
-import org.jfree.data.category.DefaultCategoryDataset;
-import org.jfree.data.xy.XYSeries;
-import org.jfree.data.xy.XYSeriesCollection;
-import org.jfree.ui.TextAnchor;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
 
 public class BenchmarkReport {
 
@@ -70,6 +81,7 @@ public class BenchmarkReport {
     protected File htmlOverviewFile = null;
     protected File summaryDirectory = null;
     protected List<File> bestScoreSummaryChartFileList = null;
+    protected List<File> bestScorePerTimeChartFileList = null;
     protected List<File> bestScoreScalabilitySummaryChartFileList = null;
     protected List<File> winningScoreDifferenceSummaryChartFileList = null;
     protected List<File> worstScoreDifferencePercentageSummaryChartFileList = null;
@@ -107,6 +119,10 @@ public class BenchmarkReport {
     public List<File> getBestScoreSummaryChartFileList() {
         return bestScoreSummaryChartFileList;
     }
+    
+    public List<File> getBestScorePerTimeChartFileList() {
+		return bestScorePerTimeChartFileList;
+	}
 
     public List<File> getBestScoreScalabilitySummaryChartFileList() {
         return bestScoreScalabilitySummaryChartFileList;
@@ -178,6 +194,7 @@ public class BenchmarkReport {
         summaryDirectory.mkdir();
         fillWarningList();
         writeBestScoreSummaryCharts();
+        writeBestScorePerTimeSummaryChart();
         writeBestScoreScalabilitySummaryChart();
         writeWinningScoreDifferenceSummaryChart();
         writeWorstScoreDifferencePercentageSummaryChart();
@@ -237,6 +254,93 @@ public class BenchmarkReport {
             bestScoreSummaryChartFileList.add(writeChartToImageFile(chart, "bestScoreSummaryLevel" + scoreLevelIndex));
             scoreLevelIndex++;
         }
+    }
+    
+    private void writeBestScorePerTimeSummaryChart() {
+    	// Each scoreLevel has it's own dataset and chartFile
+        List<XYSeriesCollection> datasetList = new ArrayList<XYSeriesCollection>(CHARTED_SCORE_LEVEL_SIZE);
+        for (SolverBenchmark solverBenchmark : plannerBenchmark.getSolverBenchmarkList()) {
+            String solverLabel = solverBenchmark.getNameWithFavoriteSuffix();
+            for (SingleBenchmark singleBenchmark : solverBenchmark.getSingleBenchmarkList()) {
+            	String planningProblemLabel = singleBenchmark.getProblemBenchmark().getName();
+                if (singleBenchmark.isSuccess()) {
+                    double[] levelValues = singleBenchmark.getScore().toDoubleLevels();
+                    for (int i = 0; i < levelValues.length && i < CHARTED_SCORE_LEVEL_SIZE; i++) {
+                        if (i >= datasetList.size()) {
+                            datasetList.add(new XYSeriesCollection());
+                        }	
+                        double relativeScore = (levelValues[i] == 0d) ? 0 : levelValues[i] / Math.abs(plannerBenchmark.getAverageSolverScore().toDoubleLevels()[i]);
+                        if (datasetList.get(i).getSeriesIndex(solverLabel) == -1) {
+                        	XYSeries series = new XYSeries(solverLabel, false);
+                        	series.add(singleBenchmark.getTimeMillisSpend(), relativeScore);
+                        	datasetList.get(i).addSeries(series);
+                        } else {
+                            datasetList.get(i).getSeries(solverLabel).add(singleBenchmark.getTimeMillisSpend(), relativeScore);
+                        }
+                    }
+                }
+            }
+        }
+        bestScorePerTimeChartFileList = new ArrayList<File>(datasetList.size());
+        int scoreLevelIndex = 0;
+        for (XYSeriesCollection dataset : datasetList) {
+        	XYPlot plot = createBestScorePerTimePlot(dataset, "Time spent", "Best score (relative to average)", NumberFormat.getInstance(locale));
+            JFreeChart chart = new JFreeChart("Best score per time level " + scoreLevelIndex + " (lower is better)", plot);
+            addBestScorePerTimeLegend(chart);        
+            bestScorePerTimeChartFileList.add(writeChartToImageFile(chart, "bestScorePerTimeLevel" + scoreLevelIndex));
+            scoreLevelIndex++;
+        }
+    }
+
+	private void addBestScorePerTimeLegend(JFreeChart chart) {
+		LegendTitle legend = new LegendTitle(new LegendItemSource() {
+		    @Override
+		    public LegendItemCollection getLegendItems() {
+		        LegendItemCollection coll = new LegendItemCollection();
+		        int i = 1;
+		        for (ProblemBenchmark pb : plannerBenchmark.getUnifiedProblemBenchmarkList()) {
+		            coll.add(new LegendItem("(" + i++ +") " + pb.getName()));
+
+		        }
+		        return coll;
+		    }
+		});
+		legend.setBorder(1, 1, 1, 1);
+		legend.setBackgroundPaint(Color.WHITE);
+		chart.addLegend(legend);
+	}
+    
+    private XYPlot createBestScorePerTimePlot(XYSeriesCollection dataset, String xAxisLabel,
+    		String yAxisLabel, NumberFormat numberFormat) {
+    	NumberAxis xAxis = new NumberAxis(xAxisLabel);
+    	xAxis.setNumberFormatOverride(numberFormat);
+    	NumberAxis yAxis = new NumberAxis(yAxisLabel);
+    	yAxis.setNumberFormatOverride(numberFormat);
+    	double minimum = DatasetUtilities.findMinimumRangeValue(dataset).doubleValue();
+    	if (minimum != 0d) {
+    		yAxis.setUpperBound(0.02);
+        	yAxis.setLowerBound(minimum - 0.2);
+    	}
+    	XYPlot plot = new XYPlot(dataset, xAxis, yAxis, createBestScorePerTimeRenderer());
+    	plot.setOrientation(PlotOrientation.VERTICAL);
+    	return plot;
+    }
+    
+    private XYItemRenderer createBestScorePerTimeRenderer() {
+    	XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer(false, true);
+    	renderer.setAutoPopulateSeriesShape(false);
+    	renderer.setBaseSeriesVisible(true);
+			renderer.setBaseItemLabelGenerator(new XYItemLabelGenerator() {
+				@Override
+				public String generateLabel(XYDataset xyd, int i, int i1) {
+					return String.valueOf(i1 + 1);
+				}
+			});
+			renderer.setBaseItemLabelsVisible(true);
+			ItemLabelPosition position = new ItemLabelPosition(ItemLabelAnchor.CENTER, TextAnchor.CENTER);
+			renderer.setBaseNegativeItemLabelPosition(position);
+			renderer.setBasePositiveItemLabelPosition(position);
+    	return renderer;
     }
 
     private void writeBestScoreScalabilitySummaryChart() {
