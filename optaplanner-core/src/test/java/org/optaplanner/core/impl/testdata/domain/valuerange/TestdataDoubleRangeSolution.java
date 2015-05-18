922d6c47b5d2688d5c20924b76dc9e807bb40d64
Jiri Vahala <jvahala@redhat.com>
2015-05-18 13:22:10 +0200

    Added custom valueRange integration test

---
+++ optaplanner-core/src/test/java/org/optaplanner/core/impl/testdata/domain/valuerange/TestdataDoubleRangeSolution.java
@@ -0,0 +1,42 @@
+package org.optaplanner.core.impl.testdata.domain.valuerange;
+
+import org.optaplanner.core.api.domain.solution.PlanningEntityCollectionProperty;
+import org.optaplanner.core.api.domain.solution.PlanningSolution;
+import org.optaplanner.core.api.domain.solution.Solution;
+import org.optaplanner.core.api.score.buildin.simple.SimpleScore;
+import org.optaplanner.core.impl.testdata.domain.TestdataObject;
+
+import java.util.Collection;
+import java.util.List;
+
+@PlanningSolution
+public class TestdataDoubleRangeSolution extends TestdataObject implements Solution<SimpleScore> {
+
+    private List<TestdataUncountableEntity> entities;
+    private SimpleScore score;
+
+    @PlanningEntityCollectionProperty
+    public List<TestdataUncountableEntity> getEntities() {
+        return entities;
+    }
+
+    public void setEntities(List<TestdataUncountableEntity> entities) {
+        this.entities = entities;
+    }
+
+    @Override
+    public SimpleScore getScore() {
+        return score;
+    }
+
+    @Override
+    public void setScore(SimpleScore score) {
+        this.score = score;
+    }
+
+    @Override
+    public Collection<?> getProblemFacts() {
+        return null;
+    }
+
+}
