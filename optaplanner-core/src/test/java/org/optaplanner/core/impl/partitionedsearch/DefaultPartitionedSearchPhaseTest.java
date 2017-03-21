bc777b6df970ec1537b16b0dff057d77f4f98b09
Jiří Locker <jiri.locker@gmail.com>
2017-03-21 17:06:06 +0100

    Use Config API

--- optaplanner-core/src/test/java/org/optaplanner/core/impl/partitionedsearch/DefaultPartitionedSearchPhaseTest.java
+++ optaplanner-core/src/test/java/org/optaplanner/core/impl/partitionedsearch/DefaultPartitionedSearchPhaseTest.java
@@ -19,37 +19,27 @@ import java.lang.management.ManagementFactory;
 import java.lang.management.ThreadMXBean;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Random;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.optaplanner.core.api.score.buildin.simple.SimpleScore;
 import org.optaplanner.core.api.solver.SolverFactory;
 import org.optaplanner.core.config.constructionheuristic.ConstructionHeuristicPhaseConfig;
-import org.optaplanner.core.config.heuristic.policy.HeuristicConfigPolicy;
-import org.optaplanner.core.config.solver.EnvironmentMode;
-import org.optaplanner.core.config.solver.recaller.BestSolutionRecallerConfig;
+import org.optaplanner.core.config.localsearch.LocalSearchPhaseConfig;
+import org.optaplanner.core.config.partitionedsearch.PartitionedSearchPhaseConfig;
+import org.optaplanner.core.config.solver.SolverConfig;
 import org.optaplanner.core.config.solver.termination.TerminationConfig;
 import org.optaplanner.core.impl.partitionedsearch.partitioner.SolutionPartitioner;
 import org.optaplanner.core.impl.partitionedsearch.scope.PartitionedSearchPhaseScope;
 import org.optaplanner.core.impl.phase.event.PhaseLifecycleListenerAdapter;
-import org.optaplanner.core.impl.phase.event.PhaseLifecycleSupport;
 import org.optaplanner.core.impl.phase.scope.AbstractPhaseScope;
-import org.optaplanner.core.impl.score.director.InnerScoreDirectorFactory;
 import org.optaplanner.core.impl.score.director.ScoreDirector;
-import org.optaplanner.core.impl.solver.event.SolverEventSupport;
-import org.optaplanner.core.impl.solver.recaller.BestSolutionRecaller;
-import org.optaplanner.core.impl.solver.scope.DefaultSolverScope;
-import org.optaplanner.core.impl.solver.termination.BasicPlumbingTermination;
-import org.optaplanner.core.impl.solver.termination.Termination;
-import org.optaplanner.core.impl.solver.thread.DefaultSolverThreadFactory;
+import org.optaplanner.core.impl.solver.DefaultSolver;
 import org.optaplanner.core.impl.testdata.domain.TestdataEntity;
 import org.optaplanner.core.impl.testdata.domain.TestdataSolution;
 import org.optaplanner.core.impl.testdata.domain.TestdataValue;
 import org.optaplanner.core.impl.testdata.util.PlannerTestUtils;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
 
 public class DefaultPartitionedSearchPhaseTest {
 
@@ -63,40 +53,10 @@ public class DefaultPartitionedSearchPhaseTest {
 
     @Test
     public void solve() {
-        BestSolutionRecaller<TestdataSolution> bestSolutionRecaller = new BestSolutionRecallerConfig()
-                .buildBestSolutionRecaller(EnvironmentMode.REPRODUCIBLE);
-        bestSolutionRecaller.setSolverEventSupport(mock(SolverEventSupport.class));
-
-        SolverFactory<TestdataSolution> sf = PlannerTestUtils
-                .buildSolverFactory(TestdataSolution.class, TestdataEntity.class);
-        HeuristicConfigPolicy configPolicy = new HeuristicConfigPolicy(EnvironmentMode.REPRODUCIBLE,
-                (InnerScoreDirectorFactory) sf.buildSolver().getScoreDirectorFactory());
-
-        DefaultSolverScope<TestdataSolution> scope = new DefaultSolverScope<>();
-        scope.setScoreDirector(configPolicy.getScoreDirectorFactory().buildScoreDirector());
-
-        TestdataSolution solution = new TestdataSolution();
-        solution.setEntityList(Arrays.asList(new TestdataEntity("A")));
-        solution.setValueList(Arrays.asList(new TestdataValue("1"), new TestdataValue("2")));
-
-        scope.setBestSolution(solution);
-        scope.setBestScore(SimpleScore.valueOfUninitialized(-1, 0));
-        scope.setWorkingSolutionFromBestSolution();
-        scope.setWorkingRandom(new Random(0));
-
-        PhaseLifecycleSupport<TestdataSolution> phaseLifecycleSupport = new PhaseLifecycleSupport<>();
-        Termination termination = new TerminationConfig()
-                .buildTermination(configPolicy, new BasicPlumbingTermination(false));
-
-        DefaultPartitionedSearchPhase<TestdataSolution> phase
-                = new DefaultPartitionedSearchPhase<>(0, "", bestSolutionRecaller, termination);
-        phase.setSolverPhaseLifecycleSupport(phaseLifecycleSupport);
-        phase.setSolutionPartitioner(new TestdataSolutionPartitioner());
-        phase.setRunnablePartThreadLimit(1);
-        phase.setThreadFactory(new DefaultSolverThreadFactory("PartThread"));
-        phase.setConfigPolicy(configPolicy);
-        ConstructionHeuristicPhaseConfig constructionHeuristicPhaseConfig = new ConstructionHeuristicPhaseConfig();
-        phase.setPhaseConfigList(Arrays.asList(constructionHeuristicPhaseConfig));
+        SolverFactory<TestdataSolution> solverFactory = createSolverFactory();
+        DefaultSolver<TestdataSolution> solver = (DefaultSolver<TestdataSolution>) solverFactory.buildSolver();
+        PartitionedSearchPhase<TestdataSolution> phase
+                = (PartitionedSearchPhase<TestdataSolution>) solver.getPhaseList().get(0);
 
         // test partCount
         phase.addPhaseLifecycleListener(new PhaseLifecycleListenerAdapter<TestdataSolution>() {
@@ -107,10 +67,31 @@ public class DefaultPartitionedSearchPhaseTest {
             }
         });
 
-        scope.startingNow();
-        phase.solve(scope);
-        phase.solve(scope);
+        solver.solve(createSolution());
         assertEquals(initialThreadCount, threads.getThreadCount());
+    }
+
+    private static SolverFactory<TestdataSolution> createSolverFactory() {
+        SolverFactory<TestdataSolution> solverFactory = PlannerTestUtils
+                .buildSolverFactory(TestdataSolution.class, TestdataEntity.class);
+        SolverConfig solverConfig = solverFactory.getSolverConfig();
+        PartitionedSearchPhaseConfig partitionedSearchPhaseConfig = new PartitionedSearchPhaseConfig();
+        partitionedSearchPhaseConfig.setSolutionPartitionerClass(TestdataSolutionPartitioner.class);
+        solverConfig.setPhaseConfigList(Arrays.asList(partitionedSearchPhaseConfig));
+        ConstructionHeuristicPhaseConfig constructionHeuristicPhaseConfig = new ConstructionHeuristicPhaseConfig();
+        LocalSearchPhaseConfig localSearchPhaseConfig = new LocalSearchPhaseConfig();
+        TerminationConfig terminationConfig = new TerminationConfig();
+        terminationConfig.setStepCountLimit(1);
+        localSearchPhaseConfig.setTerminationConfig(terminationConfig);
+        partitionedSearchPhaseConfig.setPhaseConfigList(Arrays.asList(constructionHeuristicPhaseConfig, localSearchPhaseConfig));
+        return solverFactory;
+    }
+
+    private static TestdataSolution createSolution() {
+        TestdataSolution solution = new TestdataSolution();
+        solution.setEntityList(Arrays.asList(new TestdataEntity("A")));
+        solution.setValueList(Arrays.asList(new TestdataValue("1"), new TestdataValue("2")));
+        return solution;
     }
 
     public static class TestdataSolutionPartitioner implements SolutionPartitioner<TestdataSolution> {
