2392030c1ca296821a03f068b74c55f02e07cd7a
Lukáš Petrovický <lpetrovi@redhat.com>
2019-09-05 08:34:25 +0200

    Remove the TODOs and introduce more code reuse

--- optaplanner-examples/src/main/java/org/optaplanner/examples/taskassigning/solver/score/TaskAssigningConstraintProvider.java
+++ optaplanner-examples/src/main/java/org/optaplanner/examples/taskassigning/solver/score/TaskAssigningConstraintProvider.java
@@ -13,13 +13,30 @@ public final class TaskAssigningConstraintProvider implements ConstraintProvider
     private static final int BENDABLE_SCORE_HARD_LEVELS_SIZE = 1;
     private static final int BENDABLE_SCORE_SOFT_LEVELS_SIZE = 4;
 
-    private static UniConstraintStream<Task> getInitializedTaskUniStream(ConstraintFactory constraintFactory) {
+    @Override
+    public Constraint[] defineConstraints(ConstraintFactory constraintFactory) {
+        return new Constraint[]{
+                noMissingSkills(constraintFactory),
+                minimizeMakespan(constraintFactory),
+                criticalPriorityBasedTaskEndTime(constraintFactory),
+                majorPriorityTaskEndTime(constraintFactory),
+                minorPriorityTaskEndTime(constraintFactory)
+        };
+    }
+
+    private static UniConstraintStream<Task> getInitializedTask(ConstraintFactory constraintFactory) {
         return constraintFactory.from(Task.class)
                 .filter(task -> task.getEmployee() != null);
     }
 
+    private static UniConstraintStream<Task> getInitializedTaskWithPriority(ConstraintFactory constraintFactory,
+            Priority priority) {
+        return getInitializedTask(constraintFactory)
+                .filter(task -> task.getPriority() == priority);
+    }
+
     private static Constraint noMissingSkills(ConstraintFactory constraintFactory) {
-        return getInitializedTaskUniStream(constraintFactory)
+        return getInitializedTask(constraintFactory)
                 .filter(task -> task.getMissingSkillCount() > 0)
                 .penalize("No missing skills",
                         BendableScore.ofHard(BENDABLE_SCORE_HARD_LEVELS_SIZE, BENDABLE_SCORE_SOFT_LEVELS_SIZE, 0, 1),
@@ -27,54 +44,32 @@ public final class TaskAssigningConstraintProvider implements ConstraintProvider
     }
 
     private static Constraint criticalPriorityBasedTaskEndTime(ConstraintFactory constraintFactory) {
-        return getInitializedTaskUniStream(constraintFactory)
-                .filter(task -> task.getPriority() == Priority.CRITICAL)
+        return getInitializedTaskWithPriority(constraintFactory, Priority.CRITICAL)
                 .penalize("Critical priority task end time",
                         BendableScore.ofSoft(BENDABLE_SCORE_HARD_LEVELS_SIZE, BENDABLE_SCORE_SOFT_LEVELS_SIZE, 0, 1),
                         Task::getEndTime);
     }
 
     private static Constraint minimizeMakespan(ConstraintFactory constraintFactory) {
-        return getInitializedTaskUniStream(constraintFactory)
+        return getInitializedTask(constraintFactory)
                 .filter(task -> task.getNextTask() == null)
                 .penalize("Minimize makespan, latest ending employee first",
                         BendableScore.ofSoft(BENDABLE_SCORE_HARD_LEVELS_SIZE, BENDABLE_SCORE_SOFT_LEVELS_SIZE, 1, 1),
                         task -> task.getEndTime() * task.getEndTime());
-        /*
-         * TODO ^^^ Some constraints need exponential weighting. perhaps we should have dynamic
-         *  constraint weights, inferred from the entity?
-         */
     }
 
     private static Constraint majorPriorityTaskEndTime(ConstraintFactory constraintFactory) {
-        return getInitializedTaskUniStream(constraintFactory)
-                .filter(task -> task.getPriority() == Priority.MAJOR)
+        return getInitializedTaskWithPriority(constraintFactory, Priority.MAJOR)
                 .penalize("Major priority task end time",
                         BendableScore.ofSoft(BENDABLE_SCORE_HARD_LEVELS_SIZE, BENDABLE_SCORE_SOFT_LEVELS_SIZE, 2, 1),
                         Task::getEndTime);
     }
 
     private static Constraint minorPriorityTaskEndTime(ConstraintFactory constraintFactory) {
-        return getInitializedTaskUniStream(constraintFactory)
-                .filter(task -> task.getPriority() == Priority.MINOR)
+        return getInitializedTaskWithPriority(constraintFactory, Priority.MINOR)
                 .penalize("Minor priority task end time",
                         BendableScore.ofSoft(BENDABLE_SCORE_HARD_LEVELS_SIZE, BENDABLE_SCORE_SOFT_LEVELS_SIZE, 3, 1),
                         Task::getEndTime);
     }
 
-    @Override
-    public Constraint[] defineConstraints(ConstraintFactory constraintFactory) {
-        return new Constraint[]{
-                noMissingSkills(constraintFactory),
-                minimizeMakespan(constraintFactory),
-                /*
-                 * TODO if we had a way of determining constraint weight from the entity, different weight for different
-                 *  priority, then the following 3 constraints could have been one constraint, with associated
-                 *  performance benefits.
-                 */
-                criticalPriorityBasedTaskEndTime(constraintFactory),
-                majorPriorityTaskEndTime(constraintFactory),
-                minorPriorityTaskEndTime(constraintFactory)
-        };
-    }
 }
