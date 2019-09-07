6ac6f13d4aeff8bc73dda2caf39574616291dcfe
Lukáš Petrovický <lukas@petrovicky.net>
2019-09-07 20:47:43 +0200

    Better shift comparator

--- optaplanner-examples/src/main/java/org/optaplanner/examples/nurserostering/domain/ShiftAssignmentComparator.java
+++ optaplanner-examples/src/main/java/org/optaplanner/examples/nurserostering/domain/ShiftAssignmentComparator.java
@@ -4,12 +4,13 @@ import java.util.Comparator;
 
 public class ShiftAssignmentComparator implements Comparator<ShiftAssignment> {
 
-    private static final Comparator<ShiftAssignment> COMPARATOR =
-            Comparator.comparing((ShiftAssignment a) -> a.getShiftDate().getDate())
-            .thenComparing(ShiftAssignment::getIndexInShift);
+    private static final Comparator<Shift> COMPARATOR =
+            Comparator.comparing(Shift::getShiftDate)
+                    .thenComparing(a -> a.getShiftType().getStartTimeString())
+                    .thenComparing(a -> a.getShiftType().getEndTimeString());
 
     @Override
     public int compare(ShiftAssignment o1, ShiftAssignment o2) {
-        return COMPARATOR.compare(o1, o2);
+        return COMPARATOR.compare(o1.getShift(), o2.getShift());
     }
 }
