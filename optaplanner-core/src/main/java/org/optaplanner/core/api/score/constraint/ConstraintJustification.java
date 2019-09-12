529cde5d93be3637aba56a4f0b6d18599489005a
Lukáš Petrovický <lukas@petrovicky.net>
2019-09-12 17:59:11 +0200

    PLANNER-1607 Improve Javadoc for ConstraintJustification

--- optaplanner-core/src/main/java/org/optaplanner/core/api/score/constraint/ConstraintJustification.java
+++ optaplanner-core/src/main/java/org/optaplanner/core/api/score/constraint/ConstraintJustification.java
@@ -5,7 +5,8 @@ import java.util.Comparator;
 public interface ConstraintJustification {
 
     /**
-     * Used by the GUI to sort the {@link ConstraintMatch} list by {@link ConstraintMatch#getJustificationList()}.
+     * Used by the GUI to sort the {@link ConstraintMatch} list by {@link ConstraintMatch#getJustificationList()}. Must
+     * be able to meaningfully compare classes of different types, if only by comparing their {@link Class#getName()}.
      */
     Comparator getConstraintJustificationComparator();
 
