86159f79fa53211eb4cc821c5b28096677657fb5
Lukáš Petrovický <lukas@petrovicky.net>
2019-09-12 16:34:18 +0200

    PLANNER-1607 taskassigning avoids CompareToBuilder

--- optaplanner-examples/src/main/java/org/optaplanner/examples/taskassigning/domain/solver/TaskDifficultyComparator.java
+++ optaplanner-examples/src/main/java/org/optaplanner/examples/taskassigning/domain/solver/TaskDifficultyComparator.java
@@ -19,19 +19,20 @@ package org.optaplanner.examples.taskassigning.domain.solver;
 import java.io.Serializable;
 import java.util.Comparator;
 
-import org.apache.commons.lang3.builder.CompareToBuilder;
+import org.optaplanner.core.api.score.constraint.ConstraintJustification;
 import org.optaplanner.examples.taskassigning.domain.Task;
 
-public class TaskDifficultyComparator implements Comparator<Task>, Serializable {
+public class TaskDifficultyComparator implements Comparator<Task>,
+        Serializable {
+
+    private static final Comparator<Task> COMPARATOR =
+            Comparator.comparing(Task::getPriority)
+                    .thenComparingInt(task -> task.getTaskType().getRequiredSkillList().size())
+                    .thenComparingInt(task -> task.getTaskType().getBaseDuration())
+                    .thenComparing(ConstraintJustification.COMPARATOR);
 
     @Override
     public int compare(Task a, Task b) {
-        return new CompareToBuilder()
-                .append(a.getPriority(), b.getPriority())
-                .append(a.getTaskType().getRequiredSkillList().size(), b.getTaskType().getRequiredSkillList().size())
-                .append(a.getTaskType().getBaseDuration(), b.getTaskType().getBaseDuration())
-                .append(a.getId(), b.getId())
-                .toComparison();
+        return COMPARATOR.compare(a, b);
     }
-
 }
