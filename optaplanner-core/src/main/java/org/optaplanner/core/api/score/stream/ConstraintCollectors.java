08dd6f66fa8c172b921e4710b4209d53044cc494
Lukáš Petrovický <lukas@petrovicky.net>
2019-09-24 15:13:18 +0200

    PLANNER-1667 Restore the original collectors following up on code review

--- optaplanner-core/src/main/java/org/optaplanner/core/api/score/stream/ConstraintCollectors.java
+++ optaplanner-core/src/main/java/org/optaplanner/core/api/score/stream/ConstraintCollectors.java
@@ -21,14 +21,12 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.SortedMap;
 import java.util.TreeMap;
-import java.util.concurrent.atomic.LongAdder;
 import java.util.function.Function;
 import java.util.function.ToIntBiFunction;
 import java.util.function.ToIntFunction;
 import java.util.function.ToLongBiFunction;
 import java.util.function.ToLongFunction;
 
-import com.google.common.base.Functions;
 import org.optaplanner.core.api.score.stream.bi.BiConstraintCollector;
 import org.optaplanner.core.api.score.stream.uni.UniConstraintCollector;
 import org.optaplanner.core.api.score.stream.uni.UniConstraintStream;
@@ -46,39 +44,43 @@ public final class ConstraintCollectors {
     // ************************************************************************
 
     public static <A> UniConstraintCollector<A, ?, Integer> count() {
-        return countLong(Long::intValue);
+        return new DefaultUniConstraintCollector<>(
+                () -> new int[1],
+                (resultContainer, a) -> {
+                    resultContainer[0]++;
+                    return (() -> resultContainer[0]--);
+                },
+                resultContainer -> resultContainer[0]);
     }
 
     public static <A> UniConstraintCollector<A, ?, Long> countLong() {
-        return countLong(Functions.identity());
-    }
-
-    private static <A, B extends Number> UniConstraintCollector<A, ?, B> countLong(Function<Long, B> countConverter) {
         return new DefaultUniConstraintCollector<>(
-                LongAdder::new,
+                () -> new long[1],
                 (resultContainer, a) -> {
-                    resultContainer.increment();
-                    return (resultContainer::decrement);
+                    resultContainer[0]++;
+                    return (() -> resultContainer[0]--);
                 },
-                resultContainer -> countConverter.apply(resultContainer.longValue()));
+                resultContainer -> resultContainer[0]);
     }
 
     public static <A, B> BiConstraintCollector<A, B, ?, Integer> countBi() {
-        return countLongBi(Long::intValue);
+        return new DefaultBiConstraintCollector<>(
+                () -> new int[1],
+                (resultContainer, a, b) -> {
+                    resultContainer[0]++;
+                    return (() -> resultContainer[0]--);
+                },
+                resultContainer -> resultContainer[0]);
     }
 
     public static <A, B> BiConstraintCollector<A, B, ?, Long> countLongBi() {
-        return countLongBi(Functions.identity());
-    }
-
-    private static <A, B, C extends Number> BiConstraintCollector<A, B, ?, C> countLongBi(Function<Long, C> countConverter) {
         return new DefaultBiConstraintCollector<>(
-                LongAdder::new,
+                () -> new long[1],
                 (resultContainer, a, b) -> {
-                    resultContainer.increment();
-                    return (resultContainer::decrement);
+                    resultContainer[0]++;
+                    return (() -> resultContainer[0]--);
                 },
-                resultContainer-> countConverter.apply(resultContainer.longValue()));
+                resultContainer -> resultContainer[0]);
     }
 
     // ************************************************************************
@@ -86,42 +88,63 @@ public final class ConstraintCollectors {
     // ************************************************************************
 
     public static <A> UniConstraintCollector<A, ?, Integer> countDistinct(Function<A, ?> groupValueMapping) {
-        return ConstraintCollectors.countDistinctLong(groupValueMapping, Long::intValue);
-    }
-
-    public static <A> UniConstraintCollector<A, ?, Long> countDistinctLong(Function<A, ?> groupValueMapping) {
-        return countDistinctLong(groupValueMapping, l -> l);
-    }
-
-    private static <A, B extends Number> UniConstraintCollector<A, ?, B> countDistinctLong(Function<A, ?> groupValueMapping,
-            Function<Long, B> countConverter) {
         class CountDistinctResultContainer {
-            long count = 0L;
-            Map<Object, LongAdder> objectCountMap = new HashMap<>();
+            int count = 0;
+            Map<Object, int[]> objectCountMap = new HashMap<>();
         }
         return new DefaultUniConstraintCollector<>(
                 CountDistinctResultContainer::new,
                 (resultContainer, a) -> {
                     Object value = groupValueMapping.apply(a);
-                    LongAdder objectCount = resultContainer.objectCountMap.computeIfAbsent(value, k -> new LongAdder());
-                    if (objectCount.longValue() == 0L) {
+                    int[] objectCount = resultContainer.objectCountMap.computeIfAbsent(value, k -> new int[1]);
+                    if (objectCount[0] == 0) {
                         resultContainer.count++;
                     }
-                    objectCount.increment();
+                    objectCount[0]++;
                     return (() -> {
-                        LongAdder objectCount2 = resultContainer.objectCountMap.get(value);
+                        int[] objectCount2 = resultContainer.objectCountMap.get(value);
                         if (objectCount2 == null) {
                             throw new IllegalStateException("Impossible state: the value (" + value
                                     + ") of A (" + a + ") is removed more times than it was added.");
                         }
-                        objectCount2.decrement();
-                        if (objectCount2.longValue() == 0L) {
+                        objectCount2[0]--;
+                        if (objectCount2[0] == 0) {
                             resultContainer.objectCountMap.remove(value);
                             resultContainer.count--;
                         }
                     });
                 },
-                resultContainer -> countConverter.apply(resultContainer.count));
+                resultContainer -> resultContainer.count);
+    }
+
+    public static <A> UniConstraintCollector<A, ?, Long> countDistinctLong(Function<A, ?> groupValueMapping) {
+        class CountDistinctResultContainer {
+            long count = 0L;
+            Map<Object, long[]> objectCountMap = new HashMap<>();
+        }
+        return new DefaultUniConstraintCollector<>(
+                CountDistinctResultContainer::new,
+                (resultContainer, a) -> {
+                    Object value = groupValueMapping.apply(a);
+                    long[] objectCount = resultContainer.objectCountMap.computeIfAbsent(value, k -> new long[1]);
+                    if (objectCount[0] == 0L) {
+                        resultContainer.count++;
+                    }
+                    objectCount[0]++;
+                    return (() -> {
+                        long[] objectCount2 = resultContainer.objectCountMap.get(value);
+                        if (objectCount2 == null) {
+                            throw new IllegalStateException("Impossible state: the value (" + value
+                                    + ") of A (" + a + ") is removed more times than it was added.");
+                        }
+                        objectCount2[0]--;
+                        if (objectCount2[0] == 0L) {
+                            resultContainer.objectCountMap.remove(value);
+                            resultContainer.count--;
+                        }
+                    });
+                },
+                resultContainer -> resultContainer.count);
     }
 
     // ************************************************************************
@@ -130,46 +153,46 @@ public final class ConstraintCollectors {
 
     public static <A> UniConstraintCollector<A, ?, Integer> sum(ToIntFunction<? super A> groupValueMapping) {
         return new DefaultUniConstraintCollector<>(
-                LongAdder::new,
+                () -> new int[1],
                 (resultContainer, a) -> {
                     int value = groupValueMapping.applyAsInt(a);
-                    resultContainer.add(value);
-                    return (() -> resultContainer.add(-value));
+                    resultContainer[0] += value;
+                    return (() -> resultContainer[0] -= value);
                 },
-                LongAdder::intValue);
+                resultContainer -> resultContainer[0]);
     }
 
     public static <A> UniConstraintCollector<A, ?, Long> sumLong(ToLongFunction<? super A> groupValueMapping) {
         return new DefaultUniConstraintCollector<>(
-                LongAdder::new,
+                () -> new long[1],
                 (resultContainer, a) -> {
                     long value = groupValueMapping.applyAsLong(a);
-                    resultContainer.add(value);
-                    return (() -> resultContainer.add(-value));
+                    resultContainer[0] += value;
+                    return (() -> resultContainer[0] -= value);
                 },
-                LongAdder::longValue);
+                resultContainer -> resultContainer[0]);
     }
 
     public static <A, B> BiConstraintCollector<A, B, ?, Integer> sum(ToIntBiFunction<? super A, ? super B> groupValueMapping) {
         return new DefaultBiConstraintCollector<>(
-                LongAdder::new,
+                () -> new int[1],
                 (resultContainer, a, b) -> {
                     int value = groupValueMapping.applyAsInt(a, b);
-                    resultContainer.add(value);
-                    return (() -> resultContainer.add(-value));
+                    resultContainer[0] += value;
+                    return (() -> resultContainer[0] -= value);
                 },
-                LongAdder::intValue);
+                resultContainer -> resultContainer[0]);
     }
 
     public static <A, B> BiConstraintCollector<A, B, ?, Long> sumLong(ToLongBiFunction<? super A, ? super B> groupValueMapping) {
         return new DefaultBiConstraintCollector<>(
-                LongAdder::new,
+                () -> new long[1],
                 (resultContainer, a, b) -> {
                     long value = groupValueMapping.applyAsLong(a, b);
-                    resultContainer.add(value);
-                    return (() -> resultContainer.add(-value));
+                    resultContainer[0] += value;
+                    return (() -> resultContainer[0] -= value);
                 },
-                LongAdder::longValue);
+                resultContainer -> resultContainer[0]);
     }
 
     // ************************************************************************
