f4ee0fb4e2cbb7ef1fc8aeda23276915aa506e50
Lukáš Petrovický <lukas@petrovicky.net>
2019-10-02 14:48:25 +0200

    PLANNER-1670 Add copyrights

--- optaplanner-core/src/main/java/org/optaplanner/core/impl/score/stream/drools/bi/DroolsFilterBiConstraintStream.java
+++ optaplanner-core/src/main/java/org/optaplanner/core/impl/score/stream/drools/bi/DroolsFilterBiConstraintStream.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2019 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.optaplanner.core.impl.score.stream.drools.bi;
 
 import java.util.function.BiPredicate;
@@ -40,7 +56,6 @@ public class DroolsFilterBiConstraintStream<Solution_, A, B> extends DroolsAbstr
 
     @Override
     public String toString() {
-        return "BiFilter() with " + childStreamList.size()  + " children";
+        return "BiFilter() with " + childStreamList.size() + " children";
     }
-
 }
