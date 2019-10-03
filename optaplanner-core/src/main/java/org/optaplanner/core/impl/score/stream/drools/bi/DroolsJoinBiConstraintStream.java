67026200f62b0a01620c748a6360279bc25c062d
Lukáš Petrovický <lukas@petrovicky.net>
2019-10-03 15:35:11 +0200

    PLANNER-1670 Improve performance of joins

--- optaplanner-core/src/main/java/org/optaplanner/core/impl/score/stream/drools/bi/DroolsJoinBiConstraintStream.java
+++ optaplanner-core/src/main/java/org/optaplanner/core/impl/score/stream/drools/bi/DroolsJoinBiConstraintStream.java
@@ -16,6 +16,10 @@
 package org.optaplanner.core.impl.score.stream.drools.bi;
 
 import java.util.Collection;
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.function.BiPredicate;
+import java.util.stream.Stream;
 
 import org.drools.model.Declaration;
 import org.drools.model.PatternDSL;
@@ -31,6 +35,8 @@ public class DroolsJoinBiConstraintStream<Solution_, A, B> extends DroolsAbstrac
     private final DroolsAbstractUniConstraintStream<Solution_, B> rightParentStream;
     private final AbstractBiJoiner<A, B> biJoiner;
     private final PatternDSL.PatternDef<B> rightPattern;
+    private final Map<JoinerType, BiPredicate<Object, Object>> joinerTypeToMatcherMap =
+            new EnumMap<>(JoinerType.class);
 
     public DroolsJoinBiConstraintStream(DroolsConstraintFactory<Solution_> constraintFactory,
             DroolsAbstractUniConstraintStream<Solution_, A> parent,
@@ -72,69 +78,69 @@ public class DroolsJoinBiConstraintStream<Solution_, A, B> extends DroolsAbstrac
         return rightPattern;
     }
 
-    private static boolean matches(Object left, Object right, JoinerType type) {
+    private boolean matches(A left, B right) {
+        Object[] leftMappings = biJoiner.getLeftCombinedMapping().apply(left);
+        Object[] rightMappings = biJoiner.getRightCombinedMapping().apply(right);
+        BiPredicate[] matchers = Stream.of(biJoiner.getJoinerTypes())
+                .map(joinerType -> joinerTypeToMatcherMap.computeIfAbsent(joinerType,
+                        DroolsJoinBiConstraintStream::createMatcher))
+                .toArray(BiPredicate[]::new);
+        for (int i = 0; i < matchers.length; i++) {
+            BiPredicate matcher = matchers[i];
+            if (!matcher.test(leftMappings[i], rightMappings[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static BiPredicate<Object, Object> createMatcher(JoinerType type) {
         switch (type) {
             case EQUAL:
-                return left.equals(right);
+                return Object::equals;
             case LESS_THAN:
+                return (a, b) -> lessThan((Comparable) a, b);
             case LESS_THAN_OR_EQUAL:
+                return (a, b) -> lessThanOrEqual((Comparable) a, b);
             case GREATER_THAN:
+                return (a, b) -> greaterThan((Comparable) a, b);
             case GREATER_THAN_OR_EQUAL:
-                return matchesComparable((Comparable)left, right, type);
+                return (a, b) -> greaterThanOrEqual((Comparable) a, b);
             case CONTAINING:
-                return ((Collection) left).contains(right);
+                return (a, b) -> ((Collection) a).contains(b);
             case DISJOINT:
+                return (a, b) -> disjointColections((Collection) a, (Collection) b);
             case INTERSECTING:
-                return matchesCollection((Collection)left, (Collection) right, type);
+                return (a, b) -> intersectingCollections((Collection) a, (Collection) b);
             default:
                 throw new IllegalStateException("Unsupported joiner type (" + type + ").");
         }
     }
 
-    private static boolean matchesComparable(Comparable left, Object right, JoinerType joinerType) {
-        int comparison = left.compareTo(right);
-        switch (joinerType) {
-            case LESS_THAN:
-                return comparison < 0;
-            case LESS_THAN_OR_EQUAL:
-                return comparison <= 0;
-            case GREATER_THAN:
-                return comparison > 0;
-            case GREATER_THAN_OR_EQUAL:
-                return comparison >= 0;
-            default:
-                throw new IllegalStateException("Unsupported joiner type (" + joinerType + ").");
-        }
+    private static boolean lessThan(Comparable left, Object right) {
+        return left.compareTo(right) < 0;
     }
 
-    private static boolean matchesCollection(Collection leftCollection, Collection rightCollection,
-            JoinerType joinerType) {
-        switch (joinerType) {
-            case DISJOINT:
-                return leftCollection.stream().noneMatch(rightCollection::contains) &&
-                        rightCollection.stream().noneMatch(leftCollection::contains);
-            case INTERSECTING:
-                return leftCollection.stream().anyMatch(rightCollection::contains) ||
-                        rightCollection.stream().anyMatch(leftCollection::contains);
-            default:
-                throw new IllegalStateException("Unsupported joiner type (" + joinerType + ").");
-        }
+    private static boolean lessThanOrEqual(Comparable left, Object right) {
+        return left.compareTo(right) <= 0;
     }
 
-    private boolean matches(A left, B right) {
-        Object[] leftMappings = biJoiner.getLeftCombinedMapping().apply(left);
-        Object[] rightMappings = biJoiner.getRightCombinedMapping().apply(right);
-        JoinerType[] joinerTypes = biJoiner.getJoinerTypes();
-        for (int i = 0; i < leftMappings.length; i++) {
-            Object leftMapping = leftMappings[i];
-            Object rightMapping = rightMappings[i];
-            JoinerType joinerType = joinerTypes[i];
-            boolean matches = matches(leftMapping, rightMapping, joinerType);
-            if (!matches) {
-                return false;
-            }
-        }
-        return true;
+    private static boolean greaterThan(Comparable left, Object right) {
+        return left.compareTo(right) > 0;
+    }
+
+    private static boolean greaterThanOrEqual(Comparable left, Object right) {
+        return left.compareTo(right) >= 0;
+    }
+
+    private static boolean disjointColections(Collection leftCollection, Collection rightCollection) {
+        return leftCollection.stream().noneMatch(rightCollection::contains) &&
+                rightCollection.stream().noneMatch(leftCollection::contains);
+    }
+
+    private static boolean intersectingCollections(Collection leftCollection, Collection rightCollection) {
+        return leftCollection.stream().anyMatch(rightCollection::contains) ||
+                rightCollection.stream().anyMatch(leftCollection::contains);
     }
 
     @Override
