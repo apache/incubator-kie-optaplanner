17f20a534e0f1ddc982a909bc2432433c9d6bb70
Lukáš Petrovický <lpetrovi@redhat.com>
2019-12-12 09:38:34 +0100

    Address code review comments

--- optaplanner-core/src/main/java/org/optaplanner/core/config/localsearch/decider/acceptor/AcceptorConfig.java
+++ optaplanner-core/src/main/java/org/optaplanner/core/config/localsearch/decider/acceptor/AcceptorConfig.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010 Red Hat, Inc. and/or its affiliates.
+ * Copyright 2019 Red Hat, Inc. and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -65,7 +65,9 @@ public class AcceptorConfig extends AbstractConfig<AcceptorConfig> {
     protected Integer fadingMoveTabuSize = null;
     protected Integer undoMoveTabuSize = null;
     protected Integer fadingUndoMoveTabuSize = null;
+    @Deprecated
     protected Integer solutionTabuSize = null;
+    @Deprecated
     protected Integer fadingSolutionTabuSize = null;
 
     protected String simulatedAnnealingStartingTemperature = null;
