f7d8b88e368c3520257a7e1797a4b7dbd8707041
Lukáš Petrovický <lpetrovi@redhat.com>
2020-01-20 10:25:41 +0100

    PLANNER-1787 Add test

--- optaplanner-core/src/test/java/org/optaplanner/core/api/score/constraint/ConstraintMatchTest.java
+++ optaplanner-core/src/test/java/org/optaplanner/core/api/score/constraint/ConstraintMatchTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 Red Hat, Inc. and/or its affiliates.
+ * Copyright 2020 Red Hat, Inc. and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,10 +17,14 @@
 package org.optaplanner.core.api.score.constraint;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 
 import org.junit.Test;
 import org.optaplanner.core.api.score.buildin.simple.SimpleScore;
 import org.optaplanner.core.impl.testdata.util.PlannerAssert;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ConstraintMatchTest {
 
@@ -68,6 +72,13 @@ public class ConstraintMatchTest {
                 new ConstraintMatch("a.c", "a", Arrays.asList("a", "ab"), SimpleScore.ZERO),
                 new ConstraintMatch("a.c", "a", Arrays.asList("a", "c"), SimpleScore.ZERO)
         );
+    }
+
+    @Test
+    public void listIsModifiable() { // The list is sorted externally in AbstractScoreDirector.
+        ConstraintMatch match = new ConstraintMatch("a.b", "a", Arrays.asList("b", "a"), SimpleScore.ZERO);
+        Collections.sort(match.getJustificationList(), Comparator.comparing(a -> ((String) a)));
+        assertThat(match.getJustificationList()).containsExactly("a", "b");
     }
 
 }
