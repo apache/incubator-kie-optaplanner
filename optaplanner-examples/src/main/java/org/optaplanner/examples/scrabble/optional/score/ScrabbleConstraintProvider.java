f41070583c35bac44a83f6640697cb999814c8f1
Christopher Chianelli <chris.chianelli@gmail.com>
2020-02-10 15:54:52 -0500

    Fix penalty for "Character Conflict" and reward for "Maximize merges per word"; optimize parallel constraints.

--- optaplanner-examples/src/main/java/org/optaplanner/examples/scrabble/optional/score/ScrabbleConstraintProvider.java
+++ optaplanner-examples/src/main/java/org/optaplanner/examples/scrabble/optional/score/ScrabbleConstraintProvider.java
@@ -5,7 +5,7 @@ import org.optaplanner.core.api.score.stream.Constraint;
 import org.optaplanner.core.api.score.stream.ConstraintCollectors;
 import org.optaplanner.core.api.score.stream.ConstraintFactory;
 import org.optaplanner.core.api.score.stream.ConstraintProvider;
-import org.optaplanner.core.impl.score.stream.bi.FilteringBiJoiner;
+import org.optaplanner.core.api.score.stream.Joiners;
 import org.optaplanner.examples.scrabble.domain.ScrabbleWordDirection;
 import org.optaplanner.examples.scrabble.domain.ScrabbleCell;
 import org.optaplanner.examples.scrabble.domain.ScrabbleWordAssignment;
@@ -27,24 +27,24 @@ public class ScrabbleConstraintProvider implements ConstraintProvider {
     private Constraint characterConflict(ConstraintFactory cf) {
         return cf.from(ScrabbleCell.class)
                  .filter(sc -> sc.getCharacterSet().size() >= 2)
-                 .penalize("Character confict", HardMediumSoftScore.ONE_HARD, sc -> 1 - sc.getCharacterSet().size());
+                 .penalize("Character confict", HardMediumSoftScore.ONE_HARD, sc -> sc.getCharacterSet().size() - 1);
     }
 
     private Constraint noParallelHorizontalNeighbours(ConstraintFactory cf) {
-        return cf.fromUniquePair(ScrabbleCell.class)
-                 .filter((c1, c2) -> c1.getX() == c2.getX() &&
-                                     Math.abs(c1.getY() - c2.getY()) == 1 &&
-                                     c1.hasWordSet(ScrabbleWordDirection.HORIZONTAL) &&
-                                     c2.hasWordSet(ScrabbleWordDirection.HORIZONTAL))
+        return cf.from(ScrabbleCell.class).filter(sc -> sc.hasWordSet(ScrabbleWordDirection.HORIZONTAL))
+                 .join(ScrabbleCell.class,
+                       Joiners.equal(ScrabbleCell::getX), Joiners.lessThan(ScrabbleCell::getId),
+                       Joiners.filtering((first, other) -> other.hasWordSet(ScrabbleWordDirection.HORIZONTAL)))
+                 .filter((c1, c2) -> Math.abs(c1.getY() - c2.getY()) == 1)
                  .penalize("No parallel horizontal neighbours", HardMediumSoftScore.ONE_HARD);
     }
 
     private Constraint noParallelVerticalNeighbours(ConstraintFactory cf) {
-        return cf.fromUniquePair(ScrabbleCell.class)
-                 .filter((c1, c2) -> c1.getY() == c2.getY() &&
-                                     Math.abs(c1.getX() - c2.getX()) == 1 &&
-                                     c1.hasWordSet(ScrabbleWordDirection.VERTICAL) &&
-                                     c2.hasWordSet(ScrabbleWordDirection.VERTICAL))
+        return cf.from(ScrabbleCell.class).filter(sc -> sc.hasWordSet(ScrabbleWordDirection.VERTICAL))
+                 .join(ScrabbleCell.class,
+                       Joiners.equal(ScrabbleCell::getY), Joiners.lessThan(ScrabbleCell::getId),
+                       Joiners.filtering((first, other) -> other.hasWordSet(ScrabbleWordDirection.VERTICAL)))
+                 .filter((c1, c2) -> Math.abs(c1.getX() - c2.getX()) == 1)
                  .penalize("No parallel vertical neighbours", HardMediumSoftScore.ONE_HARD);
     }
 
@@ -56,9 +56,9 @@ public class ScrabbleConstraintProvider implements ConstraintProvider {
 
     private Constraint maximizeMergesPerWord(ConstraintFactory cf) {
         return cf.from(ScrabbleWordAssignment.class)
-                 .join(ScrabbleCell.class, new FilteringBiJoiner<>((swa, sc) -> sc.getWordSet().contains(swa) && sc.hasMerge()))
+                 .join(ScrabbleCell.class, Joiners.filtering((swa, sc) -> sc.getWordSet().contains(swa) && sc.hasMerge()))
                  .groupBy((swa, sc) -> swa.getId(), ConstraintCollectors.countBi())
-                 .penalize("Maximize merges per word", HardMediumSoftScore.ONE_MEDIUM, (id, count) -> count * count);
+                 .reward("Maximize merges per word", HardMediumSoftScore.ONE_MEDIUM, (id, count) -> count * count);
     }
 
     private Constraint pullToCenter(ConstraintFactory cf) {
