131ced03b5e1f154cc0e214a64edd290ada1a23c
Lukáš Petrovický <lukas@petrovicky.net>
2020-03-26 09:11:10 +0100

    Test all constraints

--- optaplanner-examples/src/test/java/org/optaplanner/examples/nqueens/solver/score/NQueensConstraintProviderTest.java
+++ optaplanner-examples/src/test/java/org/optaplanner/examples/nqueens/solver/score/NQueensConstraintProviderTest.java
@@ -29,35 +29,91 @@ public class NQueensConstraintProviderTest {
     private final ConstraintVerifier<NQueens> constraintVerifier =
             ConstraintVerifier.createFor(NQueens.class, Queen.class);
 
-    private final Row row = new Row(0);
+    private final Row row1 = new Row(0);
+    private final Row row2 = new Row(1);
+    private final Row row3 = new Row(2);
     private final Column column1 = new Column(0);
     private final Column column2 = new Column(1);
     private final Column column3 = new Column(2);
 
     @Test
     public void noHorizontalConflictWithOneQueen() {
-        Queen queen1 = new Queen(0, row, column1);
+        Queen queen1 = new Queen(0, row1, column1);
         constraintVerifier.forConstraint(constraintProvider::horizontalConflict)
-                .given(queen1, row, column1)
+                .given(queen1)
                 .expectNoImpact();
     }
 
     @Test
     public void horizontalConflictWithTwoQueens() {
-        Queen queen1 = new Queen(0, row, column1);
-        Queen queen2 = new Queen(1, row, column2);
+        Queen queen1 = new Queen(0, row1, column1);
+        Queen queen2 = new Queen(1, row1, column2);
         constraintVerifier.forConstraint(constraintProvider::horizontalConflict)
-                .given(queen1, queen2, row, column1, column2)
-                .expectReward(1, "One pair of queens on the same row.");
+                .given(queen1, queen2)
+                .expectReward(1);
     }
 
     @Test
     public void horizontalConflictWithThreeQueens() {
-        Queen queen1 = new Queen(0, row, column1);
-        Queen queen2 = new Queen(1, row, column2);
-        Queen queen3 = new Queen(2, row, column3);
+        Queen queen1 = new Queen(0, row1, column1);
+        Queen queen2 = new Queen(1, row1, column2);
+        Queen queen3 = new Queen(2, row1, column3);
         constraintVerifier.forConstraint(constraintProvider::horizontalConflict)
-                .given(queen1, queen2, queen3, row, column1, column2, column3)
+                .given(queen1, queen2, queen3)
+                .expectReward(3);
+    }
+
+    @Test
+    public void noAscendingDiagonalConflictWithOneQueen() {
+        Queen queen1 = new Queen(0, row1, column1);
+        constraintVerifier.forConstraint(constraintProvider::ascendingDiagonalConflict)
+                .given(queen1)
+                .expectNoImpact();
+    }
+
+    @Test
+    public void ascendingDiagonalConflictWithTwoQueens() {
+        Queen queen1 = new Queen(0, row1, column2);
+        Queen queen2 = new Queen(1, row2, column1);
+        constraintVerifier.forConstraint(constraintProvider::ascendingDiagonalConflict)
+                .given(queen1, queen2)
+                .expectReward(1);
+    }
+
+    @Test
+    public void ascendingDiagonalConflictWithThreeQueens() {
+        Queen queen1 = new Queen(0, row1, column3);
+        Queen queen2 = new Queen(1, row2, column2);
+        Queen queen3 = new Queen(2, row3, column1);
+        constraintVerifier.forConstraint(constraintProvider::ascendingDiagonalConflict)
+                .given(queen1, queen2, queen3)
+                .expectReward(3);
+    }
+
+    @Test
+    public void noDescendingDiagonalConflictWithOneQueen() {
+        Queen queen1 = new Queen(0, row1, column1);
+        constraintVerifier.forConstraint(constraintProvider::descendingDiagonalConflict)
+                .given(queen1)
+                .expectNoImpact();
+    }
+
+    @Test
+    public void descendingDiagonalConflictWithTwoQueens() {
+        Queen queen1 = new Queen(0, row1, column1);
+        Queen queen2 = new Queen(1, row2, column2);
+        constraintVerifier.forConstraint(constraintProvider::descendingDiagonalConflict)
+                .given(queen1, queen2)
+                .expectReward(1);
+    }
+
+    @Test
+    public void descendingDiagonalConflictWithThreeQueens() {
+        Queen queen1 = new Queen(0, row1, column1);
+        Queen queen2 = new Queen(1, row2, column2);
+        Queen queen3 = new Queen(2, row3, column3);
+        constraintVerifier.forConstraint(constraintProvider::descendingDiagonalConflict)
+                .given(queen1, queen2, queen3)
                 .expectReward(3);
     }
 }
