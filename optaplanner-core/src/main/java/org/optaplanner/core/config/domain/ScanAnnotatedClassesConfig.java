fd7fe1cae551027a4569e40b3ea2fb3f33047801
Lukáš Petrovický <lukas@petrovicky.net>
2020-05-19 09:29:37 +0200

    Scanning annotated classes has been deprecated

--- optaplanner-core/src/main/java/org/optaplanner/core/config/domain/ScanAnnotatedClassesConfig.java
+++ optaplanner-core/src/main/java/org/optaplanner/core/config/domain/ScanAnnotatedClassesConfig.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 Red Hat, Inc. and/or its affiliates.
+ * Copyright 2020 Red Hat, Inc. and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -37,7 +37,11 @@ import org.reflections.util.FilterBuilder;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamImplicit;
 
+/**
+ * @deprecated This functionality will be removed in the next major version of OptaPlanner.
+ */
 @XStreamAlias("scanAnnotatedClasses")
+@Deprecated(/* forRemoval = true */)
 public class ScanAnnotatedClassesConfig extends AbstractConfig<ScanAnnotatedClassesConfig> {
 
     @XStreamImplicit(itemFieldName = "packageInclude")
