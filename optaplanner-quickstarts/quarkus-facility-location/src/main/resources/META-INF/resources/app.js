c31d26919d8d5f9ab321c5f0dd0e5246afaa5295
Jiří Locker <jiri.locker@gmail.com>
2020-06-22 19:30:00 +0200

    Show problem

--- optaplanner-quickstarts/quarkus-facility-location/src/main/resources/META-INF/resources/app.js
+++ optaplanner-quickstarts/quarkus-facility-location/src/main/resources/META-INF/resources/app.js
@@ -1,7 +1,16 @@
 const get = () => {
   fetch('http://localhost:8080/flp/get')
     .then(response => response.json())
-    .then(data => map.fitBounds(data.bounds));
+    .then(data => showProblem(data));
+};
+
+const showProblem = (problem) => {
+  map.fitBounds(problem.bounds);
+  problem.facilities.forEach(facility => L.marker(facility.location).addTo(map));
+  problem.demandPoints.forEach(dp => L.circleMarker(dp.location).addTo(map));
+  problem.demandPoints
+    .filter(dp => dp.facility !== null)
+    .forEach(dp => L.polyline([dp.location, dp.facility.location]).addTo(map));
 };
 
 const map = L.map('map').setView([51.505, -0.09], 13);
