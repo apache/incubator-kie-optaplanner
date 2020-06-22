7740ce19ffdfe6a9adaba197bb641669c4c94d21
Jiří Locker <jiri.locker@gmail.com>
2020-06-22 17:52:39 +0200

    Add index page with Leaflet map

---
+++ optaplanner-quickstarts/quarkus-facility-location/src/main/resources/META-INF/resources/app.js
@@ -0,0 +1,6 @@
+const map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+}).addTo(map);
