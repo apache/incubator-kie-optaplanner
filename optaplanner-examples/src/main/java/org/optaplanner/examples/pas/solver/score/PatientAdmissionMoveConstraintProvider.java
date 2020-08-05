96a8dfb82c31693e0986b69789326dfed7ec8458
Anna Dupliak <adupliak@redhat.com>
2020-08-05 13:47:18 +0200

    Implement sameBedInSameNightConstraint rule

--- optaplanner-examples/src/main/java/org/optaplanner/examples/pas/solver/score/PatientAdmissionMoveConstraintProvider.java
+++ optaplanner-examples/src/main/java/org/optaplanner/examples/pas/solver/score/PatientAdmissionMoveConstraintProvider.java
@@ -1,5 +1,8 @@
 package org.optaplanner.examples.pas.solver.score;
 
+import static org.optaplanner.core.api.score.stream.Joiners.filtering;
+import static org.optaplanner.core.api.score.stream.Joiners.lessThan;
+
 import org.optaplanner.core.api.score.buildin.hardmediumsoft.HardMediumSoftScore;
 import org.optaplanner.core.api.score.stream.Constraint;
 import org.optaplanner.core.api.score.stream.ConstraintFactory;
@@ -13,14 +16,36 @@ public class PatientAdmissionMoveConstraintProvider implements ConstraintProvide
     @Override
     public Constraint[] defineConstraints(ConstraintFactory constraintFactory) {
         return new Constraint[] {
-                femaleInMaleRoom(constraintFactory)
+                femaleInMaleRoomConstraint(constraintFactory),
+                maleInFemaleRoomConstraint(constraintFactory),
+                sameBedInSameNightConstraint(constraintFactory)
         };
     }
 
-    public Constraint femaleInMaleRoom(ConstraintFactory constraintFactory) {
+    public Constraint femaleInMaleRoomConstraint(ConstraintFactory constraintFactory) {
         return constraintFactory.from(BedDesignation.class)
                 .filter(bd -> bd.getPatientGender() == Gender.FEMALE
                         && bd.getRoomGenderLimitation() == GenderLimitation.MALE_ONLY)
                 .penalize("femaleInMaleRoom", HardMediumSoftScore.ofHard(50), BedDesignation::getAdmissionPartNightCount);
+    }
+
+    public Constraint maleInFemaleRoomConstraint(ConstraintFactory constraintFactory) {
+        return constraintFactory.from(BedDesignation.class)
+                .filter(bd -> bd.getPatientGender() == Gender.MALE
+                        && bd.getRoomGenderLimitation() == GenderLimitation.FEMALE_ONLY)
+                .penalize("maleInFemaleRoom", HardMediumSoftScore.ofHard(50), BedDesignation::getAdmissionPartNightCount);
+    }
+
+    public Constraint sameBedInSameNightConstraint(ConstraintFactory constraintFactory) {
+        return constraintFactory.from(BedDesignation.class)
+                .join(BedDesignation.class,
+                        lessThan(BedDesignation::getId),
+                        filtering((leftBd,
+                                rightBd) -> leftBd.getAdmissionPart().calculateSameNightCount(rightBd.getAdmissionPart()) > 0),
+                        filtering((leftBd,
+                                rightBd) -> leftBd.getBed() != null && rightBd.getBed() != null))
+                .filter((leftBd, leftBd2) -> leftBd.getBed().getId().equals(leftBd2.getBed().getId()))
+                .penalize("sameBedInSameNight", HardMediumSoftScore.ofHard(1000),
+                        (leftBd, rightBd) -> leftBd.getAdmissionPart().calculateSameNightCount(rightBd.getAdmissionPart()));
     }
 }
