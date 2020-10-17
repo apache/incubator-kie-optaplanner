5429ef37fb5bdb0b5f3ec983f67e05aafd7073f4
Lukáš Petrovický <lukas@petrovicky.net>
2020-10-17 10:06:42 +0200

    Remove MVEL

--- optaplanner-core/src/test/resources/org/optaplanner/core/impl/score/director/drools/testgen/TestGenWriterOutput.java
+++ optaplanner-core/src/test/resources/org/optaplanner/core/impl/score/director/drools/testgen/TestGenWriterOutput.java
@@ -1,6 +1,23 @@
+/*
+ * Copyright 2020 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.optaplanner.testgen;
 
 import java.io.File;
+import org.drools.modelcompiler.ExecutableModelProject;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.kie.api.KieServices;
@@ -24,7 +41,7 @@ public class TestGenWriterOutput {
                 .newClassPathResource("x"));
         kfs.write(kieServices.getResources()
                 .newClassPathResource("y"));
-        kieServices.newKieBuilder(kfs).buildAll();
+        kieServices.newKieBuilder(kfs).buildAll(ExecutableModelProject.class);
         KieContainer kieContainer = kieServices.newKieContainer(kieServices.getRepository().getDefaultReleaseId());
         KieSession kieSession = kieContainer.newKieSession();
 
