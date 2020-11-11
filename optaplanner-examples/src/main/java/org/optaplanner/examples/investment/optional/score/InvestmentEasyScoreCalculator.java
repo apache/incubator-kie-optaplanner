453eac41009ee240ec92c320348e3d65e39460e0
Lukáš Petrovický <lukas@petrovicky.net>
2020-11-11 09:39:09 +0100

    investment

--- optaplanner-examples/src/main/java/org/optaplanner/examples/investment/optional/score/InvestmentEasyScoreCalculator.java
+++ optaplanner-examples/src/main/java/org/optaplanner/examples/investment/optional/score/InvestmentEasyScoreCalculator.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.optaplanner.examples.investment.solver.score;
+package org.optaplanner.examples.investment.optional.score;
 
 import java.util.Map;
 
