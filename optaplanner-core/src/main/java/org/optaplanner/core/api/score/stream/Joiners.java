feb3988247913960c670770b055b105787f4983b
Lukáš Petrovický <lukas@petrovicky.net>
2021-02-01 12:44:57 +0100

    Add examples

--- optaplanner-core/src/main/java/org/optaplanner/core/api/score/stream/Joiners.java
+++ optaplanner-core/src/main/java/org/optaplanner/core/api/score/stream/Joiners.java
@@ -16,6 +16,10 @@
 
 package org.optaplanner.core.api.score.stream;
 
+import java.util.function.BiFunction;
+import java.util.function.BiPredicate;
+import java.util.function.Function;
+
 import org.optaplanner.core.api.function.PentaPredicate;
 import org.optaplanner.core.api.function.QuadFunction;
 import org.optaplanner.core.api.function.QuadPredicate;
@@ -40,10 +44,6 @@ import org.optaplanner.core.impl.score.stream.quad.SingleQuadJoiner;
 import org.optaplanner.core.impl.score.stream.tri.AbstractTriJoiner;
 import org.optaplanner.core.impl.score.stream.tri.FilteringTriJoiner;
 import org.optaplanner.core.impl.score.stream.tri.SingleTriJoiner;
-
-import java.util.function.BiFunction;
-import java.util.function.BiPredicate;
-import java.util.function.Function;
 
 /**
  * Creates an {@link BiJoiner}, {@link TriJoiner}, ... instance
@@ -85,7 +85,10 @@ public final class Joiners {
      * Joins every A and B that share a property.
      * These are exactly the pairs where {@code leftMapping.apply(A).equals(rightMapping.apply(B))}.
      *
-     * @param <A> the type of object on the left
+     * For example, on a cartesian product of list {@code [Ann(age = 20), Beth(age = 25), Eric(age = 20)]}
+     * with both leftMapping and rightMapping being {@code Person::getAge},
+     * this joiner will produce pairs {@code (Ann, Ann), (Ann, Eric), (Beth, Beth), (Eric, Ann), (Eric, Eric)}.
+     *
      * @param <B> the type of object on the right
      * @param <Property_> the type of the property to compare
      * @param leftMapping mapping function to apply to A
@@ -99,9 +102,7 @@ public final class Joiners {
     }
 
     /**
-     * Joins every A and B where a value of property on A is less than the value of the same property on B.
-     * These are exactly the pairs where {@code mapping.apply(A).compareTo(mapping.apply(B)) < 0}.
-     * Delegates to {@link #lessThan(Function, Function)} with both arguments using the same mapping.
+     * As defined by {@link #lessThan(Function, Function)} with both arguments using the same mapping.
      *
      * @param mapping mapping function to apply
      * @param <A> the type of both objects
@@ -117,6 +118,10 @@ public final class Joiners {
      * Joins every A and B where a value of property on A is less than the value of a property on B.
      * These are exactly the pairs where {@code leftMapping.apply(A).compareTo(rightMapping.apply(B)) < 0}.
      *
+     * For example, on a cartesian product of list {@code [Ann(age = 20), Beth(age = 25), Eric(age = 20)]}
+     * with both leftMapping and rightMapping being {@code Person::getAge},
+     * this joiner will produce pairs {@code (Ann, Beth), (Eric, Beth)}.
+     *
      * @param leftMapping mapping function to apply to A
      * @param rightMapping mapping function to apply to B
      * @param <A> the type of object on the left
@@ -131,9 +136,7 @@ public final class Joiners {
     }
 
     /**
-     * Joins every A and B where a value of property on A is less than or equal to the value of the same property on B.
-     * These are exactly the pairs where {@code mapping.apply(A).compareTo(mapping.apply(B)) <= 0}.
-     * Delegates to {@link #lessThanOrEqual(Function, Function)} with both arguments using the same mapping.
+     * As defined by {@link #lessThanOrEqual(Function, Function)} with both arguments using the same mapping.
      *
      * @param mapping mapping function to apply
      * @param <A> the type of both objects
@@ -150,6 +153,11 @@ public final class Joiners {
      * Joins every A and B where a value of property on A is less than or equal to the value of a property on B.
      * These are exactly the pairs where {@code leftMapping.apply(A).compareTo(rightMapping.apply(B)) <= 0}.
      *
+     * For example, on a cartesian product of list {@code [Ann(age = 20), Beth(age = 25), Eric(age = 20)]}
+     * with both leftMapping and rightMapping being {@code Person::getAge},
+     * this joiner will produce pairs
+     * {@code (Ann, Ann), (Ann, Beth), (Ann, Eric), (Beth, Beth), (Eric, Ann), (Eric, Beth), (Eric, Eric)}.
+     *
      * @param leftMapping mapping function to apply to A
      * @param rightMapping mapping function to apply to B
      * @param <A> the type of object on the left
@@ -164,9 +172,7 @@ public final class Joiners {
     }
 
     /**
-     * Joins every A and B where a value of property on A is greater than the value of the same property on B.
-     * These are exactly the pairs where {@code mapping.apply(A).compareTo(mapping.apply(B)) > 0}.
-     * Delegates to {@link #greaterThan(Function, Function)} with both arguments using the same mapping.
+     * As defined by {@link #greaterThan(Function, Function)} with both arguments using the same mapping.
      *
      * @param mapping mapping function to apply
      * @param <A> the type of both objects
@@ -183,6 +189,10 @@ public final class Joiners {
      * Joins every A and B where a value of property on A is greater than the value of a property on B.
      * These are exactly the pairs where {@code leftMapping.apply(A).compareTo(rightMapping.apply(B)) > 0}.
      *
+     * For example, on a cartesian product of list {@code [Ann(age = 20), Beth(age = 25), Eric(age = 20)]}
+     * with both leftMapping and rightMapping being {@code Person::getAge},
+     * this joiner will produce pairs {@code (Beth, Ann), (Beth, Eric)}.
+     *
      * @param leftMapping mapping function to apply to A
      * @param rightMapping mapping function to apply to B
      * @param <A> the type of object on the left
@@ -197,10 +207,7 @@ public final class Joiners {
     }
 
     /**
-     * Joins every A and B where a value of property on A is greater than or equal to the value of
-     * the same property on B.
-     * These are exactly the pairs where {@code mapping.apply(A).compareTo(mapping.apply(B)) >= 0}.
-     * Delegates to {@link #greaterThanOrEqual(Function, Function)} with both arguments using the same mapping.
+     * As defined by {@link #greaterThanOrEqual(Function, Function)} with both arguments using the same mapping.
      *
      * @param mapping mapping function to apply
      * @param <A> the type of both objects
@@ -216,6 +223,11 @@ public final class Joiners {
     /**
      * Joins every A and B where a value of property on A is greater than or equal to the value of a property on B.
      * These are exactly the pairs where {@code leftMapping.apply(A).compareTo(rightMapping.apply(B)) >= 0}.
+     *
+     * For example, on a cartesian product of list {@code [Ann(age = 20), Beth(age = 25), Eric(age = 20)]}
+     * with both leftMapping and rightMapping being {@code Person::getAge},
+     * this joiner will produce pairs
+     * {@code (Ann, Ann), (Ann, Eric), (Beth, Ann), (Beth, Beth), (Beth, Eric), (Eric, Ann), (Eric, Eric)}.
      *
      * @param leftMapping mapping function to apply to A
      * @param rightMapping mapping function to apply to B
@@ -233,6 +245,10 @@ public final class Joiners {
     /**
      * Applies a filter to the joined tuple, with the semantics of {@link BiConstraintStream#filter(BiPredicate)}.
      *
+     * For example, on a cartesian product of list {@code [Ann(age = 20), Beth(age = 25), Eric(age = 20)]}
+     * with filter being {@code age == 20},
+     * this joiner will produce pairs {@code (Ann, Ann), (Ann, Eric), (Eric, Ann), (Eric, Eric)}.
+     *
      * @param filter never null, filter to apply
      * @param <A> type of the first fact in the tuple
      * @param <B> type of the second fact in the tuple
@@ -245,6 +261,12 @@ public final class Joiners {
     /**
      * Joins every A and B that overlap for an interval which is specified by a start and end property on both A and B.
      * These are exactly the pairs where {@code A.start < B.end} and {@code A.end > B.start}.
+     *
+     * For example, on a cartesian product of list
+     * {@code [Ann(start=08:00, end=14:00), Beth(start=12:00, end=18:00), Eric(start=16:00, end=22:00)]}
+     * with startMapping being {@code Person::getStart} and endMapping being {@code Person::getEnd},
+     * this joiner will produce pairs
+     * {@code (Ann, Ann), (Ann, Beth), (Beth, Ann), (Beth, Beth), (Beth, Eric), (Eric, Beth), (Eric, Eric)}.
      *
      * @param startMapping maps the argument to the start point of its interval (inclusive)
      * @param endMapping maps the argument to the end point of its interval (exclusive)
