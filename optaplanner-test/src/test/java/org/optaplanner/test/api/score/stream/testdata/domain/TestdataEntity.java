01b838656783664cc8954759c78261a23926ae31
Lukáš Petrovický <lukas@petrovicky.net>
2021-02-16 09:37:18 +0100

    Add test coverage

---
+++ optaplanner-test/src/test/java/org/optaplanner/test/api/score/stream/testdata/domain/TestdataEntity.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2021 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.optaplanner.test.api.score.stream.testdata.domain;
+
+import org.optaplanner.core.api.domain.entity.PlanningEntity;
+import org.optaplanner.core.api.domain.variable.PlanningVariable;
+import org.optaplanner.core.impl.domain.entity.descriptor.EntityDescriptor;
+import org.optaplanner.core.impl.domain.solution.descriptor.SolutionDescriptor;
+import org.optaplanner.core.impl.domain.variable.descriptor.GenuineVariableDescriptor;
+
+@PlanningEntity
+public class TestdataEntity extends TestdataObject {
+
+    public static EntityDescriptor<TestdataSolution> buildEntityDescriptor() {
+        SolutionDescriptor<TestdataSolution> solutionDescriptor = TestdataSolution.buildSolutionDescriptor();
+        return solutionDescriptor.findEntityDescriptorOrFail(TestdataEntity.class);
+    }
+
+    public static GenuineVariableDescriptor<TestdataSolution> buildVariableDescriptorForValue() {
+        SolutionDescriptor<TestdataSolution> solutionDescriptor = TestdataSolution.buildSolutionDescriptor();
+        EntityDescriptor<TestdataSolution> entityDescriptor = solutionDescriptor
+                .findEntityDescriptorOrFail(TestdataEntity.class);
+        return entityDescriptor.getGenuineVariableDescriptor("value");
+    }
+
+    private TestdataValue value;
+
+    public TestdataEntity() {
+    }
+
+    public TestdataEntity(String code) {
+        super(code);
+    }
+
+    public TestdataEntity(String code, TestdataValue value) {
+        this(code);
+        this.value = value;
+    }
+
+    @PlanningVariable(valueRangeProviderRefs = "valueRange")
+    public TestdataValue getValue() {
+        return value;
+    }
+
+    public void setValue(TestdataValue value) {
+        this.value = value;
+    }
+
+    // ************************************************************************
+    // Complex methods
+    // ************************************************************************
+
+}
