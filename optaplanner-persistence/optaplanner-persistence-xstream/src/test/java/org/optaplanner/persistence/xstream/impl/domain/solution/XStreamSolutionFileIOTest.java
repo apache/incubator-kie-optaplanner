47b7afd9cdf491acbc1f7e458da8374d6defc193
Lukáš Petrovický <lukas@petrovicky.net>
2021-03-23 09:47:05 +0100

    Fix XStream failures

--- optaplanner-persistence/optaplanner-persistence-xstream/src/test/java/org/optaplanner/persistence/xstream/impl/domain/solution/XStreamSolutionFileIOTest.java
+++ optaplanner-persistence/optaplanner-persistence-xstream/src/test/java/org/optaplanner/persistence/xstream/impl/domain/solution/XStreamSolutionFileIOTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Red Hat, Inc. and/or its affiliates.
+ * Copyright 2021 Red Hat, Inc. and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,19 +16,20 @@
 
 package org.optaplanner.persistence.xstream.impl.domain.solution;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.optaplanner.core.impl.testdata.util.PlannerAssert.assertAllCodesOfIterator;
-import static org.optaplanner.core.impl.testdata.util.PlannerAssert.assertCode;
-
 import java.io.File;
 import java.util.Arrays;
-
+import java.util.List;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.optaplanner.core.api.score.buildin.simple.SimpleScore;
 import org.optaplanner.persistence.xstream.impl.testdata.domain.XStreamTestdataEntity;
 import org.optaplanner.persistence.xstream.impl.testdata.domain.XStreamTestdataSolution;
 import org.optaplanner.persistence.xstream.impl.testdata.domain.XStreamTestdataValue;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.optaplanner.core.impl.testdata.util.PlannerAssert.assertAllCodesOfIterator;
+import static org.optaplanner.core.impl.testdata.util.PlannerAssert.assertCode;
 
 public class XStreamSolutionFileIOTest {
 
@@ -48,9 +49,10 @@ public class XStreamSolutionFileIOTest {
 
         XStreamTestdataSolution original = new XStreamTestdataSolution("s1");
         XStreamTestdataValue originalV1 = new XStreamTestdataValue("v1");
-        original.setValueList(Arrays.asList(originalV1, new XStreamTestdataValue("v2")));
-        original.setEntityList(Arrays.asList(
-                new XStreamTestdataEntity("e1"), new XStreamTestdataEntity("e2", originalV1), new XStreamTestdataEntity("e3")));
+        original.setValueList(asList(originalV1, new XStreamTestdataValue("v2")));
+        original.setEntityList(
+                asList(new XStreamTestdataEntity("e1"), new XStreamTestdataEntity("e2", originalV1),
+                        new XStreamTestdataEntity("e3")));
         original.setScore(SimpleScore.of(-123));
         solutionFileIO.write(original, file);
         XStreamTestdataSolution copy = solutionFileIO.read(file);
@@ -64,6 +66,12 @@ public class XStreamSolutionFileIOTest {
         assertCode("v1", copyE2.getValue());
         assertThat(copyE2.getValue()).isSameAs(copyV1);
         assertThat(copy.getScore()).isEqualTo(SimpleScore.of(-123));
+    }
+
+    private static <X> List<X> asList(X... values) {
+        // XStream does illegal access to JDK internals if Arrays.asList(...) is used instead.
+        return Arrays.stream(values)
+                .collect(Collectors.toList());
     }
 
 }
