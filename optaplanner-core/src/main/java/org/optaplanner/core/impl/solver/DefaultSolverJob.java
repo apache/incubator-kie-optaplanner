b2e64238066dc3dfc9b1ddf9bd9279224e7f85fa
Lukáš Petrovický <lukas@petrovicky.net>
2021-03-30 18:35:58 +0200

    PLANNER-2364 Descriptive NPE
    Also a little bit of cleanup.

--- optaplanner-core/src/main/java/org/optaplanner/core/impl/solver/DefaultSolverJob.java
+++ optaplanner-core/src/main/java/org/optaplanner/core/impl/solver/DefaultSolverJob.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Red Hat, Inc. and/or its affiliates.
+ * Copyright 2021 Red Hat, Inc. and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -114,7 +114,7 @@ public final class DefaultSolverJob<Solution_, ProblemId_> implements SolverJob<
 
     private void solvingTerminated() {
         solverStatusReference.set(SolverStatus.NOT_SOLVING);
-        solverManager.getProblemIdToSolverJobMap().remove(problemId);
+        solverManager.unregisterSolverJob(problemId);
         terminatedLatch.countDown();
     }
 
