b612210059042ba31ca26221e254dbd9b2d7ac5b
Lukáš Petrovický <lukas@petrovicky.net>
2021-05-17 09:23:41 +0200

    Revert "Improve performance of csg"
    This reverts commit 5c418f919ef1bb0051dadd96cee4813239ac6868.

--- optaplanner-examples/src/main/java/org/optaplanner/examples/coachshuttlegathering/domain/solver/BusPassengerCountTotalUpdatingVariableListener.java
+++ optaplanner-examples/src/main/java/org/optaplanner/examples/coachshuttlegathering/domain/solver/BusPassengerCountTotalUpdatingVariableListener.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 Red Hat, Inc. and/or its affiliates.
+ * Copyright 2020 Red Hat, Inc. and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -69,11 +69,7 @@ public abstract class BusPassengerCountTotalUpdatingVariableListener
         if (!isCorrectBusInstance(bus)) {
             return;
         }
-        int passengerQuantity = busStop.getPassengerQuantity();
-        if (passengerQuantity == 0) {
-            return;
-        }
-        int difference = increase ? passengerQuantity : -passengerQuantity;
+        int difference = increase ? busStop.getPassengerQuantity() : -busStop.getPassengerQuantity();
         scoreDirector.beforeVariableChanged(bus, "passengerQuantityTotal");
         bus.setPassengerQuantityTotal(bus.getPassengerQuantityTotal() + difference);
         scoreDirector.afterVariableChanged(bus, "passengerQuantityTotal");
