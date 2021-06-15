b7ebfa9eb9b132ba8aab1538f7977d2c7a4cfa86
Jiří Locker <jiri.locker@gmail.com>
2021-06-15 20:04:01 +0200

    Duplicate taskassigning example
    Prepare for introduction of the list planning variable. At least during
    the feature development, the two examples will exist in parallel. That
    will allow for benchmark aggregation.

---
+++ optaplanner-examples/src/test/java/org/optaplanner/examples/taskassigning_listvariable/app/TaskAssigningPerformanceTest.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2016 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.optaplanner.examples.taskassigning_listvariable.app;
+
+import java.util.stream.Stream;
+
+import org.optaplanner.core.config.solver.EnvironmentMode;
+import org.optaplanner.examples.common.app.SolverPerformanceTest;
+import org.optaplanner.examples.taskassigning_listvariable.domain.TaskAssigningSolution;
+
+public class TaskAssigningPerformanceTest extends SolverPerformanceTest<TaskAssigningSolution> {
+
+    private static final String UNSOLVED_DATA_FILE = "data/taskassigning_listvariable/unsolved/50tasks-5employees.xml";
+
+    @Override
+    protected TaskAssigningApp createCommonApp() {
+        return new TaskAssigningApp();
+    }
+
+    @Override
+    protected Stream<TestData> testData() {
+        return Stream.of(
+                testData(UNSOLVED_DATA_FILE, "[0]hard/[-3925/-6293940/-7772/-20463]soft", EnvironmentMode.REPRODUCIBLE),
+                testData(UNSOLVED_DATA_FILE, "[0]hard/[-3988/-10452712/-15713/-21195]soft", EnvironmentMode.FAST_ASSERT));
+    }
+}
