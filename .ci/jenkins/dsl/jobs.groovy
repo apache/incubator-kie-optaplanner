d071e669edeb277d6ad807b96e0dfa48d02c525d
radtriste <tradisso@redhat.com>
2021-06-23 09:11:20 +0200

    rebased

--- .ci/jenkins/dsl/jobs.groovy
+++ .ci/jenkins/dsl/jobs.groovy
@@ -170,7 +170,7 @@ void setupDeployJob(String jobFolder, KogitoJobType jobType) {
             }
 
             //Build branch name for quickstarts
-            stringParam('QUICKSTARTS_BUILD_BRANCH_NAME', "${GIT_BRANCH}" == 'master' ? 'development' : "${GIT_BRANCH}", 'Base branch for quickstarts. Set if you are not on a multibranch pipeline.')
+            stringParam('QUICKSTARTS_BUILD_BRANCH_NAME', isMainBranch() ? 'development' : "${GIT_BRANCH}", 'Base branch for quickstarts. Set if you are not on a multibranch pipeline.')
         }
 
         environmentVariables {
