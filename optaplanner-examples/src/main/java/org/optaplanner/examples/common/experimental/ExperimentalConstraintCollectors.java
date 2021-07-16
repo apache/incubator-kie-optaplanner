2453a01b274fe087aef171484b8e3dacd8f18930
Christopher Chianelli <chris.chianelli@gmail.com>
2021-07-16 12:05:57 -0400

    Add Javadoc for differenceFunction and overloads for consecutiveIntervals

--- optaplanner-examples/src/main/java/org/optaplanner/examples/common/experimental/ExperimentalConstraintCollectors.java
+++ optaplanner-examples/src/main/java/org/optaplanner/examples/common/experimental/ExperimentalConstraintCollectors.java
@@ -16,9 +16,12 @@
 
 package org.optaplanner.examples.common.experimental;
 
+import java.time.Duration;
+import java.time.temporal.Temporal;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.function.ToIntFunction;
+import java.util.function.ToLongFunction;
 
 import org.optaplanner.core.api.function.QuadFunction;
 import org.optaplanner.core.api.function.TriFunction;
@@ -154,16 +157,21 @@ public class ExperimentalConstraintCollectors {
      * fact.
      *
      * For instance ${@code [Shift from=2, to=4] [Shift from=3, to=5] [Shift from=6, to=7] [Shift from=7, to=8]}
-     * returns the following infomation
+     * returns the following information
      *
      * ${@code
      * IntervalClusters [[Shift from=2, to=4] [Shift from=3, to=5]], [[Shift from=6, to=7] [Shift from=7, to=8]]
+     * Breaks [[Break from=5, to=6, length=1]]
      * }
      *
      * @param startMap Maps the fact to its start
      * @param endMap Maps the fact to its end
+     * @param differenceFunction Computes the difference between two points. The second argument is always
+     *        larger than the first (ex: {@link Duration#between}
+     *        or (a,b) -> b - a).
      * @param <A> type of the first mapped fact
      * @param <PointType_> type of the fact endpoints
+     * @param <DifferenceType_> type of difference between points
      * @return never null
      */
     public static <A, PointType_ extends Comparable<PointType_>, DifferenceType_ extends Comparable<DifferenceType_>>
@@ -183,14 +191,48 @@ public class ExperimentalConstraintCollectors {
     }
 
     /**
+     * Specialized version of {@link #consecutiveIntervals(Function,Function,BiFunction)} for
+     * {@link Temporal} types.
+     * 
+     * @param <A> type of the first mapped fact
+     * @param <PointType_> temporal type of the endpoints
+     * @param startMap Maps the fact to its start
+     * @param endMap Maps the fact to its end
+     * @return never null
+     */
+    public static <A, PointType_ extends Temporal & Comparable<PointType_>>
+            UniConstraintCollector<A, IntervalTree<A, PointType_, Duration>, ConsecutiveIntervalInfo<A, PointType_, Duration>>
+            consecutiveTemporalIntervals(Function<A, PointType_> startMap, Function<A, PointType_> endMap) {
+        return consecutiveIntervals(startMap, endMap, Duration::between);
+    }
+
+    /**
+     * Specialized version of {@link #consecutiveIntervals(Function,Function,BiFunction)} for Long.
+     *
+     * @param startMap Maps the fact to its start
+     * @param endMap Maps the fact to its end
+     * @param <A> type of the first mapped fact
+     * @return never null
+     */
+    public static <A> UniConstraintCollector<A, IntervalTree<A, Long, Long>, ConsecutiveIntervalInfo<A, Long, Long>>
+            consecutiveIntervals(ToLongFunction<A> startMap, ToLongFunction<A> endMap) {
+        return consecutiveIntervals(startMap::applyAsLong, endMap::applyAsLong, (a, b) -> b - a);
+    }
+
+    /**
      * As defined by {@link #consecutiveIntervals(Function,Function,BiFunction)}.
      *
-     * @param intervalMap Maps both facts to an item in the sequence
+     * @param intervalMap Maps both facts to an item in the cluster
      * @param startMap Maps the item to its start
      * @param endMap Maps the item to its end
+     * @param differenceFunction Computes the difference between two points. The second argument is always
+     *        larger than the first (ex: {@link Duration#between}
+     *        or (a,b) -> b - a).
      * @param <A> type of the first mapped fact
      * @param <B> type of the second mapped fact
+     * @param <IntervalType_> type of the item in the cluster
      * @param <PointType_> type of the item endpoints
+     * @param <DifferenceType_> type of difference between points
      * @return never null
      */
     public static <A, B, IntervalType_, PointType_ extends Comparable<PointType_>, DifferenceType_ extends Comparable<DifferenceType_>>
@@ -212,15 +254,56 @@ public class ExperimentalConstraintCollectors {
     }
 
     /**
+     * As defined by {@link #consecutiveTemporalIntervals(Function,Function)}.
+     *
+     * @param intervalMap Maps the three facts to an item in the cluster
+     * @param startMap Maps the fact to its start
+     * @param endMap Maps the fact to its end
+     * @param <A> type of the first mapped fact
+     * @param <B> type of the second mapped fact
+     * @param <IntervalType_> type of the item in the cluster
+     * @param <PointType_> temporal type of the endpoints
+     * @return never null
+     */
+    public static <A, B, IntervalType_, PointType_ extends Temporal & Comparable<PointType_>>
+            BiConstraintCollector<A, B, IntervalTree<IntervalType_, PointType_, Duration>, ConsecutiveIntervalInfo<IntervalType_, PointType_, Duration>>
+            consecutiveTemporalIntervals(BiFunction<A, B, IntervalType_> intervalMap,
+                    Function<IntervalType_, PointType_> startMap, Function<IntervalType_, PointType_> endMap) {
+        return consecutiveIntervals(intervalMap, startMap, endMap, Duration::between);
+    }
+
+    /**
+     * As defined by {@link #consecutiveIntervals(ToLongFunction, ToLongFunction)}.
+     *
+     * @param startMap Maps the fact to its start
+     * @param endMap Maps the fact to its end
+     * @param <A> type of the first mapped fact
+     * @param <B> type of the second mapped fact
+     * @param <IntervalType_> type of the item in the cluster
+     * @return never null
+     */
+    public static <A, B, IntervalType_>
+            BiConstraintCollector<A, B, IntervalTree<IntervalType_, Long, Long>, ConsecutiveIntervalInfo<IntervalType_, Long, Long>>
+            consecutiveIntervals(BiFunction<A, B, IntervalType_> intervalMap, ToLongFunction<IntervalType_> startMap,
+                    ToLongFunction<IntervalType_> endMap) {
+        return consecutiveIntervals(intervalMap, startMap::applyAsLong, endMap::applyAsLong, (a, b) -> b - a);
+    }
+
+    /**
      * As defined by {@link #consecutiveIntervals(Function,Function,BiFunction)}.
      *
-     * @param intervalMap Maps the three facts to an item in the sequence
+     * @param intervalMap Maps the three facts to an item in the cluster
      * @param startMap Maps the item to its start
      * @param endMap Maps the item to its end
+     * @param differenceFunction Computes the difference between two points. The second argument is always
+     *        larger than the first (ex: {@link Duration#between)}
+     *        or (a,b) -> b - a).
      * @param <A> type of the first mapped fact
      * @param <B> type of the second mapped fact
      * @param <C> type of the third mapped fact
+     * @param <IntervalType_> type of the item in the cluster
      * @param <PointType_> type of the item endpoints
+     * @param <DifferenceType_> type of difference between points
      * @return never null
      */
     public static <A, B, C, IntervalType_, PointType_ extends Comparable<PointType_>, DifferenceType_ extends Comparable<DifferenceType_>>
@@ -242,16 +325,59 @@ public class ExperimentalConstraintCollectors {
     }
 
     /**
+     * As defined by {@link #consecutiveTemporalIntervals(Function,Function)}.
+     *
+     * @param intervalMap Maps the three facts to an item in the cluster
+     * @param startMap Maps the fact to its start
+     * @param endMap Maps the fact to its end
+     * @param <A> type of the first mapped fact
+     * @param <B> type of the second mapped fact
+     * @param <C> type of the third mapped fact
+     * @param <IntervalType_> type of the item in the cluster
+     * @param <PointType_> temporal type of the endpoints
+     * @return never null
+     */
+    public static <A, B, C, IntervalType_, PointType_ extends Temporal & Comparable<PointType_>>
+            TriConstraintCollector<A, B, C, IntervalTree<IntervalType_, PointType_, Duration>, ConsecutiveIntervalInfo<IntervalType_, PointType_, Duration>>
+            consecutiveTemporalIntervals(TriFunction<A, B, C, IntervalType_> intervalMap,
+                    Function<IntervalType_, PointType_> startMap, Function<IntervalType_, PointType_> endMap) {
+        return consecutiveIntervals(intervalMap, startMap, endMap, Duration::between);
+    }
+
+    /**
+     * As defined by {@link #consecutiveIntervals(ToLongFunction, ToLongFunction)}.
+     *
+     * @param startMap Maps the fact to its start
+     * @param endMap Maps the fact to its end
+     * @param <A> type of the first mapped fact
+     * @param <B> type of the second mapped fact
+     * @param <C> type of the third mapped fact
+     * @param <IntervalType_> type of the item in the cluster
+     * @return never null
+     */
+    public static <A, B, C, IntervalType_>
+            TriConstraintCollector<A, B, C, IntervalTree<IntervalType_, Long, Long>, ConsecutiveIntervalInfo<IntervalType_, Long, Long>>
+            consecutiveIntervals(TriFunction<A, B, C, IntervalType_> intervalMap, ToLongFunction<IntervalType_> startMap,
+                    ToLongFunction<IntervalType_> endMap) {
+        return consecutiveIntervals(intervalMap, startMap::applyAsLong, endMap::applyAsLong, (a, b) -> b - a);
+    }
+
+    /**
      * As defined by {@link #consecutiveIntervals(Function,Function,BiFunction)}.
      *
-     * @param intervalMap Maps the four facts to an item in the sequence
+     * @param intervalMap Maps the four facts to an item in the cluster
      * @param startMap Maps the item to its start
      * @param endMap Maps the item to its end
+     * @param differenceFunction Computes the difference between two points. The second argument is always
+     *        larger than the first (ex: {@link Duration#between}
+     *        or (a,b) -> b - a).
      * @param <A> type of the first mapped fact
      * @param <B> type of the second mapped fact
      * @param <C> type of the third mapped fact
      * @param <D> type of the fourth mapped fact
+     * @param <IntervalType_> type of the item in the cluster
      * @param <PointType_> type of the item endpoints
+     * @param <DifferenceType_> type of difference between points
      * @return never null
      */
     public static <A, B, C, D, IntervalType_, PointType_ extends Comparable<PointType_>, DifferenceType_ extends Comparable<DifferenceType_>>
@@ -270,6 +396,46 @@ public class ExperimentalConstraintCollectors {
                     return () -> acc.remove(interval);
                 },
                 IntervalTree::getConsecutiveIntervalData);
+    }
+
+    /**
+     * As defined by {@link #consecutiveTemporalIntervals(Function,Function)}.
+     *
+     * @param intervalMap Maps the three facts to an item in the cluster
+     * @param startMap Maps the fact to its start
+     * @param endMap Maps the fact to its end
+     * @param <A> type of the first mapped fact
+     * @param <B> type of the second mapped fact
+     * @param <C> type of the third mapped fact
+     * @param <D> type of the fourth mapped fact
+     * @param <IntervalType_> type of the item in the cluster
+     * @param <PointType_> temporal type of the endpoints
+     * @return never null
+     */
+    public static <A, B, C, D, IntervalType_, PointType_ extends Temporal & Comparable<PointType_>>
+            QuadConstraintCollector<A, B, C, D, IntervalTree<IntervalType_, PointType_, Duration>, ConsecutiveIntervalInfo<IntervalType_, PointType_, Duration>>
+            consecutiveTemporalIntervals(QuadFunction<A, B, C, D, IntervalType_> intervalMap,
+                    Function<IntervalType_, PointType_> startMap, Function<IntervalType_, PointType_> endMap) {
+        return consecutiveIntervals(intervalMap, startMap, endMap, Duration::between);
+    }
+
+    /**
+     * As defined by {@link #consecutiveIntervals(ToLongFunction, ToLongFunction)}.
+     *
+     * @param startMap Maps the fact to its start
+     * @param endMap Maps the fact to its end
+     * @param <A> type of the first mapped fact
+     * @param <B> type of the second mapped fact
+     * @param <C> type of the third mapped fact
+     * @param <D> type of the fourth mapped fact
+     * @param <IntervalType_> type of the item in the cluster
+     * @return never null
+     */
+    public static <A, B, C, D, IntervalType_>
+            QuadConstraintCollector<A, B, C, D, IntervalTree<IntervalType_, Long, Long>, ConsecutiveIntervalInfo<IntervalType_, Long, Long>>
+            consecutiveIntervals(QuadFunction<A, B, C, D, IntervalType_> intervalMap, ToLongFunction<IntervalType_> startMap,
+                    ToLongFunction<IntervalType_> endMap) {
+        return consecutiveIntervals(intervalMap, startMap::applyAsLong, endMap::applyAsLong, (a, b) -> b - a);
     }
 
     // Hide constructor since this is a factory class
