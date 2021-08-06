3e6ee8890abc19db5dd3e89950c1a30a9d8f7418
Jiří Locker <jiri.locker@gmail.com>
2021-08-06 21:23:33 +0200

    Improve ListChangeMove documentation and test coverage

--- optaplanner-core/src/test/java/org/optaplanner/core/impl/heuristic/selector/move/generic/list/ListChangeMoveTest.java
+++ optaplanner-core/src/test/java/org/optaplanner/core/impl/heuristic/selector/move/generic/list/ListChangeMoveTest.java
@@ -16,15 +16,24 @@
 
 package org.optaplanner.core.impl.heuristic.selector.move.generic.list;
 
+import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 import static org.mockito.Mockito.mock;
 
+import java.util.List;
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.optaplanner.core.api.score.buildin.simple.SimpleScore;
 import org.optaplanner.core.api.score.director.ScoreDirector;
 import org.optaplanner.core.impl.domain.variable.descriptor.ListVariableDescriptor;
 import org.optaplanner.core.impl.heuristic.move.AbstractMove;
 import org.optaplanner.core.impl.score.director.InnerScoreDirector;
+import org.optaplanner.core.impl.testdata.domain.TestdataObject;
 import org.optaplanner.core.impl.testdata.domain.list.TestdataListEntity;
 import org.optaplanner.core.impl.testdata.domain.list.TestdataListSolution;
 import org.optaplanner.core.impl.testdata.domain.list.TestdataListValue;
@@ -76,5 +85,65 @@ class ListChangeMoveTest {
         assertThat(new ListChangeMove<>(variableDescriptor, e1, 0, e1, 2).isMoveDoable(scoreDirector)).isFalse();
         // different entity => doable
         assertThat(new ListChangeMove<>(variableDescriptor, e1, 0, e2, 0).isMoveDoable(scoreDirector)).isTrue();
+    }
+
+    static Stream<Arguments> doAndUndoMoveOnTheSameEntity() {
+        // Given E.valueList = [V0, V1, V2, V3, V4],
+        // when V2 is moved to destinationIndex (arg0),
+        // then the resulting valueList should be arg1.
+        return Stream.of(
+                arguments(0, asList("V2", "V0", "V1", "V3", "V4")),
+                arguments(1, asList("V0", "V2", "V1", "V3", "V4")),
+                arguments(2, null), // undoable (no-op)
+                arguments(3, asList("V0", "V1", "V3", "V2", "V4")),
+                arguments(4, asList("V0", "V1", "V3", "V4", "V2")),
+                arguments(5, null) // undoable (out of bounds)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void doAndUndoMoveOnTheSameEntity(int destinationIndex, List<String> expectedValueList) {
+        // Given...
+        final int sourceIndex = 2; // we're always moving V2
+        TestdataListValue v0 = new TestdataListValue("V0");
+        TestdataListValue v1 = new TestdataListValue("V1");
+        TestdataListValue v2 = new TestdataListValue("V2");
+        TestdataListValue v3 = new TestdataListValue("V3");
+        TestdataListValue v4 = new TestdataListValue("V4");
+        TestdataListEntity e = new TestdataListEntity("E", v0, v1, v2, v3, v4);
+
+        InnerScoreDirector<TestdataListSolution, SimpleScore> scoreDirector = mock(InnerScoreDirector.class);
+        ListVariableDescriptor<TestdataListSolution> variableDescriptor =
+                TestdataListEntity.buildVariableDescriptorForValueList();
+
+        // When V2 is moved to destinationIndex...
+        ListChangeMove<TestdataListSolution> move =
+                new ListChangeMove<>(variableDescriptor, e, sourceIndex, e, destinationIndex);
+
+        // Some destinationIndexes make the move undoable.
+        if (expectedValueList == null) {
+            assertThat(move.isMoveDoable(scoreDirector)).isFalse();
+            return;
+        }
+
+        // Otherwise, the move is doable...
+        assertThat(move.isMoveDoable(scoreDirector)).isTrue();
+        // ...and when it's done...
+        AbstractMove<TestdataListSolution> undoMove = move.doMove(scoreDirector);
+        // ...V2 ends up at the destinationIndex
+        assertThat(e.getValueList().indexOf(v2)).isEqualTo(destinationIndex);
+        assertThat(variableDescriptor.getElement(e, destinationIndex)).isEqualTo(v2);
+        // ...and the modified value list matches the expectation.
+        assertThat(e.getValueList()).map(TestdataObject::toString).isEqualTo(expectedValueList);
+
+        // Making an undo move...
+        AbstractMove<TestdataListSolution> undoUndoMove = undoMove.doMove(scoreDirector);
+        // ...produces the original move...
+        assertThat(undoUndoMove).isEqualTo(move);
+        // ...and returns everything to the original state.
+        assertThat(e.getValueList().indexOf(v2)).isEqualTo(sourceIndex);
+        assertThat(variableDescriptor.getElement(e, sourceIndex)).isEqualTo(v2);
+        assertThat(e.getValueList()).containsExactly(v0, v1, v2, v3, v4);
     }
 }
