yurloc/optaplanner

Switch task assigning domain to list variable

optaplanner-examples/src/main/java/org/optaplanner/examples/taskassigning/app/TaskAssigningApp.java
@@ -18,7 +18,7 @@ package org.optaplanner.examples.taskassigning.app;
 
 import org.optaplanner.examples.common.app.CommonApp;
 import org.optaplanner.examples.taskassigning.domain.TaskAssigningSolution;
-import org.optaplanner.examples.taskassigning.persistence.TaskAssigningXmlSolutionFileIO;
+import org.optaplanner.examples.taskassigning.persistence.orig.BridgeTaskAssigningXmlSolutionFileIO;
 import org.optaplanner.examples.taskassigning.swingui.TaskAssigningPanel;
 import org.optaplanner.persistence.common.api.domain.solution.SolutionFileIO;
 
@@ -50,7 +50,7 @@ public class TaskAssigningApp extends CommonApp<TaskAssigningSolution> {
 
     @Override
     public SolutionFileIO<TaskAssigningSolution> createSolutionFileIO() {
-        return new TaskAssigningXmlSolutionFileIO();
+        return new BridgeTaskAssigningXmlSolutionFileIO();
     }
 
 }

optaplanner-examples/src/main/java/org/optaplanner/examples/taskassigning/domain/solver/solution/initializer/RoundRobinInitializer.java
@@ -1,0 +1,64 @@
+/*
+ * Copyright 2020 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.optaplanner.examples.taskassigning.domain.solver.solution.initializer;
+
+import java.util.ArrayList;
+
+import org.optaplanner.core.api.score.director.ScoreDirector;
+import org.optaplanner.core.impl.phase.custom.CustomPhaseCommand;
+import org.optaplanner.examples.taskassigning.domain.Employee;
+import org.optaplanner.examples.taskassigning.domain.Task;
+import org.optaplanner.examples.taskassigning.domain.TaskAssigningSolution;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RoundRobinInitializer implements CustomPhaseCommand<TaskAssigningSolution> {
+
+    private static final Logger logger = LoggerFactory.getLogger(RoundRobinInitializer.class);
+
+    @Override
+    public void changeWorkingSolution(ScoreDirector<TaskAssigningSolution> scoreDirector) {
+        TaskAssigningSolution solution = scoreDirector.getWorkingSolution();
+
+        solution.getEmployeeList().stream()
+                .filter(employee -> employee.getTasks() == null)
+                .forEach(employee -> employee.setTasks(new ArrayList<>()));
+
+        int employeeListSize = solution.getEmployeeList().size();
+        int tasksAssigned = 0;
+
+        for (int i = 0; i < solution.getTaskList().size(); i++) {
+            Task task = solution.getTaskList().get(i);
+            if (task.getEmployee() == null) {
+                Employee employee = solution.getEmployeeList().get(i % employeeListSize);
+                scoreDirector.beforeVariableChanged(employee, "tasks");
+                employee.getTasks().add(task);
+                scoreDirector.afterVariableChanged(employee, "tasks");
+                tasksAssigned++;
+            }
+        }
+
+        if (logger.isDebugEnabled()) {
+            logger.debug("    {} task{} ha{} been assigned.",
+                    tasksAssigned,
+                    tasksAssigned == 1 ? "" : "s",
+                    tasksAssigned == 1 ? "s" : "ve");
+        }
+
+        scoreDirector.triggerVariableListeners();
+    }
+}

optaplanner-examples/src/main/java/org/optaplanner/examples/taskassigning/optional/score/TaskAssigningConstraintProvider.java
@@ -21,6 +21,7 @@ import org.optaplanner.core.api.score.stream.Constraint;
 import org.optaplanner.core.api.score.stream.ConstraintFactory;
 import org.optaplanner.core.api.score.stream.ConstraintProvider;
 import org.optaplanner.core.api.score.stream.uni.UniConstraintStream;
+import org.optaplanner.examples.taskassigning.domain.Employee;
 import org.optaplanner.examples.taskassigning.domain.Priority;
 import org.optaplanner.examples.taskassigning.domain.Task;
 
@@ -65,11 +66,10 @@ public final class TaskAssigningConstraintProvider implements ConstraintProvider
     }
 
     private Constraint minimizeMakespan(ConstraintFactory constraintFactory) {
-        return constraintFactory.from(Task.class)
-                .filter(task -> task.getNextTask() == null)
+        return constraintFactory.from(Employee.class)
                 .penalize("Minimize makespan, latest ending employee first",
                         BendableScore.ofSoft(BENDABLE_SCORE_HARD_LEVELS_SIZE, BENDABLE_SCORE_SOFT_LEVELS_SIZE, 1, 1),
-                        task -> task.getEndTime() * task.getEndTime());
+                        employee -> employee.getEndTime() * employee.getEndTime());
     }
 
     private Constraint majorPriorityTaskEndTime(ConstraintFactory constraintFactory) {

apache/incubator-kie-optaplanner

optaplanner-examples/src/main/java/org/optaplanner/examples/taskassigning_listvariable/app/TaskAssigningApp.java
@@ -18,7 +18,7 @@ package org.optaplanner.examples.taskassigning_listvariable.app;
 
 import org.optaplanner.examples.common.app.CommonApp;
 import org.optaplanner.examples.taskassigning_listvariable.domain.TaskAssigningSolution;
-import org.optaplanner.examples.taskassigning_listvariable.persistence.TaskAssigningXmlSolutionFileIO;
+import org.optaplanner.examples.taskassigning_listvariable.persistence.orig.BridgeTaskAssigningXmlSolutionFileIO;
 import org.optaplanner.examples.taskassigning_listvariable.swingui.TaskAssigningPanel;
 import org.optaplanner.persistence.common.api.domain.solution.SolutionFileIO;
 
@@ -27,7 +27,7 @@ public class TaskAssigningApp extends CommonApp<TaskAssigningSolution> {
     public static final String SOLVER_CONFIG =
             "org/optaplanner/examples/taskassigning_listvariable/solver/taskAssigningSolverConfig.xml";
 
-    public static final String DATA_DIR_NAME = "taskassigning_listvariable";
+    public static final String DATA_DIR_NAME = "taskassigning";
 
     public static void main(String[] args) {
         prepareSwingEnvironment();
@@ -51,7 +51,7 @@ public class TaskAssigningApp extends CommonApp<TaskAssigningSolution> {
 
     @Override
     public SolutionFileIO<TaskAssigningSolution> createSolutionFileIO() {
-        return new TaskAssigningXmlSolutionFileIO();
+        return new BridgeTaskAssigningXmlSolutionFileIO();
     }
 
 }

optaplanner-examples/src/main/java/org/optaplanner/examples/taskassigning_listvariable/domain/solver/solution/initializer/RoundRobinInitializer.java
@@ -1,0 +1,64 @@
+/*
+ * Copyright 2020 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.optaplanner.examples.taskassigning_listvariable.domain.solver.solution.initializer;
+
+import java.util.ArrayList;
+
+import org.optaplanner.core.api.score.director.ScoreDirector;
+import org.optaplanner.core.impl.phase.custom.CustomPhaseCommand;
+import org.optaplanner.examples.taskassigning_listvariable.domain.Employee;
+import org.optaplanner.examples.taskassigning_listvariable.domain.Task;
+import org.optaplanner.examples.taskassigning_listvariable.domain.TaskAssigningSolution;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RoundRobinInitializer implements CustomPhaseCommand<TaskAssigningSolution> {
+
+    private static final Logger logger = LoggerFactory.getLogger(RoundRobinInitializer.class);
+
+    @Override
+    public void changeWorkingSolution(ScoreDirector<TaskAssigningSolution> scoreDirector) {
+        TaskAssigningSolution solution = scoreDirector.getWorkingSolution();
+
+        solution.getEmployeeList().stream()
+                .filter(employee -> employee.getTasks() == null)
+                .forEach(employee -> employee.setTasks(new ArrayList<>()));
+
+        int employeeListSize = solution.getEmployeeList().size();
+        int tasksAssigned = 0;
+
+        for (int i = 0; i < solution.getTaskList().size(); i++) {
+            Task task = solution.getTaskList().get(i);
+            if (task.getEmployee() == null) {
+                Employee employee = solution.getEmployeeList().get(i % employeeListSize);
+                scoreDirector.beforeVariableChanged(employee, "tasks");
+                employee.getTasks().add(task);
+                scoreDirector.afterVariableChanged(employee, "tasks");
+                tasksAssigned++;
+            }
+        }
+
+        if (logger.isDebugEnabled()) {
+            logger.debug("    {} task{} ha{} been assigned.",
+                    tasksAssigned,
+                    tasksAssigned == 1 ? "" : "s",
+                    tasksAssigned == 1 ? "s" : "ve");
+        }
+
+        scoreDirector.triggerVariableListeners();
+    }
+}

optaplanner-examples/src/main/java/org/optaplanner/examples/taskassigning_listvariable/optional/score/TaskAssigningConstraintProvider.java
@@ -21,6 +21,7 @@ import org.optaplanner.core.api.score.stream.Constraint;
 import org.optaplanner.core.api.score.stream.ConstraintFactory;
 import org.optaplanner.core.api.score.stream.ConstraintProvider;
 import org.optaplanner.core.api.score.stream.uni.UniConstraintStream;
+import org.optaplanner.examples.taskassigning_listvariable.domain.Employee;
 import org.optaplanner.examples.taskassigning_listvariable.domain.Priority;
 import org.optaplanner.examples.taskassigning_listvariable.domain.Task;
 
@@ -65,11 +66,10 @@ public final class TaskAssigningConstraintProvider implements ConstraintProvider
     }
 
     private Constraint minimizeMakespan(ConstraintFactory constraintFactory) {
-        return constraintFactory.from(Task.class)
-                .filter(task -> task.getNextTask() == null)
+        return constraintFactory.from(Employee.class)
                 .penalize("Minimize makespan, latest ending employee first",
                         BendableScore.ofSoft(BENDABLE_SCORE_HARD_LEVELS_SIZE, BENDABLE_SCORE_SOFT_LEVELS_SIZE, 1, 1),
-                        task -> task.getEndTime() * task.getEndTime());
+                        employee -> employee.getEndTime() * employee.getEndTime());
     }
 
     private Constraint majorPriorityTaskEndTime(ConstraintFactory constraintFactory) {