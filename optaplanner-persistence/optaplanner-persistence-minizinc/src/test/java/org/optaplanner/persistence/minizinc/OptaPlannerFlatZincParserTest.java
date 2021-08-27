f7084ac33e12808b72282013e55db82795196639
Christopher Chianelli <chris.chianelli@gmail.com>
2021-08-27 16:54:05 -0400

    Fix annotation handling

--- optaplanner-persistence/optaplanner-persistence-minizinc/src/test/java/org/optaplanner/persistence/minizinc/OptaPlannerFlatZincParserTest.java
+++ optaplanner-persistence/optaplanner-persistence-minizinc/src/test/java/org/optaplanner/persistence/minizinc/OptaPlannerFlatZincParserTest.java
@@ -37,7 +37,7 @@ import org.optaplanner.persistence.minizinc.parser.ParseException;
 public class OptaPlannerFlatZincParserTest {
 
     @Test
-    public void testParseFromFile() throws ParseException {
+    public void testThreeColor() throws ParseException {
         List<FlatZincPredicate> expectedPredicateList = List.of();
         List<TypeNameValue> expectedParameterList = List.of(new TypeNameValue(int[].class, "X_INTRODUCED_0_",
                 new FlatZincArray(List.of(new FlatZincExpr(1), new FlatZincExpr(-1)))));
@@ -109,7 +109,90 @@ public class OptaPlannerFlatZincParserTest {
         FlatZincModel expected = new FlatZincModel(expectedPredicateList, expectedParameterList, expectedPlanningVariables,
                 expectedConstraints, expectedSolve);
         FlatZincModel actual =
-                OptaPlannerFlatZincParser.parse(OptaPlannerFlatZincParserTest.class.getResourceAsStream("/model.fzn"));
+                OptaPlannerFlatZincParser
+                        .parse(OptaPlannerFlatZincParserTest.class.getResourceAsStream("/three-color-model.fzn"));
+
+        // Need to use recursive comparison since ValueRange do not typically implement hashCode/equals
+        assertThat(actual)
+                .usingRecursiveComparison()
+                .isEqualTo(expected);
+    }
+
+    @Test
+    public void testLaplace() throws ParseException {
+        List<FlatZincPredicate> expectedPredicateList = List.of();
+        List<TypeNameValue> expectedParameterList = List.of(new TypeNameValue(int[].class, "X_INTRODUCED_29_",
+                new FlatZincArray(List.of(new FlatZincExpr(4), new FlatZincExpr(-1), new FlatZincExpr(-1), new FlatZincExpr(-1),
+                        new FlatZincExpr(-1)))));
+        ValueRange colors = new IntValueRange(1, 4);
+        FlatZincAnnotation outputVarAnno = new FlatZincAnnotation("output_var", List.of());
+        List<FlatZincPlanningVariable> expectedPlanningVariables =
+                List.of(new FlatZincPlanningVariable(colors, "wa", List.of(outputVarAnno)),
+                        new FlatZincPlanningVariable(colors, "nt", List.of(outputVarAnno)),
+                        new FlatZincPlanningVariable(colors, "sa", List.of(outputVarAnno)),
+                        new FlatZincPlanningVariable(colors, "q", List.of(outputVarAnno)),
+                        new FlatZincPlanningVariable(colors, "nsw", List.of(outputVarAnno)),
+                        new FlatZincPlanningVariable(colors, "v", List.of(outputVarAnno)),
+                        new FlatZincPlanningVariable(colors, "t", List.of(outputVarAnno)));
+        FlatZincExpr parameterName = new FlatZincExpr("X_INTRODUCED_0_");
+        FlatZincExpr zero = new FlatZincExpr(0);
+        FlatZincExpr wa = new FlatZincExpr("wa");
+        FlatZincExpr nt = new FlatZincExpr("nt");
+        FlatZincExpr sa = new FlatZincExpr("sa");
+        FlatZincExpr q = new FlatZincExpr("q");
+        FlatZincExpr nsw = new FlatZincExpr("nsw");
+        FlatZincExpr v = new FlatZincExpr("v");
+        FlatZincExpr t = new FlatZincExpr("t");
+        List<FlatZincConstraint> expectedConstraints = List.of(new FlatZincConstraint("int_lin_ne",
+                List.of(parameterName,
+                        new FlatZincArray(List.of(wa, nt)),
+                        zero),
+                List.of()),
+                new FlatZincConstraint("int_lin_ne",
+                        List.of(parameterName,
+                                new FlatZincArray(List.of(wa, sa)),
+                                zero),
+                        List.of()),
+                new FlatZincConstraint("int_lin_ne",
+                        List.of(parameterName,
+                                new FlatZincArray(List.of(nt, sa)),
+                                zero),
+                        List.of()),
+                new FlatZincConstraint("int_lin_ne",
+                        List.of(parameterName,
+                                new FlatZincArray(List.of(nt, q)),
+                                zero),
+                        List.of()),
+                new FlatZincConstraint("int_lin_ne",
+                        List.of(parameterName,
+                                new FlatZincArray(List.of(sa, q)),
+                                zero),
+                        List.of()),
+                new FlatZincConstraint("int_lin_ne",
+                        List.of(parameterName,
+                                new FlatZincArray(List.of(sa, nsw)),
+                                zero),
+                        List.of()),
+                new FlatZincConstraint("int_lin_ne",
+                        List.of(parameterName,
+                                new FlatZincArray(List.of(sa, v)),
+                                zero),
+                        List.of()),
+                new FlatZincConstraint("int_lin_ne",
+                        List.of(parameterName,
+                                new FlatZincArray(List.of(q, nsw)),
+                                zero),
+                        List.of()),
+                new FlatZincConstraint("int_lin_ne",
+                        List.of(parameterName,
+                                new FlatZincArray(List.of(nsw, v)),
+                                zero),
+                        List.of()));
+        FlatZincSolve expectedSolve = new FlatZincSolve(List.of(), FlatZincGoal.SATISFY, null);
+        FlatZincModel expected = new FlatZincModel(expectedPredicateList, expectedParameterList, expectedPlanningVariables,
+                expectedConstraints, expectedSolve);
+        FlatZincModel actual =
+                OptaPlannerFlatZincParser.parse(OptaPlannerFlatZincParserTest.class.getResourceAsStream("/laplace.fzn"));
 
         // Need to use recursive comparison since ValueRange do not typically implement hashCode/equals
         assertThat(actual)
