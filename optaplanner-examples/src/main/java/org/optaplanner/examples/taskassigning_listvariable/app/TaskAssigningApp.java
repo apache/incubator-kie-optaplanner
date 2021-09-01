yurloc/optaplanner

Use "Tmp" prefix for temporary files

optaplanner-examples/src/main/java/org/optaplanner/examples/taskassigning/app/TaskAssigningApp.java
@@ -18,7 +18,7 @@ package org.optaplanner.examples.taskassigning.app;
 
 import org.optaplanner.examples.common.app.CommonApp;
 import org.optaplanner.examples.taskassigning.domain.TaskAssigningSolution;
-import org.optaplanner.examples.taskassigning.persistence.orig.BridgeTaskAssigningXmlSolutionFileIO;
+import org.optaplanner.examples.taskassigning.persistence.orig.TmpBridgeTaskAssigningXmlSolutionFileIO;
 import org.optaplanner.examples.taskassigning.swingui.TaskAssigningPanel;
 import org.optaplanner.persistence.common.api.domain.solution.SolutionFileIO;
 
@@ -50,7 +50,7 @@ public class TaskAssigningApp extends CommonApp<TaskAssigningSolution> {
 
     @Override
     public SolutionFileIO<TaskAssigningSolution> createSolutionFileIO() {
-        return new BridgeTaskAssigningXmlSolutionFileIO();
+        return new TmpBridgeTaskAssigningXmlSolutionFileIO();
     }
 
 }

apache/incubator-kie-optaplanner

optaplanner-examples/src/main/java/org/optaplanner/examples/taskassigning_listvariable/app/TaskAssigningApp.java
@@ -18,7 +18,7 @@ package org.optaplanner.examples.taskassigning_listvariable.app;
 
 import org.optaplanner.examples.common.app.CommonApp;
 import org.optaplanner.examples.taskassigning_listvariable.domain.TaskAssigningSolution;
-import org.optaplanner.examples.taskassigning_listvariable.persistence.orig.BridgeTaskAssigningXmlSolutionFileIO;
+import org.optaplanner.examples.taskassigning_listvariable.persistence.orig.TmpBridgeTaskAssigningXmlSolutionFileIO;
 import org.optaplanner.examples.taskassigning_listvariable.swingui.TaskAssigningPanel;
 import org.optaplanner.persistence.common.api.domain.solution.SolutionFileIO;
 
@@ -51,7 +51,7 @@ public class TaskAssigningApp extends CommonApp<TaskAssigningSolution> {
 
     @Override
     public SolutionFileIO<TaskAssigningSolution> createSolutionFileIO() {
-        return new BridgeTaskAssigningXmlSolutionFileIO();
+        return new TmpBridgeTaskAssigningXmlSolutionFileIO();
     }
 
 }