fd7c172a71ac5d49a86e635fef0c60a126423876
Jiří Locker <jiri.locker@gmail.com>
2021-11-30 16:11:00 +0100

    Show thread dump of a failed test
    Show tread dump for
    - DefaultPartitionedSearchPhaseTest
    - CloudBalancingDaemonTest
    
    Cherry-pick [7.59.x] PLANNER-2563 Improve partition search tests stability (#1695)

---
+++ optaplanner-core/src/test/java/org/optaplanner/core/impl/util/ThreadDumpExtension.java
@@ -0,0 +1,34 @@
+package org.optaplanner.core.impl.util;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadInfo;
+import java.lang.management.ThreadMXBean;
+
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.TestWatcher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Dumps all threads if the test fails.
+ */
+public class ThreadDumpExtension implements TestWatcher {
+
+    private static final Logger logger = LoggerFactory.getLogger(ThreadDumpExtension.class);
+
+    @Override
+    public void testFailed(ExtensionContext context, Throwable cause) {
+        logger.error("Thread dump of a failed test ({}):{}", context.getUniqueId(), threadDump());
+    }
+
+    private static String threadDump() {
+        ThreadMXBean threadMXBean = ManagementFactory.getThreadMXBean();
+        boolean lockedMonitors = threadMXBean.isObjectMonitorUsageSupported();
+        boolean lockedSynchronizers = threadMXBean.isSynchronizerUsageSupported();
+        StringBuffer threadDump = new StringBuffer(System.lineSeparator());
+        for (ThreadInfo threadInfo : threadMXBean.dumpAllThreads(lockedMonitors, lockedSynchronizers)) {
+            threadDump.append(threadInfo.toString());
+        }
+        return threadDump.toString();
+    }
+}
