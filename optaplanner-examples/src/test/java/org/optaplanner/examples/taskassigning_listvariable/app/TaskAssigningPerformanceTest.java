48e75cb3e10de575b5fa3b5877898d7015b111e3
Jiří Locker <jiri.locker@gmail.com>
2022-01-11 20:21:21 +0100

    Update benchmark
    - Re-generate data sets using the new model.
    - Drop bridge solution IO.
    - Update the benchmark to use the new input files and to run both CH and
      LS.

--- optaplanner-examples/src/test/java/org/optaplanner/examples/taskassigning_listvariable/app/TaskAssigningPerformanceTest.java
+++ optaplanner-examples/src/test/java/org/optaplanner/examples/taskassigning_listvariable/app/TaskAssigningPerformanceTest.java
@@ -25,7 +25,7 @@ import org.optaplanner.examples.taskassigning_listvariable.domain.TaskAssigningS
 
 public class TaskAssigningPerformanceTest extends SolverPerformanceTest<TaskAssigningSolution, BendableScore> {
 
-    private static final String UNSOLVED_DATA_FILE = "data/taskassigning/unsolved/50tasks-5employees.xml";
+    private static final String UNSOLVED_DATA_FILE = "data/taskassigning_listvariable/unsolved/50tasks-5employees.xml";
 
     @Override
     protected TaskAssigningApp createCommonApp() {
