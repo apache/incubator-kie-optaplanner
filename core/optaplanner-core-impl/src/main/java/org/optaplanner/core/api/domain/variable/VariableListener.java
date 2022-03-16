cea535c866918e9b98480aa4e897f272e2cc766a
Lukáš Petrovický <lukas@petrovicky.net>
2022-03-16 17:55:22 +0100

    Update core/optaplanner-core-impl/src/main/java/org/optaplanner/core/api/domain/variable/VariableListener.java
    Co-authored-by: Jiří Locker <jiri.locker@gmail.com>

--- core/optaplanner-core-impl/src/main/java/org/optaplanner/core/api/domain/variable/VariableListener.java
+++ core/optaplanner-core-impl/src/main/java/org/optaplanner/core/api/domain/variable/VariableListener.java
@@ -108,7 +108,7 @@ public interface VariableListener<Solution_, Entity_> extends Closeable {
     }
 
     /**
-     * Call before either a problem fact changes or a planning entity changes without changing its planning variable.
+     * Called before either a problem fact changes or a planning entity changes without changing its planning variable.
      *
      * <p>
      * <strong>Do not use this method when changing planning variable values on planning entities.</strong>
