0aad94bccf991664cc8a74816c529019e864713d
Lukáš Petrovický <lukas@petrovicky.net>
2022-05-16 15:21:49 +0200

    Use the Quadruple we now have

--- core/optaplanner-core-impl/src/main/java/org/optaplanner/core/impl/util/Quadruple.java
+++ core/optaplanner-core-impl/src/main/java/org/optaplanner/core/impl/util/Quadruple.java
@@ -16,6 +16,16 @@
 
 package org.optaplanner.core.impl.util;
 
+/**
+ * A tuple of four values.
+ * Two instances {@link #equals(Object) are equal} if all four values in the first instance are equal to their counterpart in
+ * the other instance.
+ *
+ * @param <A>
+ * @param <B>
+ * @param <C>
+ * @param <D>
+ */
 public interface Quadruple<A, B, C, D> {
 
     static <A, B, C, D> Quadruple<A, B, C, D> of(A a, B b, C c, D d) {
