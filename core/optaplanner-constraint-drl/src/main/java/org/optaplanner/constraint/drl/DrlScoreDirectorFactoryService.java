75967c3b476a6cef0f4e6aa2d85ced95068b36e2
Lukáš Petrovický <lukas@petrovicky.net>
2022-05-23 16:38:09 +0200

    PLANNER-2713 Remove DRL testgen

--- core/optaplanner-constraint-drl/src/main/java/org/optaplanner/constraint/drl/DrlScoreDirectorFactoryService.java
+++ core/optaplanner-constraint-drl/src/main/java/org/optaplanner/constraint/drl/DrlScoreDirectorFactoryService.java
@@ -16,22 +16,33 @@
 
 package org.optaplanner.constraint.drl;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Supplier;
 
+import org.drools.ancompiler.KieBaseUpdaterANC;
+import org.drools.modelcompiler.ExecutableModelProject;
 import org.kie.api.KieBase;
+import org.kie.api.KieBaseConfiguration;
+import org.kie.api.KieServices;
+import org.kie.api.conf.KieBaseMutabilityOption;
+import org.kie.internal.builder.conf.PropertySpecificOption;
+import org.kie.internal.utils.KieHelper;
 import org.optaplanner.core.api.score.Score;
 import org.optaplanner.core.config.score.director.ScoreDirectorFactoryConfig;
 import org.optaplanner.core.config.util.ConfigUtils;
 import org.optaplanner.core.impl.domain.solution.descriptor.SolutionDescriptor;
 import org.optaplanner.core.impl.score.director.AbstractScoreDirectorFactory;
+import org.optaplanner.core.impl.score.director.ScoreDirectorFactoryService;
+import org.optaplanner.core.impl.score.director.ScoreDirectorType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public final class DrlScoreDirectorFactoryService<Solution_, Score_ extends Score<Score_>>
-        extends AbstractDrlScoreDirectorFactoryService<Solution_, Score_> {
+        implements ScoreDirectorFactoryService<Solution_, Score_> {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(DrlScoreDirectorFactoryService.class);
     private static final AtomicBoolean DRL_DEPRECATION_MESSAGE_SHOWN = new AtomicBoolean();
@@ -39,10 +50,6 @@ public final class DrlScoreDirectorFactoryService<Solution_, Score_ extends Scor
     @Override
     public Supplier<AbstractScoreDirectorFactory<Solution_, Score_>> buildScoreDirectorFactory(ClassLoader classLoader,
             SolutionDescriptor<Solution_> solutionDescriptor, ScoreDirectorFactoryConfig config) {
-        if (isTestGenRequested()) {
-            return null; // TestGenDrlScoreDirectorFactoryService will be called.
-        }
-
         if (ConfigUtils.isEmptyCollection(config.getScoreDrlList())
                 && ConfigUtils.isEmptyCollection(config.getScoreDrlFileList())) {
             if (config.getKieBaseConfigurationProperties() != null) {
@@ -75,8 +82,55 @@ public final class DrlScoreDirectorFactoryService<Solution_, Score_ extends Scor
     }
 
     @Override
-    protected DrlScoreDirectorFactory<Solution_, Score_> createScoreDirectorFactory(ScoreDirectorFactoryConfig config,
-            SolutionDescriptor<Solution_> solutionDescriptor, KieBase kieBase) {
+    public ScoreDirectorType getSupportedScoreDirectorType() {
+        return ScoreDirectorType.DRL;
+    }
+
+    private DrlScoreDirectorFactory<Solution_, Score_> buildScoreDirectorFactory(ClassLoader classLoader,
+            SolutionDescriptor<Solution_> solutionDescriptor, ScoreDirectorFactoryConfig config,
+            List<String> scoreDrlList) {
+        KieBase kieBase;
+        if (config.getGizmoKieBaseSupplier() != null) {
+            kieBase = config.getGizmoKieBaseSupplier().get();
+        } else {
+            KieHelper kieHelper = new KieHelper(PropertySpecificOption.ALLOWED)
+                    .setClassLoader(classLoader);
+            scoreDrlList.forEach(scoreDrl -> kieHelper
+                    .addResource(KieServices.get().getResources().newClassPathResource(scoreDrl, classLoader)));
+            if (!ConfigUtils.isEmptyCollection(config.getScoreDrlFileList())) {
+                for (File scoreDrlFile : config.getScoreDrlFileList()) {
+                    kieHelper.addResource(KieServices.get().getResources().newFileSystemResource(scoreDrlFile));
+                }
+            }
+            KieBaseConfiguration kieBaseConfiguration = buildKieBaseConfiguration(config, KieServices.get());
+            kieBaseConfiguration.setOption(KieBaseMutabilityOption.DISABLED); // Performance improvement.
+            try {
+                kieBase = kieHelper.build(ExecutableModelProject.class, kieBaseConfiguration);
+            } catch (Exception ex) {
+                throw new IllegalStateException("There is an error in a scoreDrl or scoreDrlFile.", ex);
+            }
+        }
+
+        if (config.isDroolsAlphaNetworkCompilationEnabled()) {
+            KieBaseUpdaterANC.generateAndSetInMemoryANC(kieBase); // Enable Alpha Network Compiler for performance.
+        }
+        return createScoreDirectorFactory(solutionDescriptor, kieBase);
+    }
+
+    private static KieBaseConfiguration buildKieBaseConfiguration(ScoreDirectorFactoryConfig config,
+            KieServices kieServices) {
+        KieBaseConfiguration kieBaseConfiguration = kieServices.newKieBaseConfiguration();
+        if (config.getKieBaseConfigurationProperties() != null) {
+            for (Map.Entry<String, String> entry : config.getKieBaseConfigurationProperties().entrySet()) {
+                kieBaseConfiguration.setProperty(entry.getKey(), entry.getValue());
+            }
+        }
+        return kieBaseConfiguration;
+    }
+
+    private DrlScoreDirectorFactory<Solution_, Score_>
+            createScoreDirectorFactory(SolutionDescriptor<Solution_> solutionDescriptor, KieBase kieBase) {
         return new DrlScoreDirectorFactory<>(solutionDescriptor, kieBase);
     }
+
 }
