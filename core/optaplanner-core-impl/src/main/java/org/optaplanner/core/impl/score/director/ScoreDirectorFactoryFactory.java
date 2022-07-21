218240f93de5f637e7fb0ee6efc8215a9aa9ae4d
Lukáš Petrovický <lukas@petrovicky.net>
2022-07-21 10:30:36 +0200

    Cleanup

--- core/optaplanner-core-impl/src/main/java/org/optaplanner/core/impl/score/director/ScoreDirectorFactoryFactory.java
+++ core/optaplanner-core-impl/src/main/java/org/optaplanner/core/impl/score/director/ScoreDirectorFactoryFactory.java
@@ -72,41 +72,39 @@ public class ScoreDirectorFactoryFactory<Solution_, Score_ extends Score<Score_>
                 new EnumMap<>(ScoreDirectorType.class);
         boolean isBavet = false;
         for (ScoreDirectorFactoryService<Solution_, Score_> service : scoreDirectorFactoryServiceLoader) {
-            /*
-             * Drools will be available if one the classpath and user did not request BAVET.
-             * Bavet will be available if on the classpath and user did not request DROOLS.
-             * The following logic deals with the decision of which to pick if both available.
-             */
             Supplier<AbstractScoreDirectorFactory<Solution_, Score_>> factory =
                     service.buildScoreDirectorFactory(classLoader, solutionDescriptor, config);
-            if (factory != null) {
-                if (service.getSupportedScoreDirectorType() == CONSTRAINT_STREAMS) {
-                    switch (service.getPriority()) {
-                        case Integer.MAX_VALUE:
-                            // Drools will be registered as the CS impl.
-                            isBavet = false;
-                            break;
-                        case Integer.MIN_VALUE:
-                            if (scoreDirectorFactorySupplierMap.containsKey(CONSTRAINT_STREAMS)) {
-                                /*
-                                 * We already have a CS service registered, and it is of a higher priority.
-                                 * This means Drools was loaded first, but Bavet is available too.
-                                 * Such situation can only happen if the user did not specify an impl type.
-                                 * Therefore, we skip Bavet as Drools is the default and already registered.
-                                 */
-                                continue;
-                            } else {
-                                // Bavet will be registered as the CS impl.
-                                isBavet = true;
-                            }
-                            break;
-                        default:
-                            throw new IllegalStateException(
-                                    "Impossible state: Unknown service priority (" + service.getPriority() + ")");
-                    }
+            if (factory != null && service.getSupportedScoreDirectorType() == CONSTRAINT_STREAMS) {
+                /*
+                 * CS-D will be available if on the classpath and user did not request BAVET.
+                 * CS-B will be available if on the classpath and user did not request DROOLS.
+                 * The following logic deals with the decision of which CS impl to pick if both available.
+                 */
+                switch (service.getPriority()) {
+                    case Integer.MAX_VALUE:
+                        // Drools will be registered as the CS impl.
+                        isBavet = false;
+                        break;
+                    case Integer.MIN_VALUE:
+                        if (scoreDirectorFactorySupplierMap.containsKey(CONSTRAINT_STREAMS)) {
+                            /*
+                             * We already have a CS service registered, and it is of a higher priority.
+                             * This means Drools was loaded first, but Bavet is available too.
+                             * Such situation can only happen if the user did not specify an impl type.
+                             * Therefore, we skip Bavet as Drools is the default and already registered.
+                             */
+                            continue;
+                        } else {
+                            // Bavet will be registered as the CS impl.
+                            isBavet = true;
+                        }
+                        break;
+                    default:
+                        throw new IllegalStateException(
+                                "Impossible state: Unknown service priority (" + service.getPriority() + ")");
                 }
-                scoreDirectorFactorySupplierMap.put(service.getSupportedScoreDirectorType(), factory);
             }
+            scoreDirectorFactorySupplierMap.put(service.getSupportedScoreDirectorType(), factory);
         }
 
         Supplier<AbstractScoreDirectorFactory<Solution_, Score_>> easyScoreDirectorFactorySupplier =
@@ -139,14 +137,12 @@ public class ScoreDirectorFactoryFactory<Solution_, Score_ extends Score<Score_>
                 validateNoGizmoKieBaseSupplier();
             }
             return constraintStreamScoreDirectorFactorySupplier.get();
-        } else {
-            String expectedModule = isBavet ? "optaplanner-constraint-streams-bavet" : "optaplanner-constraint-streams-drools";
-            if (config.getConstraintProviderClass() != null) {
-                throw new IllegalStateException("Constraint Streams requested via constraintProviderClass (" +
-                        config.getConstraintProviderClass() + ") but the supporting classes were not found on the classpath.\n"
-                        + "Maybe include org.optaplanner:" + expectedModule + " dependency in your project?\n"
-                        + "Maybe ensure your uberjar bundles META-INF/services from included JAR files?");
-            }
+        } else if (config.getConstraintProviderClass() != null) {
+            throw new IllegalStateException("Constraint Streams requested via constraintProviderClass (" +
+                    config.getConstraintProviderClass() + ") but the supporting classes were not found on the classpath.\n"
+                    + "Maybe include org.optaplanner:optaplanner-constraint-streams-drools or "
+                    + "org.optaplanner:optaplanner-constraint-streams-bavet dependency in your project?\n"
+                    + "Maybe ensure your uberjar bundles META-INF/services from included JAR files?");
         }
 
         if (drlScoreDirectorFactorySupplier != null) {
