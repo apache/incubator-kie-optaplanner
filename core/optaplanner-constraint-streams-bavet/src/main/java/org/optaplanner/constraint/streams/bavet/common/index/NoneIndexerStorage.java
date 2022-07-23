330beb30939a6610769808e3c51ddfdcfce15169
Lukáš Petrovický <lpetrovi@redhat.com>
2022-07-23 20:31:01 +0200

    Even less allocations
    Signed-off-by: Lukáš Petrovický <lpetrovi@redhat.com>

--- core/optaplanner-constraint-streams-bavet/src/main/java/org/optaplanner/constraint/streams/bavet/common/index/NoneIndexerStorage.java
+++ core/optaplanner-constraint-streams-bavet/src/main/java/org/optaplanner/constraint/streams/bavet/common/index/NoneIndexerStorage.java
@@ -1,60 +1,76 @@
 package org.optaplanner.constraint.streams.bavet.common.index;
 
-import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Objects;
 import java.util.function.BiConsumer;
 
 /**
- * Uses {@link LinkedHashMap} as tuple storage unless it is the first and only tuple,
- * in which case it uses {@link Collections#singletonMap(Object, Object)}.
- *
- * This helps avoid the overhead of creating and accessing a hash map if we only have 1 tuple in the index.
+ * Uses {@link LinkedHashMap} as tuple storage unless it is the first and only entry, in which case it uses fields.
+ * This helps avoid the overhead of creating and accessing a hash map if we only have 1 entry in the index.
+ * This situation occurs often enough that this optimization has been proven beneficial.
  */
 final class NoneIndexerStorage<Key_, Value_> {
 
+    private Key_ singletonKey;
+    private Value_ singletonValue;
     private Map<Key_, Value_> map;
     private int size = 0;
 
-    public Value_ put(Key_ tuple, Value_ value) {
-        if (map == null) {
-            map = Collections.singletonMap(tuple, value);
-            size = 1;
-            return null;
-        } else if (isSingletonMap()) {
-            Map.Entry<Key_, Value_> entry = map.entrySet().iterator().next();
-            map = new LinkedHashMap<>();
-            map.put(entry.getKey(), entry.getValue());
+    public Value_ put(Key_ key, Value_ value) {
+        if (map == null) { // We have not yet created the map.
+            if (size == 0) { // Use the fields instead of the map.
+                singletonKey = key;
+                singletonValue = value;
+                size = 1;
+                return null;
+            } else if (size == 1) { // Switch from the fields to the map.
+                map = new LinkedHashMap<>();
+                map.put(singletonKey, singletonValue);
+                singletonKey = null;
+                singletonValue = null;
+            } else {
+                throw new IllegalStateException("Impossible state: storage size (" + size + ") > 1 yet no map used.");
+            }
         }
         size += 1;
-        return map.put(tuple, value);
+        return map.put(key, value);
     }
 
-    private boolean isSingletonMap() {
-        return size == 1 && !(map instanceof LinkedHashMap);
-    }
-
-    public Value_ remove(Key_ tuple) {
-        if (isSingletonMap()) {
-            Value_ value = map.get(tuple);
-            map = null;
+    public Value_ remove(Key_ key) {
+        if (map == null) { // We're using the fields.
+            if (!Objects.equals(singletonKey, key)) {
+                return null; // Key was not found.
+            }
+            Value_ oldValue = singletonValue;
+            singletonKey = null;
+            singletonValue = null;
             size = 0;
-            return value;
+            return oldValue;
         }
-        Value_ value = map.remove(tuple);
+        Value_ value = map.remove(key);
         size -= 1;
         return value;
     }
 
-    public Value_ get(Key_ tuple) {
-        return map.get(tuple);
+    public Value_ get(Key_ key) {
+        if (map == null) { // We're using the fields.
+            if (!Objects.equals(singletonKey, key)) {
+                return null; // Key was not found.
+            }
+            return singletonValue;
+        }
+        return map.get(key);
     }
 
-    public void visit(BiConsumer<Key_, Value_> tupleValueVisitor) {
+    public void visit(BiConsumer<Key_, Value_> keyValueVisitor) {
         if (size == 0) {
             return;
+        } else if (map == null) {
+            keyValueVisitor.accept(singletonKey, singletonValue);
+            return;
         }
-        map.forEach(tupleValueVisitor);
+        map.forEach(keyValueVisitor);
     }
 
     public boolean isEmpty() {
