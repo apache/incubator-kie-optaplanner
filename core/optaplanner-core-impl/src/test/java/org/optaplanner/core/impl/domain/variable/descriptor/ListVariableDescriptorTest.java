e9a76d559aeee28cbecbea117918d1c40607349d
Jiří Locker <jiri.locker@gmail.com>
2022-08-12 14:46:04 +0200

    Test that ValueRangeProvider on an entity is not allowed

--- core/optaplanner-core-impl/src/test/java/org/optaplanner/core/impl/domain/variable/descriptor/ListVariableDescriptorTest.java
+++ core/optaplanner-core-impl/src/test/java/org/optaplanner/core/impl/domain/variable/descriptor/ListVariableDescriptorTest.java
@@ -2,6 +2,7 @@ package org.optaplanner.core.impl.domain.variable.descriptor;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 import java.util.List;
 
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.optaplanner.core.impl.testdata.domain.list.TestdataListEntity;
 import org.optaplanner.core.impl.testdata.domain.list.TestdataListSolution;
 import org.optaplanner.core.impl.testdata.domain.list.TestdataListValue;
+import org.optaplanner.core.impl.testdata.domain.list.valuerange.TestdataListEntityProvidingSolution;
 import org.optaplanner.core.impl.testdata.domain.list.valuerange.TestdataListEntityWithArrayValueRange;
 
 class ListVariableDescriptorTest {
@@ -26,5 +28,12 @@ class ListVariableDescriptorTest {
     void buildDescriptorWithArrayValueRange() {
         assertThatCode(TestdataListEntityWithArrayValueRange::buildVariableDescriptorForValueList)
                 .doesNotThrowAnyException();
+    }
+
+    @Test
+    void requiresEntityIndependentValueRange() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(TestdataListEntityProvidingSolution::buildSolutionDescriptor)
+                .withMessageContaining("is not supported with a list variable");
     }
 }
