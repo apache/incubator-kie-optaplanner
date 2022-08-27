e6169d26f64ceeb43b1f575273bd71bb77d27ecf
Lukáš Petrovický <lukas@petrovicky.net>
2022-08-27 11:58:22 +0200

    Make the CV fully thread-safe

--- optaplanner-test/src/main/java/org/optaplanner/test/impl/score/stream/DefaultConstraintVerifier.java
+++ optaplanner-test/src/main/java/org/optaplanner/test/impl/score/stream/DefaultConstraintVerifier.java
@@ -1,23 +1,12 @@
 package org.optaplanner.test.impl.score.stream;
 
 import static java.util.Objects.requireNonNull;
-import static org.optaplanner.core.api.score.stream.ConstraintStreamImplType.DROOLS;
 
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.Objects;
-import java.util.ServiceLoader;
 import java.util.UUID;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReadWriteLock;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.function.BiFunction;
-import java.util.stream.Collectors;
 
 import org.optaplanner.constraint.streams.common.AbstractConstraintStreamScoreDirectorFactory;
-import org.optaplanner.constraint.streams.common.AbstractConstraintStreamScoreDirectorFactoryService;
 import org.optaplanner.core.api.score.Score;
 import org.optaplanner.core.api.score.stream.Constraint;
 import org.optaplanner.core.api.score.stream.ConstraintFactory;
@@ -25,8 +14,6 @@ import org.optaplanner.core.api.score.stream.ConstraintProvider;
 import org.optaplanner.core.api.score.stream.ConstraintStreamImplType;
 import org.optaplanner.core.config.util.ConfigUtils;
 import org.optaplanner.core.impl.domain.solution.descriptor.SolutionDescriptor;
-import org.optaplanner.core.impl.score.director.ScoreDirectorFactoryService;
-import org.optaplanner.core.impl.score.director.ScoreDirectorType;
 import org.optaplanner.test.api.score.stream.ConstraintVerifier;
 
 public final class DefaultConstraintVerifier<ConstraintProvider_ extends ConstraintProvider, Solution_, Score_ extends Score<Score_>>
@@ -36,30 +23,23 @@ public final class DefaultConstraintVerifier<ConstraintProvider_ extends Constra
      * Exists so that people can not, even by accident, pick the same constraint ID as the default map key.
      */
     private final String defaultScoreDirectorFactoryMapKey = UUID.randomUUID().toString();
-    private final Map<String, AbstractConstraintStreamScoreDirectorFactory<Solution_, Score_>> scoreDirectorFactoryMap =
-            new HashMap<>();
 
-    private final ServiceLoader<ScoreDirectorFactoryService<Solution_, Score_>> serviceLoader;
     private final ConstraintProvider_ constraintProvider;
-    private final SolutionDescriptor<Solution_> solutionDescriptor;
-    private final Lock scoreDirectorFactoryMapReadLock;
-    private final Lock scoreDirectorFactoryMapWriteLock;
+    /**
+     * {@link java.util.ServiceLoader} is sensitive to classloaders.
+     * Therefore we ensure that all SPI operations are bound to the current thread's classloader.
+     * This also allows us to avoid thread safety concerns in the {@link ScoreDirectorFactoryCache}.
+     */
+    private final ThreadLocal<ScoreDirectorFactoryCache<ConstraintProvider_, Solution_, Score_>> scoreDirectorFactoryContainerThreadLocal;
 
-    private ConstraintStreamImplType constraintStreamImplType;
-    private Boolean droolsAlphaNetworkCompilationEnabled;
+    // Need to be volatile as they will be accessed from the thread-local score director factory cache.
+    private volatile ConstraintStreamImplType constraintStreamImplType;
+    private volatile Boolean droolsAlphaNetworkCompilationEnabled;
 
     public DefaultConstraintVerifier(ConstraintProvider_ constraintProvider, SolutionDescriptor<Solution_> solutionDescriptor) {
-        ServiceLoader uncastServiceLoader = ServiceLoader.load(ScoreDirectorFactoryService.class);
-        this.serviceLoader = uncastServiceLoader;
         this.constraintProvider = constraintProvider;
-        this.solutionDescriptor = solutionDescriptor;
-        /*
-         * Creating score director factory is expensive and possibly not thread-safe.
-         * We want to make sure these operations are mutually exclusive.
-         */
-        ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
-        this.scoreDirectorFactoryMapReadLock = readWriteLock.readLock();
-        this.scoreDirectorFactoryMapWriteLock = readWriteLock.writeLock();
+        this.scoreDirectorFactoryContainerThreadLocal =
+                ThreadLocal.withInitial(() -> new ScoreDirectorFactoryCache<>(this, solutionDescriptor));
     }
 
     public ConstraintStreamImplType getConstraintStreamImplType() {
@@ -71,17 +51,11 @@ public final class DefaultConstraintVerifier<ConstraintProvider_ extends Constra
             ConstraintStreamImplType constraintStreamImplType) {
         requireNonNull(constraintStreamImplType);
         this.constraintStreamImplType = constraintStreamImplType;
-        clearScoreDirectorFactoryMap();
         return this;
     }
 
-    private void clearScoreDirectorFactoryMap() {
-        scoreDirectorFactoryMapWriteLock.lock();
-        try {
-            this.scoreDirectorFactoryMap.clear(); // All score director factories are invalidated.
-        } finally {
-            scoreDirectorFactoryMapWriteLock.unlock();
-        }
+    public Boolean getDroolsAlphaNetworkCompilationEnabled() {
+        return droolsAlphaNetworkCompilationEnabled;
     }
 
     public boolean isDroolsAlphaNetworkCompilationEnabled() {
@@ -92,7 +66,6 @@ public final class DefaultConstraintVerifier<ConstraintProvider_ extends Constra
     public ConstraintVerifier<ConstraintProvider_, Solution_> withDroolsAlphaNetworkCompilationEnabled(
             boolean droolsAlphaNetworkCompilationEnabled) {
         this.droolsAlphaNetworkCompilationEnabled = droolsAlphaNetworkCompilationEnabled;
-        clearScoreDirectorFactoryMap();
         return this;
     }
 
@@ -105,94 +78,15 @@ public final class DefaultConstraintVerifier<ConstraintProvider_ extends Constra
             BiFunction<ConstraintProvider_, ConstraintFactory, Constraint> constraintFunction) {
         requireNonNull(constraintFunction);
         AbstractConstraintStreamScoreDirectorFactory<Solution_, Score_> scoreDirectorFactory =
-                getScoreDirectorFactory(constraintFunction);
+                scoreDirectorFactoryContainerThreadLocal.get().getScoreDirectorFactory(constraintFunction, constraintProvider);
         return new DefaultSingleConstraintVerification<>(scoreDirectorFactory);
-    }
-
-    private AbstractConstraintStreamScoreDirectorFactory<Solution_, Score_> getScoreDirectorFactory(
-            BiFunction<ConstraintProvider_, ConstraintFactory, Constraint> constraintFunction) {
-        AbstractConstraintStreamScoreDirectorFactoryService<Solution_, Score_> scoreDirectorFactoryService =
-                getScoreDirectorFactoryService(serviceLoader, constraintStreamImplType);
-        // Creating score director factory is potentially expensive; cache it per constraint.
-        Constraint constraint = constraintFunction.apply(constraintProvider,
-                scoreDirectorFactoryService.buildConstraintFactory(solutionDescriptor));
-        String constraintId = constraint.getConstraintId();
-        return getOrCreateScoreDirectorFactory(scoreDirectorFactoryService, constraintId,
-                constraintFactory -> new Constraint[] {
-                        constraintFunction.apply(constraintProvider, constraintFactory)
-                });
-    }
-
-    /**
-     * Ensures that score director factories are created in a thread-safe manner,
-     * and that for each key only one instance is ever created.
-     *
-     * @param scoreDirectorFactoryService never null, used to create the score director factory
-     * @param key never null, unique identifier of the constraint provider
-     * @param constraintProvider never null, the constraint provider to be used by the score director factory
-     * @return never null
-     */
-    private AbstractConstraintStreamScoreDirectorFactory<Solution_, Score_> getOrCreateScoreDirectorFactory(
-            AbstractConstraintStreamScoreDirectorFactoryService<Solution_, Score_> scoreDirectorFactoryService,
-            String key, ConstraintProvider constraintProvider) {
-        scoreDirectorFactoryMapReadLock.lock();
-        try { // If already calculated, don't require the write lock.
-            AbstractConstraintStreamScoreDirectorFactory<Solution_, Score_> scoreDirectorFactory =
-                    scoreDirectorFactoryMap.get(key);
-            if (scoreDirectorFactory != null) {
-                return scoreDirectorFactory;
-            }
-        } finally {
-            scoreDirectorFactoryMapReadLock.unlock();
-        }
-        scoreDirectorFactoryMapWriteLock.lock();
-        try {
-            return scoreDirectorFactoryMap.computeIfAbsent(key, k -> {
-                boolean isDroolsAlphaNetworkCompilationEnabled =
-                        droolsAlphaNetworkCompilationEnabled == null
-                                ? scoreDirectorFactoryService.supportsImplType(DROOLS)
-                                        && isDroolsAlphaNetworkCompilationEnabled()
-                                : droolsAlphaNetworkCompilationEnabled;
-                return scoreDirectorFactoryService.buildScoreDirectorFactory(solutionDescriptor, constraintProvider,
-                        isDroolsAlphaNetworkCompilationEnabled);
-            });
-        } finally {
-            scoreDirectorFactoryMapWriteLock.unlock();
-        }
-    }
-
-    private static <Solution_, Score_ extends Score<Score_>>
-            AbstractConstraintStreamScoreDirectorFactoryService<Solution_, Score_>
-            getScoreDirectorFactoryService(ServiceLoader<ScoreDirectorFactoryService<Solution_, Score_>> serviceLoader,
-                    ConstraintStreamImplType constraintStreamImplType) {
-        List<AbstractConstraintStreamScoreDirectorFactoryService<Solution_, Score_>> services = serviceLoader.stream()
-                .map(ServiceLoader.Provider::get)
-                .filter(s -> s.getSupportedScoreDirectorType() == ScoreDirectorType.CONSTRAINT_STREAMS)
-                .map(s -> (AbstractConstraintStreamScoreDirectorFactoryService<Solution_, Score_>) s)
-                .filter(s -> constraintStreamImplType == null || s.supportsImplType(constraintStreamImplType))
-                .sorted(Comparator
-                        .comparingInt(
-                                (AbstractConstraintStreamScoreDirectorFactoryService<Solution_, Score_> service) -> service
-                                        .getPriority())
-                        .reversed()) // CS-D will be picked if both are available.
-                .collect(Collectors.toList());
-        if (services.isEmpty()) {
-            throw new IllegalStateException(
-                    "Constraint Streams implementation was not found on the classpath.\n"
-                            + "Maybe include org.optaplanner:optaplanner-constraint-streams-drools dependency "
-                            + "or org.optaplanner:optaplanner-constraint-streams-bavet in your project?\n"
-                            + "Maybe ensure your uberjar bundles META-INF/services from included JAR files?");
-        }
-        return services.get(0);
     }
 
     @Override
     public DefaultMultiConstraintVerification<Solution_, Score_> verifyThat() {
-        AbstractConstraintStreamScoreDirectorFactoryService<Solution_, Score_> scoreDirectorFactoryService =
-                getScoreDirectorFactoryService(serviceLoader, constraintStreamImplType);
         AbstractConstraintStreamScoreDirectorFactory<Solution_, Score_> scoreDirectorFactory =
-                getOrCreateScoreDirectorFactory(scoreDirectorFactoryService, defaultScoreDirectorFactoryMapKey,
-                        constraintProvider);
+                scoreDirectorFactoryContainerThreadLocal.get()
+                        .getScoreDirectorFactory(defaultScoreDirectorFactoryMapKey, constraintProvider);
         return new DefaultMultiConstraintVerification<>(scoreDirectorFactory, constraintProvider);
     }
 
---
+++ optaplanner-test/src/main/java/org/optaplanner/test/impl/score/stream/ScoreDirectorFactoryCache.java
@@ -0,0 +1,151 @@
+package org.optaplanner.test.impl.score.stream;
+
+import static org.optaplanner.core.api.score.stream.ConstraintStreamImplType.BAVET;
+import static org.optaplanner.core.api.score.stream.ConstraintStreamImplType.DROOLS;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.ServiceLoader;
+import java.util.function.BiFunction;
+
+import org.optaplanner.constraint.streams.common.AbstractConstraintStreamScoreDirectorFactory;
+import org.optaplanner.constraint.streams.common.AbstractConstraintStreamScoreDirectorFactoryService;
+import org.optaplanner.core.api.score.Score;
+import org.optaplanner.core.api.score.stream.Constraint;
+import org.optaplanner.core.api.score.stream.ConstraintFactory;
+import org.optaplanner.core.api.score.stream.ConstraintProvider;
+import org.optaplanner.core.api.score.stream.ConstraintStreamImplType;
+import org.optaplanner.core.impl.domain.solution.descriptor.SolutionDescriptor;
+import org.optaplanner.core.impl.score.director.ScoreDirectorFactoryService;
+import org.optaplanner.core.impl.score.director.ScoreDirectorType;
+
+/**
+ * Designed for access from a single thread.
+ *
+ * @param <ConstraintProvider_>
+ * @param <Solution_>
+ * @param <Score_>
+ */
+final class ScoreDirectorFactoryCache<ConstraintProvider_ extends ConstraintProvider, Solution_, Score_ extends Score<Score_>> {
+
+    /**
+     * Score director factory creation is expensive; we cache it.
+     * The cache needs to be recomputed every time that the parent's configuration changes.
+     */
+    private final Map<String, AbstractConstraintStreamScoreDirectorFactory<Solution_, Score_>> scoreDirectorFactoryMap =
+            new HashMap<>();
+
+    private final DefaultConstraintVerifier<ConstraintProvider_, Solution_, Score_> parent;
+    private final SolutionDescriptor<Solution_> solutionDescriptor;
+    private final ServiceLoader<ScoreDirectorFactoryService<Solution_, Score_>> serviceLoader;
+
+    private ConstraintStreamImplType lastKnownConstraintStreamImplType = null;
+    private boolean lastKnownDroolsAlphaNetworkCompilationEnabled = false;
+
+    public ScoreDirectorFactoryCache(DefaultConstraintVerifier<ConstraintProvider_, Solution_, Score_> parent,
+            SolutionDescriptor<Solution_> solutionDescriptor) {
+        this.parent = Objects.requireNonNull(parent);
+        this.solutionDescriptor = Objects.requireNonNull(solutionDescriptor);
+        ServiceLoader uncastServiceLoader = ServiceLoader.load(ScoreDirectorFactoryService.class);
+        this.serviceLoader = uncastServiceLoader;
+    }
+
+    private AbstractConstraintStreamScoreDirectorFactoryService<Solution_, Score_> getScoreDirectorFactoryService() {
+        ConstraintStreamImplType constraintStreamImplType = parent.getConstraintStreamImplType();
+        // CS-D will be picked if both are available.
+        return serviceLoader.stream()
+                .map(ServiceLoader.Provider::get)
+                .filter(s -> s.getSupportedScoreDirectorType() == ScoreDirectorType.CONSTRAINT_STREAMS)
+                .map(s -> (AbstractConstraintStreamScoreDirectorFactoryService<Solution_, Score_>) s)
+                .filter(s -> constraintStreamImplType == null || s.supportsImplType(constraintStreamImplType))
+                .max(Comparator.comparingInt(ScoreDirectorFactoryService::getPriority))
+                .orElseThrow(() -> new IllegalStateException(
+                        "Constraint Streams implementation was not found on the classpath.\n"
+                                + "Maybe include org.optaplanner:optaplanner-constraint-streams-drools dependency "
+                                + "or org.optaplanner:optaplanner-constraint-streams-bavet in your project?\n"
+                                + "Maybe ensure your uberjar bundles META-INF/services from included JAR files?"));
+    }
+
+    /**
+     * Retrieve {@link AbstractConstraintStreamScoreDirectorFactory} from the cache,
+     * or create and cache a new instance using the {@link AbstractConstraintStreamScoreDirectorFactoryService}.
+     * Cache key is the ID of the single constraint returned by calling the constraintFunction.
+     *
+     * @param constraintFunction never null, determines the single constraint to be used from the constraint provider
+     * @param constraintProvider never null, determines the constraint provider to be used
+     * @return never null
+     */
+    public AbstractConstraintStreamScoreDirectorFactory<Solution_, Score_> getScoreDirectorFactory(
+            BiFunction<ConstraintProvider_, ConstraintFactory, Constraint> constraintFunction,
+            ConstraintProvider_ constraintProvider) {
+        AbstractConstraintStreamScoreDirectorFactoryService<Solution_, Score_> scoreDirectorFactoryService =
+                getScoreDirectorFactoryService();
+        Constraint constraint = constraintFunction.apply(constraintProvider,
+                scoreDirectorFactoryService.buildConstraintFactory(solutionDescriptor));
+        String constraintId = constraint.getConstraintId();
+        return getScoreDirectorFactory(constraintId,
+                constraintFactory -> new Constraint[] {
+                        constraint
+                });
+    }
+
+    /**
+     * Retrieve {@link AbstractConstraintStreamScoreDirectorFactory} from the cache,
+     * or create and cache a new instance using the {@link AbstractConstraintStreamScoreDirectorFactoryService}.
+     *
+     * @param key never null, unique identifier of the factory in the cache
+     * @param constraintProvider never null, constraint provider to create the factory from; ignored on cache hit
+     * @return never null
+     */
+    public AbstractConstraintStreamScoreDirectorFactory<Solution_, Score_> getScoreDirectorFactory(String key,
+            ConstraintProvider constraintProvider) {
+        return scoreDirectorFactoryMap.compute(key, (k, v) -> {
+            AbstractConstraintStreamScoreDirectorFactoryService<Solution_, Score_> scoreDirectorFactoryService =
+                    getScoreDirectorFactoryService();
+            if (v == null || didParentConfigurationChange(scoreDirectorFactoryService)) {
+                return createScoreDirectorFactory(scoreDirectorFactoryService, constraintProvider);
+            }
+            return v;
+        });
+    }
+
+    private boolean didParentConfigurationChange(
+            AbstractConstraintStreamScoreDirectorFactoryService<Solution_, Score_> scoreDirectorFactoryService) {
+        boolean currentDroolsAlphaNetworkCompilationEnabled =
+                determineDroolsAlphaNetworkCompilationEnabled(scoreDirectorFactoryService);
+        if (currentDroolsAlphaNetworkCompilationEnabled != lastKnownDroolsAlphaNetworkCompilationEnabled) {
+            return true;
+        }
+        ConstraintStreamImplType currentConstraintStreamImplType =
+                determineConstraintStreamImplType(scoreDirectorFactoryService);
+        return currentConstraintStreamImplType != lastKnownConstraintStreamImplType;
+    }
+
+    private boolean determineDroolsAlphaNetworkCompilationEnabled(
+            AbstractConstraintStreamScoreDirectorFactoryService<Solution_, Score_> scoreDirectorFactoryService) {
+        Boolean parentDroolsAlphaNetworkCompilationEnabled = parent.getDroolsAlphaNetworkCompilationEnabled();
+        return parentDroolsAlphaNetworkCompilationEnabled == null
+                ? determineConstraintStreamImplType(scoreDirectorFactoryService) == DROOLS
+                        && parent.isDroolsAlphaNetworkCompilationEnabled()
+                : parentDroolsAlphaNetworkCompilationEnabled;
+    }
+
+    private ConstraintStreamImplType determineConstraintStreamImplType(
+            AbstractConstraintStreamScoreDirectorFactoryService<Solution_, Score_> scoreDirectorFactoryService) {
+        return scoreDirectorFactoryService.supportsImplType(BAVET) ? BAVET : DROOLS;
+    }
+
+    private AbstractConstraintStreamScoreDirectorFactory<Solution_, Score_> createScoreDirectorFactory(
+            AbstractConstraintStreamScoreDirectorFactoryService<Solution_, Score_> scoreDirectorFactoryService,
+            ConstraintProvider constraintProvider) {
+        this.lastKnownConstraintStreamImplType = determineConstraintStreamImplType(scoreDirectorFactoryService);
+        this.lastKnownDroolsAlphaNetworkCompilationEnabled =
+                determineDroolsAlphaNetworkCompilationEnabled(scoreDirectorFactoryService);
+
+        return scoreDirectorFactoryService.buildScoreDirectorFactory(solutionDescriptor, constraintProvider,
+                this.lastKnownDroolsAlphaNetworkCompilationEnabled);
+    }
+
+}
