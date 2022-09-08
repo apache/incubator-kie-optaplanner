5467c99e34af768fc1ebf52d35478d372f553f01
Jiří Locker <jiri.locker@gmail.com>
2022-09-08 13:40:36 +0200

    Move empty private constructor at the end of the class

--- core/optaplanner-core-impl/src/main/java/org/optaplanner/core/impl/heuristic/selector/move/generic/list/TriangularNumbers.java
+++ core/optaplanner-core-impl/src/main/java/org/optaplanner/core/impl/heuristic/selector/move/generic/list/TriangularNumbers.java
@@ -2,17 +2,14 @@ package org.optaplanner.core.impl.heuristic.selector.move.generic.list;
 
 final class TriangularNumbers {
 
-    /**
-     * Don't let anyone instantiate this class.
-     */
-    private TriangularNumbers() {
-    }
-
     static int nthTriangle(int n) {
         return Math.multiplyExact(n, n + 1) / 2;
     }
 
     static double triangularRoot(int x) {
         return (Math.sqrt(8 * x + 1) - 1) / 2;
+    }
+
+    private TriangularNumbers() {
     }
 }
