c23cba1ad16088fdd243b8e3986fba25cf51b71b
Jiří Locker <jiri.locker@gmail.com>
2022-09-15 10:53:56 +0200

    Improve VariableListener Javadoc
    Co-authored-by: Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>

--- core/optaplanner-core-impl/src/main/java/org/optaplanner/core/api/domain/variable/VariableListener.java
+++ core/optaplanner-core-impl/src/main/java/org/optaplanner/core/api/domain/variable/VariableListener.java
@@ -13,10 +13,10 @@ import org.optaplanner.core.api.score.director.ScoreDirector;
 public interface VariableListener<Solution_, Entity_> extends AbstractVariableListener<Solution_, Entity_> {
 
     /**
-     * When set to {@code true}, this has a slight performance loss in Planner.
-     * When set to {@code false}, it's often easier to make the listener implementation correct and fast.
+     * When set to {@code true}, this has a performance loss.
+     * When set to {@code false}, it's easier to make the listener implementation correct and fast.
      *
-     * @return true to guarantee that each of the before/after methods will only be called once per entity instance
+     * @return true to guarantee that each of the before/after methods is only called once per entity instance
      *         per operation type (add, change or remove).
      */
     default boolean requiresUniqueEntityEvents() {
