c67a1c930899fd34e00a8408704a1c60c6116bdd
Jiří Locker <jiri.locker@gmail.com>
2022-09-15 09:49:03 +0200

    Avoid int overflow in triangularRoot()

--- core/optaplanner-core-impl/src/test/java/org/optaplanner/core/impl/heuristic/selector/move/generic/list/TriangularNumbersTest.java
+++ core/optaplanner-core-impl/src/test/java/org/optaplanner/core/impl/heuristic/selector/move/generic/list/TriangularNumbersTest.java
@@ -1,15 +1,24 @@
 package org.optaplanner.core.impl.heuristic.selector.move.generic.list;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 import java.util.stream.Stream;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 class TriangularNumbersTest {
+
+    @Test
+    void overflow() {
+        assertThatThrownBy(() -> TriangularNumbers.nthTriangle(TriangularNumbers.HIGHEST_SAFE_N + 1))
+                .isInstanceOf(ArithmeticException.class)
+                .hasMessage("integer overflow");
+    }
 
     static Stream<Arguments> nthProvider() {
         return Stream.of(
@@ -18,7 +27,9 @@ class TriangularNumbersTest {
                 arguments(2, 3),
                 arguments(3, 6),
                 arguments(4, 10),
-                arguments(5, 15));
+                arguments(5, 15),
+                arguments(23169, 268_412_865),
+                arguments(TriangularNumbers.HIGHEST_SAFE_N, 1_073_720_970));
     }
 
     @ParameterizedTest
@@ -40,7 +51,8 @@ class TriangularNumbersTest {
                 arguments(5, 2),
                 arguments(7, 3),
                 arguments(8, 3),
-                arguments(9, 3));
+                arguments(9, 3),
+                arguments(268_435_456, 23169));
     }
 
     @ParameterizedTest
