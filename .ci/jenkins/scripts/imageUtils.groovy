5b305e551af3ba38d6636b8d4e52e0ebccd8bca6
Radovan Synek <rsynek@redhat.com>
2022-09-15 10:01:12 +0200

    Use quay.io token as credentials

--- .ci/jenkins/scripts/imageUtils.groovy
+++ .ci/jenkins/scripts/imageUtils.groovy
@@ -50,8 +50,7 @@ boolean removeQuayTag(String namespace, String imageName, String tag) {
     echo "Removing a temporary image tag ${image}"
     try {
         def output = 'false'
-       // withCredentials([usernamePassword(credentialsId: getOperatorImageRegistryCredentials(), usernameVariable: 'QUAY_USER', passwordVariable: 'QUAY_TOKEN')]) {
-        withCredentials([string(credentialsId: 'rsynek_registry_oauth_token', variable: 'QUAY_TOKEN')]) {
+        withCredentials([usernamePassword(credentialsId: getOperatorImageRegistryCredentials(), usernameVariable: 'QUAY_USER', passwordVariable: 'QUAY_TOKEN')]) {
             output = sh(returnStdout: true, script: "curl -H 'Content-Type: application/json' -H 'Authorization: Bearer ${QUAY_TOKEN}' -X DELETE https://quay.io/api/v1/repository/${namespace}/${imageName}/tag/${tag}").trim()
             if (output != '') {
                 echo "$output"
