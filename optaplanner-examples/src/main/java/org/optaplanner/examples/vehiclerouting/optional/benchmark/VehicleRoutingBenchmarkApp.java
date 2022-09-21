e3423495df14d9282c348f2edb858811fa3e6511
Jiří Locker <jiri.locker@gmail.com>
2022-09-21 22:20:29 +0200

    Update benchmark configs

--- optaplanner-examples/src/main/java/org/optaplanner/examples/vehiclerouting/optional/benchmark/VehicleRoutingBenchmarkApp.java
+++ optaplanner-examples/src/main/java/org/optaplanner/examples/vehiclerouting/optional/benchmark/VehicleRoutingBenchmarkApp.java
@@ -16,7 +16,7 @@ public class VehicleRoutingBenchmarkApp extends CommonBenchmarkApp {
                         "org/optaplanner/examples/vehiclerouting/optional/benchmark/vehicleRoutingStepLimitBenchmarkConfig.xml"),
                 new ArgOption("scoreDirector",
                         "org/optaplanner/examples/vehiclerouting/optional/benchmark/vehicleRoutingScoreDirectorBenchmarkConfig.xml")
-        // FIXME nearby not yet available with list variable.
+        // FIXME Nearby selection for a list variable not yet supported: https://issues.redhat.com/browse/PLANNER-2814
         // new ArgOption("template",
         //         "org/optaplanner/examples/vehiclerouting/optional/benchmark/vehicleRoutingBenchmarkConfigTemplate.xml.ftl",
         //         true)
