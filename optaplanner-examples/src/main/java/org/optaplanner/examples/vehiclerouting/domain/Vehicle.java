233fac2a2876001e0d3e8a72664d50563bfa9462
Radovan Synek <rsynek@redhat.com>
2022-11-14 12:55:40 +0100

    Fix references after deserialization

--- optaplanner-examples/src/main/java/org/optaplanner/examples/vehiclerouting/domain/Vehicle.java
+++ optaplanner-examples/src/main/java/org/optaplanner/examples/vehiclerouting/domain/Vehicle.java
@@ -8,9 +8,12 @@ import org.optaplanner.core.api.domain.variable.PlanningListVariable;
 import org.optaplanner.examples.common.domain.AbstractPersistableJackson;
 import org.optaplanner.examples.vehiclerouting.domain.location.Location;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
 @PlanningEntity
+@JsonIdentityInfo(scope = Vehicle.class, generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 public class Vehicle extends AbstractPersistableJackson {
 
     protected int capacity;
--- optaplanner-examples/src/main/java/org/optaplanner/examples/vehiclerouting/persistence/VehicleRoutingSolutionFileIO.java
+++ optaplanner-examples/src/main/java/org/optaplanner/examples/vehiclerouting/persistence/VehicleRoutingSolutionFileIO.java
@@ -1,6 +1,8 @@
 package org.optaplanner.examples.vehiclerouting.persistence;
 
 import java.io.File;
+import java.util.LinkedHashMap;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import org.optaplanner.examples.vehiclerouting.domain.VehicleRoutingSolution;
@@ -16,16 +18,35 @@ public class VehicleRoutingSolutionFileIO extends JacksonSolutionFileIO<VehicleR
     @Override
     public VehicleRoutingSolution read(File inputSolutionFile) {
         VehicleRoutingSolution vehicleRoutingSolution = super.read(inputSolutionFile);
-        /*
-         * Replace the duplicate MrMachine instances in the machineMoveCostMap by references to instances from
-         * the machineList.
-         */
-        deduplicateEntities(vehicleRoutingSolution, solution -> vehicleRoutingSolution.getLocationList()
-                .stream()
+
+        var roadLocationList = vehicleRoutingSolution.getLocationList().stream()
                 .filter(location -> location instanceof RoadLocation)
                 .map(location -> (RoadLocation) location)
-                .collect(Collectors.toList()),
-                RoadLocation::getId, RoadLocation::getTravelDistanceMap, RoadLocation::setTravelDistanceMap);
+                .collect(Collectors.toList());
+        var locationsById = roadLocationList.stream()
+                .collect(Collectors.toMap(RoadLocation::getId, Function.identity()));
+        /*
+         * Replace the duplicate RoadLocation instances in the travelDistanceMap by references to instances from
+         * the locationList.
+         */
+        for (RoadLocation roadLocation : roadLocationList) {
+            var originalMap = roadLocation.getTravelDistanceMap();
+            if (originalMap.isEmpty()) {
+                continue;
+            }
+            var newMap = new LinkedHashMap<RoadLocation, Double>(originalMap.size());
+            originalMap.forEach(
+                    (originalLocation, distance) -> newMap.put(locationsById.get(originalLocation.getId()), distance));
+            roadLocation.setTravelDistanceMap(newMap);
+        }
+
+        // Customers and depots have locations as well.
+        if (!roadLocationList.isEmpty()) {
+            vehicleRoutingSolution.getCustomerList()
+                    .forEach(customer -> customer.setLocation(locationsById.get(customer.getLocation().getId())));
+            vehicleRoutingSolution.getDepotList()
+                    .forEach(depot -> depot.setLocation(locationsById.get(depot.getLocation().getId())));
+        }
         return vehicleRoutingSolution;
     }
 
