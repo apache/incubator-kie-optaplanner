165383b7bf8abe3bc9f07920760e1e94aa8cc8c1
Lukáš Petrovický <lukas@petrovicky.net>
2022-11-27 13:41:54 +0100

    Add deprecation notices for all XStream classes
    Signed-off-by: Lukáš Petrovický <lukas@petrovicky.net>

--- optaplanner-persistence/optaplanner-persistence-xstream/src/main/java/org/optaplanner/persistence/xstream/api/score/package-info.java
+++ optaplanner-persistence/optaplanner-persistence-xstream/src/main/java/org/optaplanner/persistence/xstream/api/score/package-info.java
@@ -1,5 +1,8 @@
 
 /**
  * XStream bindings for {@link org.optaplanner.core.api.score.Score}.
+ * 
+ * @deprecated Prefer JAXB for serialization into XML.
  */
+@Deprecated(forRemoval = true)
 package org.optaplanner.persistence.xstream.api.score;
---
+++ optaplanner-persistence/optaplanner-persistence-xstream/src/main/java/org/optaplanner/persistence/xstream/runtime/graal/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @deprecated Prefer JAXB for serialization into XML.
+ */
+@Deprecated(forRemoval = true)
+package org.optaplanner.persistence.xstream.runtime.graal;
