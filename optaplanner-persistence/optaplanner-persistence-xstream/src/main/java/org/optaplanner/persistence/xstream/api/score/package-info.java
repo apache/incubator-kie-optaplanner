8025a769088c3125d7e456da957b0de3241c31ad
Lukáš Petrovický <lukas@petrovicky.net>
2022-11-28 08:37:17 +0100

    Make it compile with JDK 11, part 2
    Signed-off-by: Lukáš Petrovický <lukas@petrovicky.net>

--- optaplanner-persistence/optaplanner-persistence-xstream/src/main/java/org/optaplanner/persistence/xstream/api/score/package-info.java
+++
@@ -1,8 +0,0 @@
-
-/**
- * XStream bindings for {@link org.optaplanner.core.api.score.Score}.
- * 
- * @deprecated Prefer JAXB for serialization into XML.
- */
-@Deprecated(forRemoval = true)
-package org.optaplanner.persistence.xstream.api.score;
