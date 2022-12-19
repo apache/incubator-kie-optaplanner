1258ce0eb0d70fd2e73b3464be170e0e9ff6f9cc
Radovan Synek <rsynek@redhat.com>
2022-12-19 13:10:50 +0100

    Refactor JAXB import in package-info.java

--- core/optaplanner-core-impl/src/main/java/org/optaplanner/core/api/score/stream/package-info.java
+++ core/optaplanner-core-impl/src/main/java/org/optaplanner/core/api/score/stream/package-info.java
@@ -1,5 +1,5 @@
 
-@javax.xml.bind.annotation.XmlSchema(namespace = SolverConfig.XML_NAMESPACE)
+@XmlSchema(namespace = SolverConfig.XML_NAMESPACE)
 package org.optaplanner.core.api.score.stream;
 
 import org.optaplanner.core.config.solver.SolverConfig;
