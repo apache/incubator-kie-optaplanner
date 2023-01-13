b016753a7ca5a3fa4b8b0b178d0e9efd17d14571
Lukáš Petrovický <lukas@petrovicky.net>
2023-01-13 17:55:07 +0100

    Javadoc
    Signed-off-by: Lukáš Petrovický <lukas@petrovicky.net>

--- core/optaplanner-core-impl/src/main/java/org/optaplanner/core/impl/heuristic/selector/entity/EntitySelector.java
+++ core/optaplanner-core-impl/src/main/java/org/optaplanner/core/impl/heuristic/selector/entity/EntitySelector.java
@@ -9,7 +9,7 @@ import org.optaplanner.core.impl.heuristic.selector.ListIterableSelector;
 /**
  * Selects instances of 1 {@link PlanningEntity} annotated class.
  * It is expected that if two instances share the same properties,
- * they will be {@link Object#equals(Object) equal} to one another.
+ * they are {@link Object#equals(Object) equal} to one another.
  * This is necessary for proper performance of caches, such as pillar cache.
  *
  * @see AbstractEntitySelector
