723291d28d8eb76dd0fe7202b47bae6ee50d5e0d
Lukáš Petrovický <lukas@petrovicky.net>
2023-02-09 09:04:04 +0100

    One more todo
    Signed-off-by: Lukáš Petrovický <lukas@petrovicky.net>

---
+++ core/optaplanner-core-impl/src/main/java/org/optaplanner/core/impl/heuristic/selector/move/generic/list/kopt/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * TODO let's add a generic description of this move and the basic principles here;
+ *  let's also include references to the paper and other source materials
+ */
+package org.optaplanner.core.impl.heuristic.selector.move.generic.list.kopt;
