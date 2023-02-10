95518b0d7db5ba828c7b8b8c3f0547b64fdd9845
Radovan Synek <rsynek@redhat.com>
2023-02-10 16:19:23 +0100

    Migrate PR only for the 8 stream

--- .ci/jenkins/dsl/jobs.groovy
+++ .ci/jenkins/dsl/jobs.groovy
@@ -34,7 +34,7 @@ setupProjectNightlyJob()
 setupProjectReleaseJob()
 setupProjectPostReleaseJob()
 
-if (Utils.isMainBranch(this) && getOptaPlannerLatestStream() == '8') { // When there is Drools 9, we may enable the job for 9.x
+if (Utils.isMainBranch(this) && isStream8()) { // When there is Drools 9, we may enable the job for 9.x
     setupProjectDroolsJob('main')
 }
 
@@ -183,7 +183,7 @@ Map getMultijobPRConfig(JenkinsFolder jobFolder) {
                     BUILD_MVN_OPTS_CURRENT: '-Dfull',
                     OPTAPLANNER_BUILD_MVN_OPTS_UPSTREAM: '-Dfull',
                     env: [
-                        MIGRATE_TO_9: Utils.isMainBranch(this)
+                        MIGRATE_TO_9: Utils.isMainBranch(this) && isStream8()
                     ]
                 ]
             ]
@@ -410,4 +410,8 @@ String getOptaPlannerLatestStream() {
     } else {
         return gitMainBranch
     }
+}
+
+boolean isStream8() {
+    return getOptaPlannerLatestStream() == '8'
 }
