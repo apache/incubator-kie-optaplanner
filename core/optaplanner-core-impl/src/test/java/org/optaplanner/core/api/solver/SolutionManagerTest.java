c0c925763a60a789843130ada66b7adcff9eadaf
Lukáš Petrovický <lukas@petrovicky.net>
2023-02-15 09:42:47 +0100

    Add test coverage
    Signed-off-by: Lukáš Petrovický <lukas@petrovicky.net>

--- core/optaplanner-core-impl/src/test/java/org/optaplanner/core/api/solver/SolutionManagerTest.java
+++ core/optaplanner-core-impl/src/test/java/org/optaplanner/core/api/solver/SolutionManagerTest.java
@@ -9,37 +9,81 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.optaplanner.core.api.score.Score;
 import org.optaplanner.core.api.score.ScoreExplanation;
-import org.optaplanner.core.impl.testdata.domain.TestdataSolution;
+import org.optaplanner.core.impl.testdata.domain.shadow.TestdataShadowedSolution;
 
 public class SolutionManagerTest {
 
-    public static final SolverFactory<TestdataSolution> SOLVER_FACTORY =
-            SolverFactory.createFromXmlResource("org/optaplanner/core/api/solver/testdataSolverConfig.xml");
+    public static final SolverFactory<TestdataShadowedSolution> SOLVER_FACTORY =
+            SolverFactory.createFromXmlResource("org/optaplanner/core/api/solver/testdataShadowedSolverConfig.xml");
 
     @ParameterizedTest
     @EnumSource(SolutionManagerSource.class)
-    void updateScore(SolutionManagerSource SolutionManagerSource) {
-        SolutionManager<TestdataSolution, ?> SolutionManager = SolutionManagerSource.createSolutionManager(SOLVER_FACTORY);
+    void updateEverything(SolutionManagerSource SolutionManagerSource) {
+        SolutionManager<TestdataShadowedSolution, ?> SolutionManager =
+                SolutionManagerSource.createSolutionManager(SOLVER_FACTORY);
         assertThat(SolutionManager).isNotNull();
-        TestdataSolution solution = TestdataSolution.generateSolution();
-        assertThat(solution.getScore()).isNull();
+        TestdataShadowedSolution solution = TestdataShadowedSolution.generateSolution();
+        assertSoftly(softly -> {
+            softly.assertThat(solution.getScore()).isNull();
+            softly.assertThat(solution.getEntityList().get(0).getFirstShadow()).isNull();
+        });
         SolutionManager.update(solution);
-        assertThat(solution.getScore()).isNotNull();
+        assertSoftly(softly -> {
+            softly.assertThat(solution.getScore()).isNotNull();
+            softly.assertThat(solution.getEntityList().get(0).getFirstShadow()).isNotNull();
+        });
     }
 
     @ParameterizedTest
     @EnumSource(SolutionManagerSource.class)
-    void explainScore(SolutionManagerSource SolutionManagerSource) {
-        SolutionManager<TestdataSolution, ?> SolutionManager = SolutionManagerSource.createSolutionManager(SOLVER_FACTORY);
+    void updateOnlyShadowVariables(SolutionManagerSource SolutionManagerSource) {
+        SolutionManager<TestdataShadowedSolution, ?> SolutionManager =
+                SolutionManagerSource.createSolutionManager(SOLVER_FACTORY);
         assertThat(SolutionManager).isNotNull();
-        TestdataSolution solution = TestdataSolution.generateSolution();
-        ScoreExplanation<TestdataSolution, ?> scoreExplanation = SolutionManager.explain(solution);
+        TestdataShadowedSolution solution = TestdataShadowedSolution.generateSolution();
+        assertSoftly(softly -> {
+            softly.assertThat(solution.getScore()).isNull();
+            softly.assertThat(solution.getEntityList().get(0).getFirstShadow()).isNull();
+        });
+        SolutionManager.update(solution, SolutionUpdatePolicy.UPDATE_SHADOW_VARIABLES_ONLY);
+        assertSoftly(softly -> {
+            softly.assertThat(solution.getScore()).isNull();
+            softly.assertThat(solution.getEntityList().get(0).getFirstShadow()).isNotNull();
+        });
+    }
+
+    @ParameterizedTest
+    @EnumSource(SolutionManagerSource.class)
+    void updateOnlyScore(SolutionManagerSource SolutionManagerSource) {
+        SolutionManager<TestdataShadowedSolution, ?> SolutionManager =
+                SolutionManagerSource.createSolutionManager(SOLVER_FACTORY);
+        assertThat(SolutionManager).isNotNull();
+        TestdataShadowedSolution solution = TestdataShadowedSolution.generateSolution();
+        assertSoftly(softly -> {
+            softly.assertThat(solution.getScore()).isNull();
+            softly.assertThat(solution.getEntityList().get(0).getFirstShadow()).isNull();
+        });
+        SolutionManager.update(solution, SolutionUpdatePolicy.UPDATE_SCORE_ONLY);
+        assertSoftly(softly -> {
+            softly.assertThat(solution.getScore()).isNotNull();
+            softly.assertThat(solution.getEntityList().get(0).getFirstShadow()).isNull();
+        });
+    }
+
+    @ParameterizedTest
+    @EnumSource(SolutionManagerSource.class)
+    void explain(SolutionManagerSource SolutionManagerSource) {
+        SolutionManager<TestdataShadowedSolution, ?> solutionManager =
+                SolutionManagerSource.createSolutionManager(SOLVER_FACTORY);
+        assertThat(solutionManager).isNotNull();
+        TestdataShadowedSolution solution = TestdataShadowedSolution.generateSolution();
+        ScoreExplanation<TestdataShadowedSolution, ?> scoreExplanation = solutionManager.explain(solution);
         assertThat(scoreExplanation).isNotNull();
         assertSoftly(softly -> {
             softly.assertThat(scoreExplanation.getScore()).isNotNull();
             softly.assertThat(scoreExplanation.getSummary()).isNotBlank();
             softly.assertThat(scoreExplanation.getConstraintMatchTotalMap())
-                    .containsOnlyKeys("org.optaplanner.core.impl.testdata.domain/testConstraint");
+                    .containsOnlyKeys("org.optaplanner.core.impl.testdata.domain.shadow/testConstraint");
             softly.assertThat(scoreExplanation.getIndictmentMap())
                     .containsOnlyKeys(solution.getEntityList().toArray());
 
