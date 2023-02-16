859f9a5b14c49c08d070b22353f67705c6ff0751
Lukáš Petrovický <lukas@petrovicky.net>
2023-02-16 11:41:39 +0100

    Add extra coverage
    Signed-off-by: Lukáš Petrovický <lukas@petrovicky.net>

---
+++ optaplanner-test/src/test/java/org/optaplanner/test/api/score/stream/testdata/TestdataConstraintVerifierEntity.java
@@ -0,0 +1,35 @@
+package org.optaplanner.test.api.score.stream.testdata;
+
+import org.optaplanner.core.api.domain.entity.PlanningEntity;
+import org.optaplanner.core.api.domain.variable.PlanningVariable;
+import org.optaplanner.core.impl.testdata.domain.TestdataObject;
+import org.optaplanner.core.impl.testdata.domain.TestdataValue;
+
+@PlanningEntity
+public final class TestdataConstraintVerifierEntity extends TestdataObject {
+
+    private TestdataValue value;
+
+    public TestdataConstraintVerifierEntity(String code) {
+        super(code);
+    }
+
+    public TestdataConstraintVerifierEntity(String code, TestdataValue value) {
+        this(code);
+        this.value = value;
+    }
+
+    @PlanningVariable(valueRangeProviderRefs = "valueRange")
+    public TestdataValue getValue() {
+        return value;
+    }
+
+    public void setValue(TestdataValue value) {
+        this.value = value;
+    }
+
+    // ************************************************************************
+    // Complex methods
+    // ************************************************************************
+
+}
---
+++ optaplanner-test/src/test/java/org/optaplanner/test/api/score/stream/testdata/TestdataConstraintVerifierSolution.java
@@ -0,0 +1,77 @@
+package org.optaplanner.test.api.score.stream.testdata;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.optaplanner.core.api.domain.solution.PlanningEntityCollectionProperty;
+import org.optaplanner.core.api.domain.solution.PlanningScore;
+import org.optaplanner.core.api.domain.solution.PlanningSolution;
+import org.optaplanner.core.api.domain.solution.ProblemFactCollectionProperty;
+import org.optaplanner.core.api.domain.valuerange.ValueRangeProvider;
+import org.optaplanner.core.api.score.buildin.hardsoft.HardSoftScore;
+import org.optaplanner.core.impl.testdata.domain.TestdataObject;
+import org.optaplanner.core.impl.testdata.domain.TestdataValue;
+
+@PlanningSolution
+public class TestdataConstraintVerifierSolution extends TestdataObject {
+
+    public static TestdataConstraintVerifierSolution generateSolution(int valueListSize, int entityListSize) {
+        TestdataConstraintVerifierSolution solution = new TestdataConstraintVerifierSolution("Generated Solution 0");
+        List<TestdataValue> valueList = new ArrayList<>(valueListSize);
+        for (int i = 0; i < valueListSize; i++) {
+            TestdataValue value = new TestdataValue("Generated Value " + i);
+            valueList.add(value);
+        }
+        solution.setValueList(valueList);
+        List<TestdataConstraintVerifierEntity> entityList = new ArrayList<>(entityListSize);
+        for (int i = 0; i < entityListSize; i++) {
+            TestdataValue value = valueList.get(i % valueListSize);
+            TestdataConstraintVerifierEntity entity = new TestdataConstraintVerifierEntity("Generated Entity " + i, value);
+            entityList.add(entity);
+        }
+        solution.setEntityList(entityList);
+        return solution;
+    }
+
+    private List<TestdataValue> valueList;
+    private List<TestdataConstraintVerifierEntity> entityList;
+
+    private HardSoftScore score;
+
+    public TestdataConstraintVerifierSolution(String code) {
+        super(code);
+    }
+
+    @ValueRangeProvider(id = "valueRange")
+    @ProblemFactCollectionProperty
+    public List<TestdataValue> getValueList() {
+        return valueList;
+    }
+
+    public void setValueList(List<TestdataValue> valueList) {
+        this.valueList = valueList;
+    }
+
+    @PlanningEntityCollectionProperty
+    public List<TestdataConstraintVerifierEntity> getEntityList() {
+        return entityList;
+    }
+
+    public void setEntityList(List<TestdataConstraintVerifierEntity> entityList) {
+        this.entityList = entityList;
+    }
+
+    @PlanningScore
+    public HardSoftScore getScore() {
+        return score;
+    }
+
+    public void setScore(HardSoftScore score) {
+        this.score = score;
+    }
+
+    // ************************************************************************
+    // Complex methods
+    // ************************************************************************
+
+}
