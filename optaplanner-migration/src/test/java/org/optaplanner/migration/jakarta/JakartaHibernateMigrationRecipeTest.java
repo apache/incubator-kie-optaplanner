60e74de325a8c92d138b82e46fca84027b88cceb
Radovan Synek <rsynek@redhat.com>
2023-03-01 09:16:14 +0100

    Upgrade migration scripts to Quarkus 3.0.0.Alpha5

--- optaplanner-migration/src/test/java/org/optaplanner/migration/jakarta/JakartaHibernateMigrationRecipeTest.java
+++ optaplanner-migration/src/test/java/org/optaplanner/migration/jakarta/JakartaHibernateMigrationRecipeTest.java
@@ -23,11 +23,24 @@ public class JakartaHibernateMigrationRecipeTest implements RewriteTest {
                                         "    <groupId>org.optaplanner</groupId>\n" +
                                         "    <artifactId>optaplanner-persistence-jpa</artifactId>\n" +
                                         "    <version>0.0.1-SNAPSHOT</version>\n" +
+                                        "    <dependencyManagement>\n" +
+                                        "       <dependencies>\n" +
+                                        "           <dependency>\n" +
+                                        "               <groupId>org.hibernate</groupId>\n" +
+                                        "               <artifactId>hibernate-core</artifactId>\n" +
+                                        "               <version>5.6.14.Final</version>\n" +
+                                        "           </dependency>\n" +
+                                        "           <dependency>\n" +
+                                        "               <groupId>org.hibernate.orm</groupId>\n" +
+                                        "               <artifactId>hibernate-core</artifactId>\n" +
+                                        "               <version>6.2.0.CR3</version>\n" +
+                                        "           </dependency>\n" +
+                                        "       </dependencies>\n" +
+                                        "    </dependencyManagement>\n" +
                                         "    <dependencies>\n" +
                                         "        <dependency>\n" +
                                         "            <groupId>org.hibernate</groupId>\n" +
                                         "            <artifactId>hibernate-core</artifactId>\n" +
-                                        "            <version>5.6.14.Final</version>\n" +
                                         "            <exclusions>\n" +
                                         "                <exclusion>\n" +
                                         "                    <groupId>javax.activation</groupId>\n" +
@@ -53,11 +66,24 @@ public class JakartaHibernateMigrationRecipeTest implements RewriteTest {
                                         "    <groupId>org.optaplanner</groupId>\n" +
                                         "    <artifactId>optaplanner-persistence-jpa</artifactId>\n" +
                                         "    <version>0.0.1-SNAPSHOT</version>\n" +
+                                        "    <dependencyManagement>\n" +
+                                        "       <dependencies>\n" +
+                                        "           <dependency>\n" +
+                                        "               <groupId>org.hibernate</groupId>\n" +
+                                        "               <artifactId>hibernate-core</artifactId>\n" +
+                                        "               <version>5.6.14.Final</version>\n" +
+                                        "           </dependency>\n" +
+                                        "           <dependency>\n" +
+                                        "               <groupId>org.hibernate.orm</groupId>\n" +
+                                        "               <artifactId>hibernate-core</artifactId>\n" +
+                                        "               <version>6.2.0.CR3</version>\n" +
+                                        "           </dependency>\n" +
+                                        "       </dependencies>\n" +
+                                        "    </dependencyManagement>\n" +
                                         "    <dependencies>\n" +
                                         "        <dependency>\n" +
-                                        "            <groupId>org.hibernate</groupId>\n" +
-                                        "            <artifactId>hibernate-core-jakarta</artifactId>\n" +
-                                        "            <version>5.6.14.Final</version>\n" +
+                                        "            <groupId>org.hibernate.orm</groupId>\n" +
+                                        "            <artifactId>hibernate-core</artifactId>\n" +
                                         "        </dependency>\n" +
                                         "    </dependencies>\n" +
                                         "</project>\n")));
@@ -78,29 +104,55 @@ public class JakartaHibernateMigrationRecipeTest implements RewriteTest {
                                         "    <groupId>org.optaplanner</groupId>\n" +
                                         "    <artifactId>optaplanner-examples</artifactId>\n" +
                                         "    <version>0.0.1-SNAPSHOT</version>\n" +
+                                        "    <dependencyManagement>\n" +
+                                        "       <dependencies>\n" +
+                                        "           <dependency>\n" +
+                                        "               <groupId>org.hibernate</groupId>\n" +
+                                        "               <artifactId>hibernate-core</artifactId>\n" +
+                                        "               <version>5.6.14.Final</version>\n" +
+                                        "           </dependency>\n" +
+                                        "           <dependency>\n" +
+                                        "               <groupId>org.hibernate.orm</groupId>\n" +
+                                        "               <artifactId>hibernate-core</artifactId>\n" +
+                                        "               <version>6.2.0.CR3</version>\n" +
+                                        "           </dependency>\n" +
+                                        "       </dependencies>\n" +
+                                        "    </dependencyManagement>\n" +
                                         "    <dependencies>\n" +
                                         "        <dependency>\n" +
                                         "            <groupId>org.hibernate</groupId>\n" +
                                         "            <artifactId>hibernate-core</artifactId>\n" +
-                                        "            <version>5.6.14.Final</version>\n" +
                                         "        </dependency>\n" +
                                         "    </dependencies>\n" +
                                         "</project>\n",
                                 "<project xmlns=\"http://maven.apache.org/POM/4.0.0\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n"
-                                        +
-                                        "         xsi:schemaLocation=\"http://maven.apache.org/POM/4.0.0 https://maven.apache.org/xsd/maven-4.0.0.xsd\">\n"
-                                        +
-                                        "    <modelVersion>4.0.0</modelVersion>\n" +
-                                        "    <groupId>org.optaplanner</groupId>\n" +
-                                        "    <artifactId>optaplanner-examples</artifactId>\n" +
-                                        "    <version>0.0.1-SNAPSHOT</version>\n" +
-                                        "    <dependencies>\n" +
-                                        "        <dependency>\n" +
-                                        "            <groupId>org.hibernate</groupId>\n" +
-                                        "            <artifactId>hibernate-core-jakarta</artifactId>\n" +
-                                        "            <version>5.6.14.Final</version>\n" +
-                                        "        </dependency>\n" +
-                                        "    </dependencies>\n" +
-                                        "</project>\n")));
+                                +
+                                "         xsi:schemaLocation=\"http://maven.apache.org/POM/4.0.0 https://maven.apache.org/xsd/maven-4.0.0.xsd\">\n"
+                                +
+                                "    <modelVersion>4.0.0</modelVersion>\n" +
+                                "    <groupId>org.optaplanner</groupId>\n" +
+                                "    <artifactId>optaplanner-examples</artifactId>\n" +
+                                "    <version>0.0.1-SNAPSHOT</version>\n" +
+                                "    <dependencyManagement>\n" +
+                                "       <dependencies>\n" +
+                                "           <dependency>\n" +
+                                "               <groupId>org.hibernate</groupId>\n" +
+                                "               <artifactId>hibernate-core</artifactId>\n" +
+                                "               <version>5.6.14.Final</version>\n" +
+                                "           </dependency>\n" +
+                                "           <dependency>\n" +
+                                "               <groupId>org.hibernate.orm</groupId>\n" +
+                                "               <artifactId>hibernate-core</artifactId>\n" +
+                                "               <version>6.2.0.CR3</version>\n" +
+                                "           </dependency>\n" +
+                                "       </dependencies>\n" +
+                                "    </dependencyManagement>\n" +
+                                "    <dependencies>\n" +
+                                "        <dependency>\n" +
+                                "            <groupId>org.hibernate.orm</groupId>\n" +
+                                "            <artifactId>hibernate-core</artifactId>\n" +
+                                "        </dependency>\n" +
+                                "    </dependencies>\n" +
+                                "</project>\n")));
     }
 }
