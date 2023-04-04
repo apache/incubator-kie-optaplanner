a0967475063de1db825dd3c96a58cb7892f32c52
Lukáš Petrovický <lukas@petrovicky.net>
2023-04-04 09:16:01 +0200

    Better Javadoc

--- core/optaplanner-core-impl/src/main/java/org/optaplanner/core/impl/heuristic/selector/AbstractEqualityEnabledSelector.java
+++ core/optaplanner-core-impl/src/main/java/org/optaplanner/core/impl/heuristic/selector/AbstractEqualityEnabledSelector.java
@@ -1,9 +1,12 @@
 package org.optaplanner.core.impl.heuristic.selector;
 
+import org.optaplanner.core.impl.domain.variable.supply.Demand;
+
 /**
  * It is expected that if two instances share the same properties,
  * they are {@link Object#equals(Object) equal} to one another.
- * This is necessary for proper performance of caches, such as pillar cache or nearby distance matrix cache.
+ * This is necessary for proper performance of {@link Demand}-based caches,
+ * such as pillar cache or nearby distance matrix cache.
  *
  * @param <Solution_>
  */
